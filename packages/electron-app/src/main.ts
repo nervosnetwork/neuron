201892b7aa30ce491484bdeb1e8096198a3ea44a
Keith <keithwhisper@gmail.com>
2019-01-21 14:04:29 +0800

    chore: add conventional-commit and optimize tsconfig

--- packages/electron-app/src/main.ts
+++ packages/electron-app/src/main.ts
@@ -3,7 +3,7 @@ import { app, BrowserWindow } from 'electron'
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow: Electron.BrowserWindow
+let mainWindow: Electron.BrowserWindow | null
 
 function createWindow () {
   // Create the browser window.
