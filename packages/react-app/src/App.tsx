abdb336798d24ee09063691af237433ff26fbf64
James Chen <james@ashchan.com>
2019-01-29 09:01:53 +0900

    chore(ui): setup styled-components converting App.tsx to use it

--- packages/react-app/src/App.tsx
+++ packages/react-app/src/App.tsx
@@ -1,21 +1,42 @@
 import React from 'react'
+import styled, { keyframes } from 'styled-components'
 import logo from './logo.svg'
-import './App.scss'
+
+const AppHeader = styled.header`
+  background-color: white;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: black;
+`
+
+const appLogoSpin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+}
+`
+
+const AppLogo = styled.img.attrs({
+  src: logo,
+})`
+  height: 40vmin;
+  animation: ${appLogoSpin} infinite 20s linear;
+`
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+  <div>
+    <AppHeader>
+      <AppLogo />
       <p>Neuron is running...</p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Building with React
-      </a>
-    </header>
+    </AppHeader>
   </div>
 )
 
