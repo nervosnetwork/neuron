cb036fe91acd59f376656c4da277cb28b4dad32f
cezres <cezres@163.com>
2019-01-31 10:24:11 +0800

    fix: check url

--- packages/react-app/src/components/Network/SwitchNode.tsx
+++ packages/react-app/src/components/Network/SwitchNode.tsx
@@ -99,15 +99,15 @@ class SwitchNode extends React.Component<SwitchNodeProps> {
 
 function IsURL(str_url: string) {
   const strRegex = '^((https|http|ftp|rtsp|mms)?://)'
-  "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" +
+  '?(([0-9a-z_!~*().&=+$%-]+: )?[0-9a-z_!~*().&=+$%-]+@)?' +
     '(([0-9]{1,3}.){3}[0-9]{1,3}' +
     '|' +
-    "([0-9a-z_!~*'()-]+.)*" +
+    '([0-9a-z_!~*()-]+.)*' +
     '([0-9a-z][0-9a-z-]{0,61})?[0-9a-z].' +
     '[a-z]{2,6})' +
-    '(:[0-9]{1,4})?' +
+    '(:[0-9]{1,5})?' +
     '((/?)|' +
-    "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$"
+    '(/[0-9a-z_!~*().;?:@&=+$,%#-]+)+/?)$'
   const re = new RegExp(strRegex)
   if (re.test(str_url)) {
     return true
