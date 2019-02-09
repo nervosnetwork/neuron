d3d25c4e9fd21a64f76b9ca6a2dd9d123fc1253d
Keith <keithwhisper@gmail.com>
2019-02-10 02:56:41 +0800

    chore(neuron-ui): add react hooks rules

--- packages/neuron-ui/.eslintrc.js
+++ packages/neuron-ui/.eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   "extends": [
     "../../.eslintrc.js"
   ],
+  "plugins": ["react-hooks"],
   "settings": {
     "react": {
       "version": "detect"
@@ -10,6 +11,7 @@ module.exports = {
   "rules": {
     "react/jsx-filename-extension": [1, {
       "extensions": [".ts", ".tsx"]
-    }]
+    }],
+    "react-hooks/rules-of-hooks": 2
   }
 }
