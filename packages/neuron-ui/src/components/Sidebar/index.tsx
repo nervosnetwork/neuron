e13ce6c816013d8d0405e4e97034133492ef8314
James Chen <james@ashchan.com>
2019-02-11 12:06:39 +0900

    feat(ui): two-column layout

--- packages/neuron-ui/src/components/Sidebar/index.tsx
+++ packages/neuron-ui/src/components/Sidebar/index.tsx
@@ -2,24 +2,39 @@ import React from 'react'
 import { createPortal } from 'react-dom'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import { Disc } from 'grommet-icons'
 import { routes } from '../Router'
 
 const SidebarAside = styled.div`
-  display: grid;
-  background-color: #4cbc8e;
-  height: 100%;
-  h2 {
+  ul {
     margin: 0;
+    list-style: none;
+    li {
+      margin: 10px 0;
+    }
+    a {
+      display: flex;
+      align-items: center;
+      text-decoration: none;
+    }
   }
 `
 
 const Sidebar = () => (
   <SidebarAside>
-    {routes
-      .filter(route => route.showInSidebar)
-      .map(route => (
-        <Link to={route.path}>{route.name}</Link>
-      ))}
+    <ul>
+      <h2>My Wallet #1</h2>
+      {routes
+        .filter(route => route.showInSidebar)
+        .map(route => (
+          <li>
+            <Link to={route.path}>
+              <Disc />
+              {route.name}
+            </Link>
+          </li>
+        ))}
+    </ul>
   </SidebarAside>
 )
 
