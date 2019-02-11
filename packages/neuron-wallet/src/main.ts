96d6fe2f427a0173323bb2c3caec1ad98626e398
cezres <cezres@163.com>
2019-02-11 11:37:53 +0800

    fix(ui): set min window size

--- packages/neuron-wallet/src/main.ts
+++ packages/neuron-wallet/src/main.ts
@@ -17,6 +17,8 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    minWidth: 800,
+    minHeight: 600,
     webPreferences: {
       devTools: NODE_ENV === 'development',
     },
