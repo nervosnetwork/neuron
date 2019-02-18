f6f312972411f73fdfd8644786c093c1c78a3bf3
James Chen <james@ashchan.com>
2019-02-18 18:56:46 +0900

    chore(lint): customize object-curly-newline rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,12 @@ module.exports = {
       "exports": "always-multiline",
       "functions": "ignore"
     }],
+    "object-curly-newline": ["error", {
+      "ObjectExpression": "always",
+      "ObjectPattern": { "multiline": true },
+      "ImportDeclaration": "never",
+      "ExportDeclaration": { "multiline": true, "minProperties": 4 }
+    }],
     "import/no-extraneous-dependencies": [2, {
       "devDependencies": true
     }],
