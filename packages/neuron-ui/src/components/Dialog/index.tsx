33c041f78a664661dd9b03096fcc995d5db98e2a
Keith <keithwhisper@gmail.com>
2019-02-20 00:40:57 +0800

    feat(neuron-ui): add stateless dialog

---
+++ packages/neuron-ui/src/components/Dialog/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { createPortal } from 'react-dom'
+import styled from 'styled-components'
+
+const Dialog = styled.dialog.attrs({
+  open: true,
+})`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.7);
+`
+
+export default ({
+  children,
+  open,
+}: {
+  children?: React.ReactNode | string
+  open?: boolean
+}) => {
+  if (open && children) {
+    return createPortal(<Dialog>{children}</Dialog>, document.querySelector(
+      '.modal',
+    ) as HTMLElement)
+  }
+  return null
+}
