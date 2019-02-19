b84486661f0b476f1baa280df39f3aa9d6a63a8b
James Chen <james@ashchan.com>
2019-02-19 16:33:47 +0900

    chore: keep window-all-closed handler although it's unlikely to have multiple window feature

--- packages/neuron-wallet/src/main.ts
+++ packages/neuron-wallet/src/main.ts
@@ -51,6 +51,12 @@ function createWindow() {
 
 app.on('ready', createWindow)
 
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
 app.on('activate', () => {
   if (mainWindow === null) {
     createWindow()
