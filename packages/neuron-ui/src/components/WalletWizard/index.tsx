ab9ad060cec2cc98f270247ec1dccc941f197828
Keith <keithwhisper@gmail.com>
2019-02-20 02:19:55 +0800

    feat(neuron-ui): add full-screen layout for wizard

--- packages/neuron-ui/src/components/WalletWizard/index.tsx
+++ packages/neuron-ui/src/components/WalletWizard/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect, useContext } from 'react'
 import styled from 'styled-components'
 import { Launch } from 'grommet-icons'
+import WalletContext from '../../contexts/wallet'
 import { Routes } from '../../utils/const'
 import ImportWallet from './importWallet'
 import CreateWallet from './createWallet'
@@ -19,39 +20,52 @@ const Wizard = styled.div`
   }
 `
 
-export default (props: any) => (
-  <Wizard>
-    <div style={{ textAlign: 'center' }}>
-      <Launch size="large" />
-      <h1>Create or import your first wallet</h1>
-      <div className="buttonGroup">
-        <button
-          style={{ float: 'left' }}
-          type="button"
-          onKeyPress={() => {
-            //   for users with physical disabilities who cannot use a mouse
-          }}
-          onClick={() => {
-            props.history.push(Routes.CreateWallet)
-          }}
-        >
-          Create New Wallet
-        </button>
-        <button
-          style={{ float: 'right' }}
-          type="button"
-          onKeyPress={() => {
-            //   for users with physical disabilities who cannot use a mouse
-          }}
-          onClick={() => {
-            props.history.push(Routes.ImportWallet)
-          }}
-        >
-          Import Wallet
-        </button>
+export default (props: any) => {
+  const wallet = useContext(WalletContext)
+  useEffect(() => {
+    const content = document.querySelector('.main-content')
+    if (content) {
+      content.classList.add('full-screen')
+      return () => {
+        content.classList.remove('full-screen')
+      }
+    }
+    return () => {}
+  }, [wallet && wallet.name])
+  return (
+    <Wizard>
+      <div style={{ textAlign: 'center' }}>
+        <Launch size="large" />
+        <h1>Create or import your first wallet</h1>
+        <div className="buttonGroup">
+          <button
+            style={{ float: 'left' }}
+            type="button"
+            onKeyPress={() => {
+              //   for users with physical disabilities who cannot use a mouse
+            }}
+            onClick={() => {
+              props.history.push(Routes.CreateWallet)
+            }}
+          >
+            Create New Wallet
+          </button>
+          <button
+            style={{ float: 'right' }}
+            type="button"
+            onKeyPress={() => {
+              //   for users with physical disabilities who cannot use a mouse
+            }}
+            onClick={() => {
+              props.history.push(Routes.ImportWallet)
+            }}
+          >
+            Import Wallet
+          </button>
+        </div>
       </div>
-    </div>
-  </Wizard>
-)
+    </Wizard>
+  )
+}
 
 export { ImportWallet, CreateWallet }
