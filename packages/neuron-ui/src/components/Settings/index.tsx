768b10c7f15be979fda7793cf2f174a3efe6e0b9
duanyytop <duanyytop@gmail.com>
2019-02-20 20:05:35 +0800

    chore(wallet-ui): add mock data of settings

--- packages/neuron-ui/src/components/Settings/index.tsx
+++ packages/neuron-ui/src/components/Settings/index.tsx
@@ -1,20 +1,59 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Box, Tab, Tabs } from 'grommet'
+import { Tab, Tabs } from 'grommet'
 
 const SettingsPanel = styled.div`
   width: 800px;
 `
 
+const ContentPanel = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: left;
+  margin: 30px;
+`
+
+const ItemPanel = styled.li`
+  margin-top: 30px;
+`
+
 const Settings = () => {
-  const network: string = 'Network'
+  const tabs: string[] = ['General', 'Wallets', 'Network']
+  const wallets: string[] = [
+    'Wallet Name1',
+    'Wallet Name2',
+    'Wallet Name3',
+    'Wallet Name4',
+    'Wallet Name5',
+    'Wallet Name6',
+    'Wallet Name7',
+    'Wallet Name8',
+  ]
+  const networks: string[] = ['mainnet', 'testnet']
   return (
     <SettingsPanel>
       <Tabs flex="grow" alignSelf="center">
-        <Tab title="Tab 1">
-          <Box margin="small" pad="small">
-            <div>{network}</div>
-          </Box>
+        <Tab title={tabs[0]}>
+          <ContentPanel>
+            <ItemPanel>Photo Setting</ItemPanel>
+            <ItemPanel>Password Setting</ItemPanel>
+            <ItemPanel>Finger Setting</ItemPanel>
+          </ContentPanel>
+        </Tab>
+        <Tab title={tabs[1]}>
+          <ContentPanel>
+            {wallets.map(wallet => (
+              <ItemPanel>{wallet}</ItemPanel>
+            ))}
+          </ContentPanel>
+        </Tab>
+        <Tab title={tabs[2]}>
+          <ContentPanel>
+            {networks.map(network => (
+              <ItemPanel>{network}</ItemPanel>
+            ))}
+          </ContentPanel>
         </Tab>
       </Tabs>
     </SettingsPanel>
