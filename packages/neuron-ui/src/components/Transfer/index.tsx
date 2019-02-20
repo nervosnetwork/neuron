f36093950960486a46ea435f98510f06f44e55ae
duanyytop <duanyytop@gmail.com>
2019-02-20 12:45:53 +0800

    chore: delete context of transfer

--- packages/neuron-ui/src/components/Transfer/index.tsx
+++ packages/neuron-ui/src/components/Transfer/index.tsx
@@ -1,7 +1,6 @@
-import React, { useReducer, useContext } from 'react'
+import React, { useReducer } from 'react'
 import styled from 'styled-components'
 import { RangeInput } from 'grommet'
-import TransferContext from '../../contexts/transfer'
 
 import ipc from '../../utils/ipc'
 
@@ -21,21 +20,21 @@ const InputDiv = styled.div`
     font-size: 16px;
     margin-top: 10px;
     padding-left: 5px;
-    width: 600px;
+    width: 640px;
   }
 `
 
 const SendButton = styled.button`
   height: 30px;
   font-size: 18px;
-  margin: 30px 0 0 150px;
+  margin: 30px 0 0 160px;
   width: 300px;
 `
 
 const RangeLayout = styled.div`
   display: flex;
   flex-direction: column;
-  width: 500px;
+  width: 600px;
   margin-top: 30px;
 
   div {
@@ -49,13 +48,14 @@ enum TRANSFER_ACTION_TYPES {
   SUBMIT,
 }
 
-interface ITransfer {
+interface Transfer {
   addr: string
   capacity: number
+  fee: number
   submitting: boolean
 }
 
-const reducer = (state: ITransfer, action: { type: TRANSFER_ACTION_TYPES; value?: any }) => {
+const reducer = (state: Transfer, action: { type: TRANSFER_ACTION_TYPES; value?: any }) => {
   switch (action.type) {
     case TRANSFER_ACTION_TYPES.ADDR: {
       return { ...state, addr: action.value }
@@ -72,9 +72,10 @@ const reducer = (state: ITransfer, action: { type: TRANSFER_ACTION_TYPES; value?
   }
 }
 
-const initState: ITransfer = {
+const initState: Transfer = {
   addr: '',
   capacity: 0,
+  fee: 0,
   submitting: false,
 }
 
@@ -84,7 +85,6 @@ function isMouseEvent(e: React.ChangeEvent | React.MouseEvent): e is React.Mouse
 
 const Transfer = () => {
   const [state, dispatch] = useReducer(reducer, initState)
-  const transferContext = useContext(TransferContext)
   const handleAction = (type: TRANSFER_ACTION_TYPES) => (
     e: React.ChangeEvent<HTMLInputElement> | React.MouseEvent<HTMLButtonElement>,
   ) => {
@@ -107,7 +107,7 @@ const Transfer = () => {
         <input
           type="text"
           defaultValue={state.addr || ''}
-          placeholder="eg: 0xcf078d66b3614C4c32B018ceF9100A39FaE7DC0D"
+          placeholder="eg: 0x0da2fe99fe549e082d4ed483c2e968a89ea8d11aabf5d79e5cbf06522de6e674"
           onChange={() => {
             handleAction(TRANSFER_ACTION_TYPES.ADDR)
           }}
@@ -128,12 +128,12 @@ const Transfer = () => {
         <div>Transfer Fee: </div>
         <RangeInput
           type="text"
-          defaultValue={transferContext.fee.toString()}
+          defaultValue={state.fee.toString()}
           min={0}
           max={100}
           step={1}
           onChange={(event: any) => {
-            transferContext.fee = event.target.value
+            state.fee = event.target.value
           }}
         />
       </RangeLayout>
