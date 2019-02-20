dd1d7e7eb19dce11532c79433cfe342771ed1fc8
duanyytop <duanyytop@gmail.com>
2019-02-20 10:49:57 +0800

    feat: add receive page

--- packages/neuron-ui/src/components/Receive/index.tsx
+++ packages/neuron-ui/src/components/Receive/index.tsx
@@ -1,3 +1,32 @@
 import React from 'react'
+import styled from 'styled-components'
+import QRCode from 'qrcode.react'
 
-export default () => <h1>Receive</h1>
+const ReceivePanel = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 600px;
+`
+const QRCodePanel = styled.div`
+  width: 300px;
+  margin: 20px 0 30px 150px;
+`
+
+const Address = styled.div`
+  color: black;
+`
+
+const Receive = () => {
+  const address: string = '0xcf078d66b3614C4c32B018ceF9100A39FaE7DC0D'
+  return (
+    <ReceivePanel>
+      <h1>Receive</h1>
+      <QRCodePanel>
+        <QRCode value={address} />
+      </QRCodePanel>
+      <Address>{address}</Address>
+    </ReceivePanel>
+  )
+}
+
+export default Receive
