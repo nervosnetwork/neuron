49091925857a9f551a077da1cedbb13324390bf8
duanyytop <duanyytop@gmail.com>
2019-02-20 15:38:59 +0800

    chore(wallet-ui): add tabs widget to settings page

--- packages/neuron-ui/src/components/Settings/index.tsx
+++ packages/neuron-ui/src/components/Settings/index.tsx
@@ -1,3 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Box, Tab, Tabs } from 'grommet'
 
-export default () => <h1>Settings</h1>
+const SettingsPanel = styled.div`
+  width: 800px;
+`
+
+const Settings = () => {
+  const network: string = 'Network'
+  return (
+    <SettingsPanel>
+      <Tabs flex="grow" alignSelf="center">
+        <Tab title="Tab 1">
+          <Box margin="small" pad="small">
+            <div>{network}</div>
+          </Box>
+        </Tab>
+      </Tabs>
+    </SettingsPanel>
+  )
+}
+
+export default Settings
