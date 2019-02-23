9742d359e177dec501b1e6ccc8222281a03fef6d
Keith <keithwhisper@gmail.com>
2019-02-24 00:04:19 +0800

    feat(neuron-ui): fetch network from neuron-wallet

--- packages/neuron-ui/src/utils/const.ts
+++ packages/neuron-ui/src/utils/const.ts
@@ -4,8 +4,29 @@ export enum NetworkStatus {
 }
 
 export enum Channel {
-  SendCapacity = 'sendCapacity',
+  // Chain
+  GetBlock = 'getBlock',
+  GetTransaction = 'getTransaction',
+  GetLiveCell = 'getLiveCell',
+  GetTipHeader = 'getTipHeader',
+  GetTipBlockNumber = 'getTipBlockNumber',
+  GetLocalNodeId = 'getLocalNodeId',
+  GetNetwork = 'getNetwork',
+  GwitchNetwork = 'switchNetwork',
+
+  // Wallet
+  CreateWallet = 'createWallet',
+  ImportWallet = 'importWallet',
+  ExportWallet = 'exportWallet',
+  SwitchWallet = 'switchWallet',
+  GetBalance = 'getBalance',
   GetCellsByTypeHash = 'getCellsByTypeHash',
+  GetUnspentCells = 'getUnspentCells',
+  GetTransactions = 'getTransactions',
+  GetWallets = 'getWallets',
+  SendCapacity = 'sendCapacity',
+  SendTransaction = 'sendTransaction',
+  Sign = 'sign',
 }
 
 export enum Routes {
