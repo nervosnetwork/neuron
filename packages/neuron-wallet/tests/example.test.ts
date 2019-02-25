6730f5343cb5a8f725b898af8a222b5011c0467a
James Chen <james@ashchan.com>
2019-02-25 11:29:43 +0900

    chore(ci): test that electron app can start/stop

--- packages/neuron-wallet/tests/example.test.ts
+++ packages/neuron-wallet/tests/example.test.ts
@@ -1,5 +1,30 @@
+import { Application } from 'spectron'
+
 describe('Setup tests', () => {
-  it('should run test', () => {
-    expect(true).toBe(true)
+  let app: Application
+
+  beforeEach(() => {
+    app = new Application({
+      path: 'node_modules/.bin/electron',
+      args: ['dist/main.js'],
+    })
+
+    return app.start()
+  })
+
+  afterEach(() => {
+    if (app.isRunning) {
+      return app.stop()
+    }
+    return null
+  })
+
+  it('opens app window', async () => {
+    const { client, browserWindow } = app
+
+    await client.waitUntilWindowLoaded()
+    const title = await browserWindow.getTitle()
+
+    expect(title).toBe('Electron')
   })
 })
