7eaad60fc035fd17e2d10e309c785397b626f796
duanyytop <duanyytop@gmail.com>
2019-03-13 17:07:32 +0800

    refactor: refactor researching hd wallet used address

--- packages/neuron-wallet/src/keys/tool.ts
+++ packages/neuron-wallet/src/keys/tool.ts
@@ -1,20 +1,23 @@
 import { BIP32 } from 'bip32'
 import { Child } from './keystore'
 
-// coin_type. 360 is tentative value  https://github.com/satoshilabs/slips/blob/master/slip-0044.md
+// BIP44: https://github.com/satoshilabs/slips/blob/master/slip-0044.md
+const PURPOSE = "44'"
+// 360 is tentative value
 const COIN_TYPE_TESTNET: string = "360'"
-// account: 0
-const ACCOUNT = 0
+const ACCOUNT = "0'"
 // external address
 const CHANGE = 0
 
-const GAP_LIMIT = 20
-
 export default class Tool {
+  private static root: BIP32
+
   public static searchUsedChildKeys(root: BIP32) {
+    this.root = root
     const children: Child[] = []
-    // TODO: refactor search logic to reduce loop
-    for (let index = 0; index < GAP_LIMIT; index++) {
+    // TODO: refactor me
+    const nextUnusedIndex = this.searchIterationForExternalAddress(20)
+    for (let index = 0; index < nextUnusedIndex; index++) {
       const path = Tool.getPath(index)
       const { privateKey, publicKey, chainCode } = root.derivePath(path)
       if (this.isUsedAddress(this.getAddressFromPublicKey(publicKey.toString('hex')))) {
@@ -29,7 +32,7 @@ export default class Tool {
   }
 
   private static getPath(index: number): string {
-    return `m/44'/${COIN_TYPE_TESTNET}/${ACCOUNT}'/${CHANGE}/${index}`
+    return `m/${PURPOSE}/${COIN_TYPE_TESTNET}/${ACCOUNT}/${CHANGE}/${index}`
   }
 
   private static isUsedAddress(address: string) {
@@ -39,5 +42,42 @@ export default class Tool {
 
   private static getAddressFromPublicKey(publicKey: string): string {
     return `ckb${publicKey}`
+  }
+
+  private static getAddressFromHDIndex(index: number): string {
+    const path = Tool.getPath(index)
+    const { publicKey } = this.root.derivePath(path)
+    return this.getAddressFromPublicKey(publicKey.toString('hex'))
+  }
+
+  // minUnusedIndex should be big number, 100 is convenient to do test.
+  // depth is loop depth for searching used address
+  private static searchIterationForExternalAddress(
+    index: number,
+    maxUsedIndex = 0,
+    minUnusedIndex = 100,
+    depth = 0,
+  ): number {
+    if (depth >= 10) return maxUsedIndex + 1
+    if (!this.isUsedAddress(this.getAddressFromHDIndex(index))) {
+      if (index === 0) {
+        return 0
+      }
+      return this.searchIterationForExternalAddress(
+        Math.floor((index - maxUsedIndex) / 2 + maxUsedIndex),
+        maxUsedIndex,
+        Math.min(minUnusedIndex, index),
+        depth + 1,
+      )
+    }
+    if (!this.isUsedAddress(this.getAddressFromHDIndex(index + 1))) {
+      return index + 1
+    }
+    return this.searchIterationForExternalAddress(
+      Math.round((minUnusedIndex - index) / 2 + index),
+      Math.max(maxUsedIndex, index),
+      minUnusedIndex,
+      depth + 1,
+    )
   }
 }
