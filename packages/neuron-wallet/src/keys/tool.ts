c590c66ad98453f537e5a60e4f9e346c3f1122c0
duanyytop <duanyytop@gmail.com>
2019-03-13 18:59:05 +0800

    chore: update BIP44 constant

--- packages/neuron-wallet/src/keys/tool.ts
+++ packages/neuron-wallet/src/keys/tool.ts
@@ -2,25 +2,27 @@ import { BIP32 } from 'bip32'
 import { Child } from './keystore'
 
 // BIP44: https://github.com/satoshilabs/slips/blob/master/slip-0044.md
-const PURPOSE = "44'"
-// 360 is tentative value
-const COIN_TYPE_TESTNET: string = "360'"
-const ACCOUNT = "0'"
-// external address
-const CHANGE = 0
+enum Constant {
+  PURPOSE = "44'",
+  // 360 is tentative value
+  COIN_TYPE_TESTNET = "360'",
+  ACCOUNT = "0'",
+  // external address
+  CHANGE = 0,
+}
 
 export default class Tool {
   private static root: BIP32
 
   public static searchUsedChildKeys(root: BIP32) {
-    this.root = root
+    Tool.root = root
     const children: Child[] = []
     // TODO: refactor me
-    const nextUnusedIndex = this.searchIterationForExternalAddress(20)
+    const nextUnusedIndex = Tool.searchAddress(20)
     for (let index = 0; index < nextUnusedIndex; index++) {
       const path = Tool.getPath(index)
       const { privateKey, publicKey, chainCode } = root.derivePath(path)
-      if (this.isUsedAddress(this.getAddressFromPublicKey(publicKey.toString('hex')))) {
+      if (Tool.isUsedAddress(Tool.getAddressFromPublicKey(publicKey.toString('hex')))) {
         children.push({
           path,
           privateKey: privateKey.toString('hex'),
@@ -32,7 +34,7 @@ export default class Tool {
   }
 
   private static getPath(index: number): string {
-    return `m/${PURPOSE}/${COIN_TYPE_TESTNET}/${ACCOUNT}/${CHANGE}/${index}`
+    return `m/${Constant.PURPOSE}/${Constant.COIN_TYPE_TESTNET}/${Constant.ACCOUNT}/${Constant.CHANGE}/${index}`
   }
 
   private static isUsedAddress(address: string) {
@@ -46,34 +48,29 @@ export default class Tool {
 
   private static getAddressFromHDIndex(index: number): string {
     const path = Tool.getPath(index)
-    const { publicKey } = this.root.derivePath(path)
-    return this.getAddressFromPublicKey(publicKey.toString('hex'))
+    const { publicKey } = Tool.root.derivePath(path)
+    return Tool.getAddressFromPublicKey(publicKey.toString('hex'))
   }
 
   // minUnusedIndex should be big number, 100 is convenient to do test.
   // depth is loop depth for searching used address
-  private static searchIterationForExternalAddress(
-    index: number,
-    maxUsedIndex = 0,
-    minUnusedIndex = 100,
-    depth = 0,
-  ): number {
+  private static searchAddress(index: number, maxUsedIndex = 0, minUnusedIndex = 100, depth = 0): number {
     if (depth >= 10) return maxUsedIndex + 1
-    if (!this.isUsedAddress(this.getAddressFromHDIndex(index))) {
+    if (!Tool.isUsedAddress(Tool.getAddressFromHDIndex(index))) {
       if (index === 0) {
         return 0
       }
-      return this.searchIterationForExternalAddress(
+      return Tool.searchAddress(
         Math.floor((index - maxUsedIndex) / 2 + maxUsedIndex),
         maxUsedIndex,
         Math.min(minUnusedIndex, index),
         depth + 1,
       )
     }
-    if (!this.isUsedAddress(this.getAddressFromHDIndex(index + 1))) {
+    if (!Tool.isUsedAddress(Tool.getAddressFromHDIndex(index + 1))) {
       return index + 1
     }
-    return this.searchIterationForExternalAddress(
+    return Tool.searchAddress(
       Math.round((minUnusedIndex - index) / 2 + index),
       Math.max(maxUsedIndex, index),
       minUnusedIndex,
