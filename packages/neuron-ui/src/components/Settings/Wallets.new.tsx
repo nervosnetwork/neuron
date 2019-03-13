9f652071ceb65162effd11cec4aa4e376be5dd04
秋月夜 <baojun.cz@gmail.com>
2019-03-13 17:56:32 +0800

    feat(neuron-ui): wallets add button

--- packages/neuron-ui/src/components/Settings/Wallets.new.tsx
+++ packages/neuron-ui/src/components/Settings/Wallets.new.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useContext } from 'react'
-import { ListGroup, Form } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { ListGroup, Form, Container } from 'react-bootstrap'
+import { Routes } from '../../utils/const'
 import WalletsContext from '../../contexts/Wallets'
 import { Wallet } from '../../contexts/Wallet'
 import { getWallets } from '../../services/UILayer'
@@ -13,32 +15,51 @@ const Wallets = () => {
   })
 
   return (
-    // <text>{JSON.stringify(wallets)}</text>
-    <ListGroup>
-      {wallets.map((wallet, index) => {
-        const isChecked = index === walletSelected
-        return (
-          <ListGroup.Item
-            key={wallet.name}
-            style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-            }}
-          >
-            <Form.Check
-              inline
-              label={wallet.name}
-              type="radio"
-              checked={isChecked}
-              disabled={isChecked}
-              onClick={() => {
-                setWalletSelected(index)
+    <>
+      <ListGroup>
+        {wallets.map((wallet, index) => {
+          const isChecked = index === walletSelected
+          return (
+            <ListGroup.Item
+              key={wallet.name}
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
               }}
-            />
-          </ListGroup.Item>
-        )
-      })}
-    </ListGroup>
+            >
+              <Form.Check
+                inline
+                label={wallet.name}
+                type="radio"
+                checked={isChecked}
+                disabled={isChecked}
+                onChange={() => {
+                  setWalletSelected(index)
+                }}
+              />
+            </ListGroup.Item>
+          )
+        })}
+      </ListGroup>
+      <Container
+        style={{
+          marginTop: 30,
+        }}
+      >
+        <Link to={`${Routes.CreateWallet}/new`} className="btn btn-primary">
+          Create Wallet
+        </Link>
+        <Link
+          to={`${Routes.ImportWallet}/new`}
+          className="btn btn-primary"
+          style={{
+            marginLeft: 30,
+          }}
+        >
+          Import Wallet
+        </Link>
+      </Container>
+    </>
   )
 }
 
