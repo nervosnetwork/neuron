fd0fc937ca59c6e31c80101474baf0e7010f290e
duanyytop <duanyytop@gmail.com>
2019-03-14 16:43:49 +0800

    chore: add bootstrap to receive UI

--- packages/neuron-ui/src/components/Receive/index.tsx
+++ packages/neuron-ui/src/components/Receive/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { OverlayTrigger, Tooltip } from 'react-bootstrap'
+import { OverlayTrigger, Tooltip, Container, Card } from 'react-bootstrap'
 import QRCode from 'qrcode.react'
 
 declare global {
@@ -9,32 +9,35 @@ declare global {
   }
 }
 
-const ReceivePanel = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 600px;
-`
 const QRCodePanel = styled.div`
-  width: 300px;
-  margin: 20px 0 30px 150px;
+  margin-top: 30px;
+  text-align: center;
 `
 
 const Address = styled.div`
   color: black;
+  margin-top: 50px;
+  text-align: center;
 `
 
 const Receive = () => {
   const address: string = '0x0da2fe99fe549e082d4ed483c2e968a89ea8d11aabf5d79e5cbf06522de6e674'
   return (
-    <ReceivePanel>
-      <h1>Receive</h1>
-      <QRCodePanel>
-        <QRCode value={address} />
-      </QRCodePanel>
-      <OverlayTrigger placement="bottom" overlay={<Tooltip id="address-tooltip">Click to copy</Tooltip>}>
-        <Address onClick={() => window.clipboard.writeText(address)}>{address}</Address>
-      </OverlayTrigger>
-    </ReceivePanel>
+    <Container>
+      <Card>
+        <Card.Header>
+          <h2>Receive</h2>
+        </Card.Header>
+        <Card.Body>
+          <QRCodePanel>
+            <QRCode value={address} size={256} />
+          </QRCodePanel>
+          <OverlayTrigger placement="bottom" overlay={<Tooltip id="address-tooltip">Click to copy</Tooltip>}>
+            <Address onClick={() => window.clipboard.writeText(address)}>{address}</Address>
+          </OverlayTrigger>
+        </Card.Body>
+      </Card>
+    </Container>
   )
 }
 
