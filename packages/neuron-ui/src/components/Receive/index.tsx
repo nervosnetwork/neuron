20ba450be1eb7135739dcccb6250b9a20861be4e
duanyytop <duanyytop@gmail.com>
2019-03-18 19:11:06 +0800

    chore: update receive ui

--- packages/neuron-ui/src/components/Receive/index.tsx
+++ packages/neuron-ui/src/components/Receive/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { OverlayTrigger, Tooltip, Container, Card } from 'react-bootstrap'
+import { Container, Card } from 'react-bootstrap'
 import QRCode from 'qrcode.react'
 import { useTranslation } from 'react-i18next'
 
@@ -10,20 +10,42 @@ declare global {
   }
 }
 
-const QRCodePanel = styled.div`
-  margin-top: 30px;
-  text-align: center;
+const AddressTip = styled.div`
+  color: black;
+  font-size: 18px;
+  font-weight: bold;
+  margin: 15px 0 0 30px;
+`
+
+const AddressPanel = styled.div`
+  dispaly: flex;
+  display: -webkit-flex; /* Safari */
+  flex-direction: row;
+  margin: 10px 0 0 30px;
 `
 
 const Address = styled.div`
   color: black;
-  margin-top: 50px;
-  text-align: center;
+  margin-right: 10px;
 `
 
-const Receive = () => {
+const CopyImage = styled.img`
+  width: 15px;
+  height: 20px;
+  padding-top: 5px;
+`
+
+const QRCodePanel = styled.div`
+  margin: 50px 0 0 30px;
+`
+
+const Receive = ({ address }: { address: string }) => {
   const [t] = useTranslation()
-  const address: string = '0x0da2fe99fe549e082d4ed483c2e968a89ea8d11aabf5d79e5cbf06522de6e674'
+  const generateNewAddress = () => {
+    // TODO: generate new address
+    return '0x0da2fe99fe549e082d4ed483c2e968a89ea8d11aabf5d79e5cbf06522de6e674'
+  }
+  const accountAddress = address === undefined ? generateNewAddress() : address
   return (
     <Container>
       <Card>
@@ -31,12 +53,19 @@ const Receive = () => {
           <h2>{t('Receive')}</h2>
         </Card.Header>
         <Card.Body>
+          <AddressTip>{t('Address')}</AddressTip>
+          <AddressPanel>
+            <Address onClick={() => window.clipboard.writeText(accountAddress)}>{accountAddress}</Address>
+            <CopyImage
+              src="./image/copy.png"
+              alt="copy address"
+              onClick={() => window.clipboard.writeText(accountAddress)}
+              onKeyPress={() => window.clipboard.writeText(accountAddress)}
+            />
+          </AddressPanel>
           <QRCodePanel>
-            <QRCode value={address} size={256} />
+            <QRCode value={accountAddress} size={400} />
           </QRCodePanel>
-          <OverlayTrigger placement="bottom" overlay={<Tooltip id="address-tooltip">{t('Click to copy')}</Tooltip>}>
-            <Address onClick={() => window.clipboard.writeText(address)}>{address}</Address>
-          </OverlayTrigger>
         </Card.Body>
       </Card>
     </Container>
