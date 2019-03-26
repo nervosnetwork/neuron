a238dac00dab502c1d256c36e96e24b540f77e54
classicalliu <classicalliu@gmail.com>
2019-03-26 18:56:02 +0800

    chore: mock some interface in cell module

--- packages/neuron-wallet/src/cell.ts
+++ packages/neuron-wallet/src/cell.ts
@@ -1,5 +1,6 @@
 import asw from './wallets/asw'
 import ckbCore from './core'
+import { getCellChanges, storeCells } from './mock_rpc'
 
 export interface OutPoint {
   hash: string
@@ -21,6 +22,8 @@ export interface Cell {
   lock: string
   type?: Script | null
   outPoint?: OutPoint
+  state?: string
+  stateChange?: string
 }
 
 export const getUnspentCells = async () => {
@@ -31,6 +34,46 @@ export const getUnspentCells = async () => {
 export const getLiveCell = async (outPoint: OutPoint) => {
   const liveCell = await ckbCore.rpc.getLiveCell(outPoint)
   return liveCell
+}
+
+/* eslint @typescript-eslint/no-unused-vars: "warn" */
+
+// save one Cell to db
+// check cell exists and status
+// if exists, and status right, ignore
+// if exists, and status not right, update or throw(something wrong)
+// if not exist, insert
+export const saveCell = async (cell: Cell) => {
+  return cell
+}
+
+// marked a cell used
+// update state = 'spent'
+export const markCellSpent = async (cell: Cell) => {
+  return cell
+}
+
+// get cells changes from chain and save to db
+export const loadCellsFromChain = async () => {
+  const cells = await getCellChanges()
+  cells.forEach(cell => {
+    if (cell.stateChange === 'created') {
+      saveCell(cell)
+    } else if (cell.stateChange === 'spent') {
+      markCellSpent(cell)
+    }
+    // any stateChange else would be ignored
+  })
+}
+
+// different wallet has different cells and txs
+export const getCellsByWallet = async (page: number, perPage: number, walletID: string) => {
+  const cells = storeCells
+
+  return {
+    totalCount: cells.length,
+    cells,
+  }
 }
 
 export default {
