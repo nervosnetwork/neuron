75b8a2ea7fd5150af8850833389f0b3197e0072e
秋月夜 <baojun.cz@gmail.com>
2019-04-15 16:48:47 +0800

    refactor: wallet manager ui

--- packages/neuron-ui/src/components/Settings/Wallets.tsx
+++ packages/neuron-ui/src/components/Settings/Wallets.tsx
@@ -1,52 +1,24 @@
-import React, { useEffect } from 'react'
+import React, { useMemo, useEffect } from 'react'
 import { Link, RouteComponentProps } from 'react-router-dom'
 import { Col, Row, ListGroup, Form, Container } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
-import styled from 'styled-components'
-import { Configure } from 'grommet-icons'
 
 import { Routes, MnemonicAction } from '../../utils/const'
 import { ContentProps } from '../../containers/MainContent'
 import { actionCreators, MainActions } from '../../containers/MainContent/reducer'
 import InputWalletPasswordDialog, { CheckType } from './InputWalletPasswordDialog'
 import Dialog from '../../widgets/Dialog'
-import Dropdown, { DropDownItem } from '../../widgets/Dropdown'
 import { useNeuronWallet } from '../../utils/hooks'
+import ContextMenuZone from '../../widgets/ContextMenuZone'
 
-const Popover = styled.div`
-  position: relative;
-  &:hover {
-    & > ul {
-      display: block !important;
-    }
-  }
-`
+interface MenuItemParams {
+  id: string
+}
 
 const buttons = [
   { label: 'wizard.create-new-wallet', href: `${Routes.Mnemonic}/${MnemonicAction.Create}` },
   { label: 'wizard.import-wallet', href: `${Routes.Mnemonic}/${MnemonicAction.Import}` },
 ]
-
-const WalletActions = ({ isDefault, actionItems }: { isDefault: boolean; actionItems: DropDownItem[] }) => {
-  if (isDefault) {
-    actionItems.splice(0, 1)
-  }
-  return (
-    <Popover>
-      <Configure />
-      <Dropdown
-        items={actionItems}
-        style={{
-          position: 'absolute',
-          top: '100%',
-          right: '0',
-          zIndex: '999',
-          display: 'none',
-        }}
-      />
-    </Popover>
-  )
-}
 
 const Wallets = (props: React.PropsWithoutRef<ContentProps & RouteComponentProps>) => {
   const {
@@ -61,72 +33,75 @@ const Wallets = (props: React.PropsWithoutRef<ContentProps & RouteComponentProps
     dispatch(actionCreators.getActiveWallet())
   }, [])
 
-  const actionItems = (id: string) => [
-    {
-      label: t('menuitem.select'),
-      key: 'select',
-      onClick: () => {
-        dispatch(actionCreators.activateWallet(id))
+  const menuItems = useMemo(
+    () => [
+      {
+        label: t('menuitem.select'),
+        isDisabled: ({ id }: MenuItemParams) => {
+          return id === activeWallet.id
+        },
+        click: ({ id }: MenuItemParams) => {
+          dispatch(actionCreators.activateWallet(id))
+        },
       },
-    },
-    {
-      label: t('menuitem.backup'),
-      key: 'backup',
-      onClick: () => {
-        dispatch(actionCreators.backupWallet(id))
+      {
+        label: t('menuitem.backup'),
+        click: ({ id }: MenuItemParams) => {
+          dispatch(actionCreators.backupWallet(id))
+        },
       },
-    },
-    {
-      label: t('menuitem.edit'),
-      key: 'edit',
-      onClick: () => {
-        history.push(`${Routes.WalletEditor}/${id}}`)
+      {
+        label: t('menuitem.edit'),
+        click: ({ id }: MenuItemParams) => {
+          history.push(`${Routes.WalletEditor}/${id}}`)
+        },
       },
-    },
-    {
-      label: t('menuitem.remove'),
-      key: 'remove',
-      onClick: () => {
-        dispatch({
-          type: MainActions.SetDialog,
-          payload: {
-            open: true,
-            wallet: wallets.find(wallet => wallet.id === id),
-          },
-        })
+      {
+        label: t('menuitem.remove'),
+        click: ({ id }: MenuItemParams) => {
+          dispatch({
+            type: MainActions.SetDialog,
+            payload: {
+              open: true,
+              wallet: wallets.find(wallet => wallet.id === id),
+            },
+          })
+        },
       },
-    },
-  ]
+    ],
+    [],
+  )
 
   return (
     <>
-      <ListGroup>
-        {wallets.map(wallet => {
-          const isChecked = wallet.id === activeWallet.id
-          return (
-            <ListGroup.Item
-              key={wallet.id}
-              style={{
-                display: 'flex',
-                justifyContent: 'space-between',
-              }}
-            >
-              <Form.Check
-                inline
-                label={wallet.name}
-                type="radio"
-                checked={isChecked}
-                disabled={isChecked}
-                onChange={() => {
-                  // setWalletSelected(index)
-                  dispatch(actionCreators.activateWallet(wallet.id))
+      <ContextMenuZone menuItems={menuItems}>
+        <ListGroup>
+          {wallets.map(wallet => {
+            const isChecked = wallet.id === activeWallet.id
+            return (
+              <ListGroup.Item
+                key={wallet.id}
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
                 }}
-              />
-              <WalletActions isDefault={isChecked} actionItems={actionItems(wallet.id)} />
-            </ListGroup.Item>
-          )
-        })}
-      </ListGroup>
+                data-menuitem={JSON.stringify({ id: wallet.id })}
+              >
+                <Form.Check
+                  inline
+                  label={wallet.name}
+                  type="radio"
+                  checked={isChecked}
+                  disabled={isChecked}
+                  onChange={() => {
+                    dispatch(actionCreators.activateWallet(wallet.id))
+                  }}
+                />
+              </ListGroup.Item>
+            )
+          })}
+        </ListGroup>
+      </ContextMenuZone>
       <Container
         style={{
           marginTop: 30,
