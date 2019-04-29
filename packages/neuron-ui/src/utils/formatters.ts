a12f76dbed33c6edf0ef02a920933f48c9459966
Keith <keithwhisper@gmail.com>
2019-04-29 16:27:09 +0800

    refactor(neuron-ui): refactor history view, group hooks

--- packages/neuron-ui/src/utils/formatters.ts
+++ packages/neuron-ui/src/utils/formatters.ts
@@ -1,14 +1,3 @@
-export const dateFormatter = (time: Date) => {
-  const date = new Date(time)
-  const y = date.getFullYear()
-  const m = date.getMonth() + 1
-  const d = date.getDate()
-  return {
-    date: `${y}-${m < 10 ? `0${m}` : m}-${d < 10 ? `0${d}` : d}`,
-    time: date.toTimeString().substr(0, 8),
-  }
-}
-
 export const queryFormatter = (params: { [index: string]: any }) => {
   const newQuery = new URLSearchParams()
   Object.entries(params).forEach(([key, value]) => {
@@ -18,6 +7,5 @@ export const queryFormatter = (params: { [index: string]: any }) => {
 }
 
 export default {
-  dateFormatter,
   queryFormatter,
 }
