ed5935de176b7c181949aa512dac8a5bf55f7bed
classicalliu <classicalliu@gmail.com>
2019-05-09 20:08:59 +0800

    feat: init lockHashes before sync

--- packages/neuron-wallet/src/startup/loopTask/create.ts
+++ packages/neuron-wallet/src/startup/loopTask/create.ts
@@ -2,8 +2,22 @@ import { BrowserWindow } from 'electron'
 import path from 'path'
 import SyncBlocksService from '../../services/syncBlocks'
 import initConnection from '../../typeorm'
+import Address from '../../services/addresses'
+import TransactionsService from '../../services/transactions'
 
 const loadURL = `file://${path.join(__dirname, 'index.html')}`
+
+// maybe should call this every time when new address generated
+// load all addresses and convert to lockHashes
+const loadAddressesAndConvert = async (): Promise<string[]> => {
+  const addresses: string[] = Address.allAddresses().map(addr => addr.address)
+  const lockHashes: string[] = await Promise.all(
+    addresses.map(async addr => {
+      return TransactionsService.addressToLockHash(addr)
+    }),
+  )
+  return lockHashes
+}
 
 // create a background task to sync transactions
 // this task is a renderer process
@@ -24,7 +38,7 @@ const createLoopTask = () => {
 
     // TODO: call this function after get network name
     await initConnection('testnet')
-    const lockHashes: string[] = []
+    const lockHashes: string[] = await loadAddressesAndConvert()
     const syncBlocksService = new SyncBlocksService(lockHashes)
     await syncBlocksService.loopBlocks()
   })
