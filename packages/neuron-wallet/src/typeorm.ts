ee056cc85e98d6d4492539b6ccd9b63fea941e9a
classicalliu <classicalliu@gmail.com>
2019-05-20 13:06:44 +0800

    fix: regenerate init migration

--- packages/neuron-wallet/src/typeorm.ts
+++ packages/neuron-wallet/src/typeorm.ts
@@ -7,7 +7,7 @@ import Input from './entities/Input'
 import Output from './entities/Output'
 import SyncInfo from './entities/SyncInfo'
 
-import { InitMigration1557483560422 } from './migration/1557483560422-InitMigration'
+import { InitMigration1558328532490 } from './migration/1558328532490-InitMigration'
 
 const dbPath = (networkName: string): string => {
   const name = `cell-${networkName}.sqlite`
@@ -19,7 +19,7 @@ const connectOptions = async (networkName: string) => {
   Object.assign(connectionOptions, {
     database: dbPath(networkName),
     entities: [Transaction, Input, Output, SyncInfo],
-    migrations: [InitMigration1557483560422],
+    migrations: [InitMigration1558328532490],
   })
 
   return connectionOptions
