b64caf44458d59175a7880156476947226316ea6
Keith <keithwhisper@gmail.com>
2019-06-05 01:37:56 +0800

    refactor(neuron-wallet): refactor router

--- packages/neuron-wallet/src/router.ts
+++ packages/neuron-wallet/src/router.ts
@@ -1,87 +1,19 @@
 import { ipcMain } from 'electron'
-import { Channel } from './utils/const'
 import controllers from './controllers'
-
-const { AppController, NetworksController, TransactionsController, WalletsController, HelpersController } = controllers
+import logger from './utils/logger'
 
 export default class Router {
-  static start = (methods: string[] = ['app', 'networks', 'wallets', 'transactions', 'helpers']) => {
-    methods.forEach(method => {
-      const descriptor = Object.getOwnPropertyDescriptor(Router, method)
-      if (descriptor) {
-        descriptor.value()
+  constructor(controllerSet: typeof controllers = controllers) {
+    Object.values(controllerSet).forEach(controller => {
+      if (typeof controller === 'function') {
+        // TODO: use metadata to declare channel name
+        const channel = controller.name.toLowerCase().slice(0, -10)
+        ipcMain.on(channel, async (e: Electron.Event, method: keyof typeof controller, ...params: any[]) => {
+          e.sender.send(channel, method, await (controller[method] as Function)(...params))
+        })
+      } else {
+        logger.log({ level: 'error', message: `${controller} is invalid` })
       }
     })
-  }
-
-  /**
-   * @method app
-   * @memberof ChannelListeners
-   * @description listen to Channel.App and invoke corresponding method of appController
-   */
-  public static app = () => {
-    return ipcMain.on(Channel.App, async (e: Electron.Event, method: keyof typeof AppController, ...params: any[]) => {
-      e.sender.send(Channel.App, method, await (AppController[method] as Function)(...params))
-    })
-  }
-
-  /**
-   * @method networks
-   * @memberof ChannelListeners
-   * @description listen to Channel.Networks and invoke corresponding method of networksController
-   */
-  public static networks = () => {
-    return ipcMain.on(
-      Channel.Networks,
-      async (e: Electron.Event, method: keyof typeof NetworksController, ...params: any[]) => {
-        e.sender.send(Channel.Networks, method, await (NetworksController[method] as Function)(...params))
-      },
-    )
-  }
-
-  /**
-   * @method transactions
-   * @memberof ChannelListeners
-   * @description listen to Channel.Transactions and invoke corresponding method of transactionsController
-   */
-  public static transactions = () => {
-    return ipcMain.on(
-      Channel.Transactions,
-      async (e: Electron.Event, method: keyof typeof TransactionsController, ...params: any[]) => {
-        e.sender.send(Channel.Transactions, method, await (TransactionsController[method] as Function)(...params))
-      },
-    )
-  }
-
-  /**
-   * @method wallet
-   * @memberof ChannelListeners
-   * @description listen to Channel.Wallet and invoke corresponding method of WalletsController
-   */
-  public static wallets = () => {
-    return ipcMain.on(
-      Channel.Wallets,
-      async (e: Electron.Event, method: keyof typeof WalletsController, ...params: any[]) => {
-        e.sender.send(Channel.Wallets, method, await (WalletsController[method] as Function)(...params))
-      },
-    )
-  }
-
-  /**
-   * @method helpers
-   * @memberof ChannelListeners
-   * @description provide helper methods to UI layer
-   */
-  public static helpers = () => {
-    return ipcMain.on(
-      Channel.Helpers,
-      async (e: Electron.Event, method: keyof typeof HelpersController, ...params: any[]) => {
-        e.sender.send(Channel.Helpers, method, await (HelpersController[method] as Function)(...params))
-      },
-    )
-  }
-
-  constructor() {
-    Router.start()
   }
 }
