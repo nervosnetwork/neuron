ac8309506b67d11cfd60eefb55a03153284a621b
Keith <keithwhisper@gmail.com>
2019-06-13 13:22:20 +0800

    refactor(neuron-wallet): move connect detection to node service

--- packages/neuron-wallet/src/startup/init-app.ts
+++ packages/neuron-wallet/src/startup/init-app.ts
@@ -1,34 +1,13 @@
-import { distinctUntilChanged } from 'rxjs/operators'
-import windowManage from '../utils/window-manage'
 import Router from '../router'
 import NodeService from '../services/node'
 import NetworksService from '../services/networks'
-import { Channel } from '../utils/const'
-import logger from '../utils/logger'
 import app from '../app'
 import { ActiveNetowrkNotSet } from '../exceptions'
 
 const nodeService = NodeService.getInstance()
 const networksService = NetworksService.getInstance()
 
-const syncConnectStatus = () => {
-  nodeService.tipNumberSubject.pipe(distinctUntilChanged()).subscribe(
-    tipNumber => {
-      windowManage.broadcast(Channel.Networks, 'status', {
-        status: 1,
-        result: typeof tipNumber !== 'undefined',
-      })
-    },
-    err => {
-      logger.log({ level: 'error', message: err.message })
-      syncConnectStatus()
-    },
-  )
-}
-
 const initApp = async () => {
-  // TODO: this function should be moved to somewhere syncing data
-  syncConnectStatus()
   if (!nodeService.core.node.url) {
     const id = await networksService.activeId()
     const network = await networksService.get(id || '')
