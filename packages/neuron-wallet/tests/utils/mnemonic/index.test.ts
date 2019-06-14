37b685da9248fde195ad46750591abb5a263e1ad
Keith <keithwhisper@gmail.com>
2019-06-14 22:07:02 +0800

    feat(neuron-wallet): implement mnemonic module

---
+++ packages/neuron-wallet/tests/utils/mnemonic/index.test.ts
@@ -0,0 +1,25 @@
+import {
+  entropyToMnemonic,
+  mnemonicToEntropy,
+  mnemonicToSeed,
+  mnemonicToSeedSync,
+  validateMnemonic,
+} from '../../../src/utils/mnemonic'
+import fixtures from './fixtures.json'
+
+describe('mnemonic', () => {
+  it('generate, validate mnemonic', () => {
+    fixtures.vectors.map(async ({ entropy, mnemonic }: { entropy: string; mnemonic: string; seed: string }) => {
+      expect(validateMnemonic(mnemonic)).toBe(true)
+      expect(entropyToMnemonic(entropy)).toBe(mnemonic)
+      expect(mnemonicToEntropy(mnemonic)).toBe(entropy)
+    })
+  })
+
+  it('generate seed', () => {
+    fixtures.vectors.map(async ({ mnemonic, seed }: { entropy: string; mnemonic: string; seed: string }) => {
+      expect(await mnemonicToSeed(mnemonic).then(s => s.toString('hex'))).toBe(seed)
+      expect(mnemonicToSeedSync(mnemonic).toString('hex')).toBe(seed)
+    })
+  })
+})
