6c22c624bb82fe275d6cccebf6884cb3aaee4194
Keith <keithwhisper@gmail.com>
2019-06-16 19:56:18 +0800

    feat(neuron-ui): update sidebar to navbar

---
+++ packages/neuron-ui/src/containers/Navbar/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { createPortal } from 'react-dom'
+import { NavLink } from 'react-router-dom'
+import {
+  Upload as IconSend,
+  Download as IconReceive,
+  History as IconHistory,
+  Database as IconAddresses,
+} from 'grommet-icons'
+import { useTranslation } from 'react-i18next'
+
+import { useNeuronWallet } from 'utils/hooks'
+import { Routes } from 'utils/const'
+
+const menuItems = [
+  { name: 'navbar.send', route: Routes.Send, icon: IconSend },
+  { name: 'navbar.receive', route: Routes.Receive, icon: IconReceive },
+  { name: 'navbar.history', route: Routes.History, icon: IconHistory },
+  { name: 'navbar.addresses', route: Routes.Addresses, icon: IconAddresses },
+]
+
+const Sidebar = () => {
+  const {
+    settings: { wallets },
+  } = useNeuronWallet()
+  const [t] = useTranslation()
+
+  return wallets.length ? (
+    <>
+      {menuItems.map(menuItem => (
+        <NavLink
+          key={menuItem.name}
+          to={menuItem.route}
+          isActive={match => {
+            return !!match
+          }}
+          activeStyle={{
+            backgroundColor: '#eee',
+            fontWeight: 'bolder',
+          }}
+        >
+          {<menuItem.icon size="20px" style={{ paddingRight: '5px' }} />}
+          {t(menuItem.name)}
+        </NavLink>
+      ))}
+    </>
+  ) : null
+}
+
+const Container = (props: any) => createPortal(<Sidebar {...props} />, document.querySelector('navbar') as HTMLElement)
+
+export default Container
