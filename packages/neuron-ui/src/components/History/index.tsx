1c81f3e632acf3228f3f731271ed2922fc77f138
Keith <keithwhisper@gmail.com>
2019-06-18 23:57:52 +0800

    refactor(neuron-ui): refactor pagination
    use 'first, prevoius, next, last' instead of list of page number

--- packages/neuron-ui/src/components/History/index.tsx
+++ packages/neuron-ui/src/components/History/index.tsx
@@ -1,23 +1,20 @@
 import React from 'react'
 import { NavLink, RouteComponentProps } from 'react-router-dom'
-import { Container, Row, Col, Alert } from 'react-bootstrap'
+import { Container, Alert } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
 
 import TransactionList from 'components/TransactionList'
-import Pagination from 'widgets/Table/Pagination'
 
 import { ContentProps } from 'containers/MainContent'
 import { Search as SearchIcon } from 'grommet-icons'
 
 import { useNeuronWallet } from 'utils/hooks'
-import { queryFormatter } from 'utils/formatters'
 import { Routes } from 'utils/const'
 
-import { useSearch, useOnChangePage } from './hooks'
+import { useSearch } from './hooks'
 
 const History = ({
-  location: { search, pathname },
-  history,
+  location: { search },
   errorMsgs,
   dispatch,
   providerDispatch,
@@ -30,8 +27,7 @@ const History = ({
   const [t] = useTranslation()
 
   const { keywords, onKeywordsChange } = useSearch(search, dispatch, providerDispatch)
-
-  const onPageChange = useOnChangePage(search, pathname, history, queryFormatter)
+  const totalPages = Math.ceil(totalCount / pageSize) || 1
 
   return (
     <Container>
@@ -44,13 +40,28 @@ const History = ({
         </NavLink>
       </div>
       <TransactionList items={items} />
-      {totalCount > pageSize ? (
-        <Row>
-          <Col>
-            <Pagination currentPage={pageNo - 1} pageSize={pageSize} total={totalCount} onChange={onPageChange} />
-          </Col>
-        </Row>
-      ) : null}
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <NavLink to={`${Routes.History}?pageNo=1`}>{t('history.first')}</NavLink>
+        <NavLink
+          to={`${Routes.History}?pageNo=${pageNo - 1}`}
+          style={{
+            pointerEvents: pageNo - 1 < 1 ? 'none' : 'auto',
+            color: pageNo - 1 < 1 ? 'grey' : 'current',
+          }}
+        >
+          {t('history.previous')}
+        </NavLink>
+        <NavLink
+          to={`${Routes.History}?pageNo=${pageNo + 1}`}
+          style={{
+            pointerEvents: pageNo + 1 > totalPages ? 'none' : 'auto',
+            color: pageNo + 1 > totalPages ? 'grey' : 'current',
+          }}
+        >
+          {t('history.next')}
+        </NavLink>
+        <NavLink to={`${Routes.History}?pageNo=${totalPages}`}>{t('history.last')}</NavLink>
+      </div>
     </Container>
   )
 }
