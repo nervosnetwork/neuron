cf67bd9c8fc3bbff65ee201c2b8d7b0a86e949b8
James Chen <james@ashchan.com>
2019-06-19 02:22:03 +0800

    test: Remove transformIgnorePatterns from jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,6 @@ module.exports = {
   bail: 1,
   verbose: true,
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$"],
   projects: [
     "<rootDir>/packages/neuron-wallet"
   ]
