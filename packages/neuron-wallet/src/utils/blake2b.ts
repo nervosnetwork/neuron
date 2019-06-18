3ba54dc0e3e8419c6ad6b623cfd7d75a231fd55c
classicalliu <classicalliu@gmail.com>
2019-06-18 11:10:06 +0800

    chore: rename b to blake2b

--- packages/neuron-wallet/src/utils/blake2b.ts
+++ packages/neuron-wallet/src/utils/blake2b.ts
@@ -2,21 +2,21 @@ import Core from '@nervosnetwork/ckb-sdk-core'
 import NodeService from '../services/node'
 
 export default class Blake2b {
-  private b: any
+  private blake2b: any
   private core: Core
 
   constructor() {
     this.core = NodeService.getInstance().core
-    this.b = this.core.utils.blake2b(32, null, null, this.core.utils.PERSONAL)
+    this.blake2b = this.core.utils.blake2b(32, null, null, this.core.utils.PERSONAL)
   }
 
   public update = (message: string): void => {
     const msg = this.core.utils.hexToBytes(message.replace(/0x/, ''))
-    this.b.update(msg)
+    this.blake2b.update(msg)
   }
 
   public digest = (): string => {
-    return `0x${this.b.digest('hex')}`
+    return `0x${this.blake2b.digest('hex')}`
   }
 
   public static digest = (message: string): string => {
