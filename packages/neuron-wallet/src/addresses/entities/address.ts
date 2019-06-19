99b2934a008fc5a4e80b74914ffd88d9be254b7b
classicalliu <classicalliu@gmail.com>
2019-06-19 17:18:36 +0800

    feat: init address db

---
+++ packages/neuron-wallet/src/addresses/entities/address.ts
@@ -0,0 +1,38 @@
+import { Entity, BaseEntity, PrimaryGeneratedColumn, Column } from 'typeorm'
+import { AddressType } from '../../keys/address'
+
+@Entity()
+export default class Address extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id!: number
+
+  @Column({
+    type: 'varchar',
+  })
+  walletId!: string
+
+  @Column({
+    type: 'varchar',
+  })
+  address!: string
+
+  @Column({
+    type: 'varchar',
+  })
+  path!: string
+
+  @Column({
+    type: 'int',
+  })
+  addressType!: AddressType
+
+  @Column({
+    type: 'int',
+  })
+  addressIndex!: number
+
+  @Column({
+    type: 'int',
+  })
+  txCount!: number
+}
---
+++ packages/neuron-wallet/src/addresses/ormconfig.ts
@@ -0,0 +1,37 @@
+import { createConnection, getConnection as ormGetConnection } from 'typeorm'
+import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions'
+import path from 'path'
+
+import env from '../env'
+
+import Address from './entities/address'
+
+import { AddAddress1560935561058 } from './migrations/1560935561058-AddAddress'
+
+const dbPath = path.join(env.fileBasePath, 'address.sqlite')
+
+const connectionName = 'address'
+
+const connectOptions = (): SqliteConnectionOptions => {
+  return {
+    name: connectionName,
+    type: 'sqlite',
+    database: dbPath,
+    entities: [Address],
+    migrations: [AddAddress1560935561058],
+    synchronize: false,
+    migrationsRun: true,
+    logging: true,
+  }
+}
+
+export const initConnection = async () => {
+  const connectionOptions = connectOptions()
+  await createConnection(connectionOptions)
+}
+
+export const getConnection = () => {
+  return ormGetConnection(connectionName)
+}
+
+export default initConnection
