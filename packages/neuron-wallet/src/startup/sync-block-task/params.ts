7e96972f781a35650ccca4172e8f48445d2aa0b8
classicalliu <classicalliu@gmail.com>
2019-06-26 20:06:50 +0800

    feat: update lockHashes when new address generated

--- packages/neuron-wallet/src/startup/sync-block-task/params.ts
+++ packages/neuron-wallet/src/startup/sync-block-task/params.ts
@@ -1,10 +1,12 @@
 import NodeService from '../../services/node'
 import { AddressesUsedSubject } from '../../models/subjects/addresses-used-subject'
+import AddressDbChangedSubject from '../../models/subjects/address-db-changed-subject'
 
 export { networkSwitchSubject } from '../../services/networks'
 
-export { addressChangeSubject, genesisBlockHash } from './create'
+export { genesisBlockHash } from './create'
 export { databaseInitSubject } from './create'
 
 export const nodeService = NodeService.getInstance()
 export const addressesUsedSubject = AddressesUsedSubject.getSubject()
+export const addressDbChangedSubject = AddressDbChangedSubject.getSubject()
