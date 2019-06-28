1588efd82955d6e9869b5e6e59c716d36d182077
Keith <keithwhisper@gmail.com>
2019-06-28 14:53:25 +0800

    refactor(neuron-wallet): remove slash before the transaction detail url

--- packages/neuron-wallet/src/controllers/app/index.ts
+++ packages/neuron-wallet/src/controllers/app/index.ts
@@ -202,7 +202,7 @@ export default class AppController {
         preload: path.join(__dirname, '../../startup/preload.js'),
       },
     })
-    win.loadURL(`${env.mainURL}/#/transaction/${hash}`)
+    win.loadURL(`${env.mainURL}#/transaction/${hash}`)
     win.on('ready-to-show', () => {
       win.show()
       win.focus()
