b7873c9e67fba45ae0f679edfcfabb5fff77a3bc
classicalliu <classicalliu@gmail.com>
2019-07-01 11:57:45 +0800

    feat: using previous database if no network

--- packages/neuron-wallet/src/startup/sync-block-task/init-database.ts
+++ packages/neuron-wallet/src/startup/sync-block-task/init-database.ts
@@ -1,14 +1,24 @@
 import initConnection from '../../database/chain/ormconfig'
 import Utils from '../../services/sync/utils'
 import genesisBlockHash from './genesis'
+import { updateMetaInfo, getMetaInfo } from '../../database/chain/meta-info'
+import LockUtils from '../../utils/lock-utils'
 
 export const initDatabase = async () => {
   try {
     const hash = await genesisBlockHash()
     await initConnection(hash)
+    const systemScriptInfo = await LockUtils.systemScript()
+    updateMetaInfo({ genesisBlockHash: hash, systemScriptInfo })
   } catch (err) {
-    Utils.sleep(1000)
-    await initDatabase()
+    try {
+      const metaInfo = getMetaInfo()
+      await initConnection(metaInfo.genesisBlockHash)
+      LockUtils.setSystemScript(metaInfo.systemScriptInfo)
+    } catch (error) {
+      Utils.sleep(1000)
+      await initDatabase()
+    }
   }
 }
 
