69c9173b4deeb84a5494ce1c7cce97805567d80f
classicalliu <classicalliu@gmail.com>
2019-07-08 19:22:13 +0800

    chore: rename to `TransactionChangedMessage`

--- packages/neuron-wallet/src/models/subjects/tx-db-changed-subject.ts
+++ packages/neuron-wallet/src/models/subjects/tx-db-changed-subject.ts
@@ -1,20 +1,20 @@
 import { ReplaySubject } from 'rxjs'
 import { Transaction } from '../../types/cell-types'
 
-export interface EventAndTx {
+export interface TransactionChangedMessage {
   event: string
   tx: Transaction
 }
 
 // subscribe this Subject to monitor any transaction table changes
 export class TxDbChangedSubject {
-  static subject = new ReplaySubject<EventAndTx>(100)
+  static subject = new ReplaySubject<TransactionChangedMessage>(100)
 
   static getSubject() {
     return this.subject
   }
 
-  static setSubject(subject: ReplaySubject<EventAndTx>) {
+  static setSubject(subject: ReplaySubject<TransactionChangedMessage>) {
     this.subject = subject
   }
 }
