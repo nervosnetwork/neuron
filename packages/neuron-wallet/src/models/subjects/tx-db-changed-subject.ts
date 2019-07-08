d76a994d43e6601f0498c59e32fa4b60fab980ad
classicalliu <classicalliu@gmail.com>
2019-07-08 18:03:52 +0800

    fix: fix tx db changed subject and add tx in event

--- packages/neuron-wallet/src/models/subjects/tx-db-changed-subject.ts
+++ packages/neuron-wallet/src/models/subjects/tx-db-changed-subject.ts
@@ -1,14 +1,20 @@
 import { ReplaySubject } from 'rxjs'
+import { Transaction } from '../../types/cell-types'
+
+export interface EventAndTx {
+  event: string
+  tx: Transaction
+}
 
 // subscribe this Subject to monitor any transaction table changes
 export class TxDbChangedSubject {
-  static subject = new ReplaySubject<string>(100)
+  static subject = new ReplaySubject<EventAndTx>(100)
 
   static getSubject() {
     return this.subject
   }
 
-  static setSubject(subject: ReplaySubject<string>) {
+  static setSubject(subject: ReplaySubject<EventAndTx>) {
     this.subject = subject
   }
 }
