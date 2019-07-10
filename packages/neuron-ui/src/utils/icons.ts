03a5334797ea5600bff22803180b6f374fdd741b
James Chen <james@ashchan.com>
2019-07-10 09:23:48 +0900

    feat: Support registering grommet icons to be used as fabric icons

---
+++ packages/neuron-ui/src/utils/icons.ts
@@ -0,0 +1,6 @@
+import { registerIcons } from '@uifabric/styling'
+
+// Fabric button's default icon height doesn't fit Grommet Icons
+export const buttonGrommetIconStyles = { root: { height: '24px' } }
+
+export { registerIcons }
