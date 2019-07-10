4cc659573bb7e410a3bed69deff9372349f4750e
Keith <keithwhisper@gmail.com>
2019-07-10 16:25:40 +0800

    feat(neuron-ui): add storybook for tests of neuron-ui

--- packages/neuron-ui/src/components/TransactionList/index.tsx
+++ packages/neuron-ui/src/components/TransactionList/index.tsx
@@ -54,79 +54,90 @@ const TransactionList = ({
   )
 
   const transactionColumns: IColumn[] = useMemo(
-    (): IColumn[] => [
-      { name: t('history.type'), key: 'type', fieldName: 'type', minWidth: MIN_CELL_WIDTH, maxWidth: 150 },
-      {
-        name: t('history.timestamp'),
-        key: 'timestamp',
-        fieldName: 'timestamp',
-        minWidth: MIN_CELL_WIDTH,
-        maxWidth: 150,
-        onRender: (item?: FormatTransaction) => {
-          return item ? <span>{new Date(+(item.timestamp || item.createdAt)).toLocaleTimeString()}</span> : null
+    (): IColumn[] =>
+      [
+        { name: t('history.type'), key: 'type', fieldName: 'type', minWidth: MIN_CELL_WIDTH, maxWidth: 150 },
+        {
+          name: t('history.timestamp'),
+          key: 'timestamp',
+          fieldName: 'timestamp',
+          minWidth: MIN_CELL_WIDTH,
+          maxWidth: 150,
+          onRender: (item?: FormatTransaction) => {
+            return item ? <span>{new Date(+(item.timestamp || item.createdAt)).toLocaleTimeString()}</span> : null
+          },
         },
-      },
-      { name: t('history.transaction-hash'), key: 'hash', fieldName: 'hash', minWidth: MIN_CELL_WIDTH, maxWidth: 200 },
-      { name: t('history.status'), key: 'status', fieldName: 'status', minWidth: MIN_CELL_WIDTH, maxWidth: 50 },
-      {
-        name: t('history.description'),
-        key: 'description',
-        fieldName: 'description',
-        minWidth: MIN_CELL_WIDTH,
-        maxWidth: 200,
-        onRender: (item?: FormatTransaction, idx?: number) => {
-          return item && undefined !== idx ? (
-            <TextField
-              title={item.description}
-              value={localDescription[idx] || ''}
-              onKeyPress={onDescriptionPress(idx)}
-              onBlur={onDescriptionFieldBlur(idx)}
-              onChange={onDescriptionChange(idx)}
-              borderless
-              styles={(props: ITextFieldStyleProps) => {
-                return {
-                  fieldGroup: {
-                    borderColor: '#ccc',
-                    border: props.focused ? '1px solid' : 'none',
-                  },
-                }
-              }}
-            />
-          ) : null
+        {
+          name: t('history.transaction-hash'),
+          key: 'hash',
+          fieldName: 'hash',
+          minWidth: 300,
+          maxWidth: 450,
         },
-      },
-      { name: t('history.amount'), key: 'value', fieldName: 'value', minWidth: MIN_CELL_WIDTH, maxWidth: 300 },
-    ],
+        { name: t('history.status'), key: 'status', fieldName: 'status', minWidth: MIN_CELL_WIDTH, maxWidth: 50 },
+        {
+          name: t('history.description'),
+          key: 'description',
+          fieldName: 'description',
+          minWidth: MIN_CELL_WIDTH,
+          maxWidth: 200,
+          onRender: (item?: FormatTransaction, idx?: number) => {
+            return item && undefined !== idx ? (
+              <TextField
+                title={item.description}
+                value={localDescription[idx] || ''}
+                onKeyPress={onDescriptionPress(idx)}
+                onBlur={onDescriptionFieldBlur(idx)}
+                onChange={onDescriptionChange(idx)}
+                borderless
+                styles={(props: ITextFieldStyleProps) => {
+                  return {
+                    fieldGroup: {
+                      borderColor: '#ccc',
+                      border: props.focused ? '1px solid' : 'none',
+                    },
+                  }
+                }}
+              />
+            ) : null
+          },
+        },
+        { name: t('history.amount'), key: 'value', fieldName: 'value', minWidth: MIN_CELL_WIDTH, maxWidth: 300 },
+      ].map(
+        (col): IColumn => ({ fieldName: col.key, ariaLabel: col.name, isResizable: true, isCollapsable: false, ...col })
+      ),
     [localDescription, onDescriptionChange, onDescriptionFieldBlur, onDescriptionPress, t]
   )
 
   const { groups, txs } = useMemo(() => {
-    const gs: IGroup[] = [
+    const groupItems: IGroup[] = [
       {
         key: 'pending',
-        name: 'pending',
+        name: 'Pending',
         startIndex: 0,
         count: 0,
       },
     ]
-    const ts = items.map(item => {
+    const txItems = items.map(item => {
+      const date = timeFormatter.format(+(item.timestamp || item.createdAt))
       if (item.status === 'pending') {
-        gs[0].count++
+        groupItems[0].count++
+        return { ...item, date }
       }
-      const date = timeFormatter.format(+item.timestamp)
-      if (date !== gs[gs.length - 1].key) {
-        gs.push({
+
+      if (date !== groupItems[groupItems.length - 1].key) {
+        groupItems.push({
           key: date,
           name: date,
-          startIndex: gs[gs.length - 1].count + gs[gs.length - 1].startIndex,
+          startIndex: groupItems[groupItems.length - 1].count + groupItems[groupItems.length - 1].startIndex,
           count: 1,
         })
       } else {
-        gs[gs.length - 1].count++
+        groupItems[groupItems.length - 1].count++
       }
       return { ...item, date }
     })
-    return { groups: gs, txs: ts }
+    return { groups: groupItems, txs: txItems }
   }, [items])
 
   return (
---
+++ packages/neuron-ui/src/stories/TransactionList.stories.tsx
@@ -0,0 +1,104 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import TransactionList from 'components/TransactionList'
+
+const states: {
+  [index: string]: {
+    title: string
+    list: State.Transaction[]
+  }
+} = {
+  emptyList: {
+    title: 'Empty List',
+    list: [],
+  },
+  contentList: {
+    title: 'Content List',
+    list: [
+      {
+        type: 'send',
+        createdAt: (Date.now() - 100000).toString(),
+        updatedAt: '',
+        timestamp: '',
+        value: '10000',
+        hash: '0x70abeeaa2ed08b7d7659341a122b9a2f2ede99bb6bd0df7398d7ffe488beab61',
+        description: 'description of sending transaction',
+        status: 'pending',
+      },
+      {
+        type: 'receive',
+        createdAt: (Date.now() - 200000).toString(),
+        updatedAt: '',
+        timestamp: '',
+        value: '10000',
+        hash: '0x70abeeaa2ed08b7d7659341a122b9a2f2ede99bb6bd0df7398d7ffe488beab62',
+        description: 'description of receiving transaction',
+        status: 'pending',
+      },
+      {
+        type: 'send',
+        createdAt: '',
+        updatedAt: '',
+        timestamp: (Date.now() - 300000).toString(),
+        value: '10000',
+        hash: '0x70abeeaa2ed08b7d7659341a122b9a2f2ede99bb6bd0df7398d7ffe488beab63',
+        description: 'description of sending transaction',
+        status: 'success',
+      },
+      {
+        type: 'receive',
+        createdAt: '',
+        updatedAt: '',
+        timestamp: (Date.now() - 400000).toString(),
+        value: '10000',
+        hash: '0x70abeeaa2ed08b7d7659341a122b9a2f2ede99bb6bd0df7398d7ffe488beab64',
+        description: 'description of receiving transaction',
+        status: 'success',
+      },
+      {
+        type: 'send',
+        createdAt: '',
+        updatedAt: '',
+        timestamp: (Date.now() - 500000).toString(),
+        value: '10000',
+        hash: '0x70abeeaa2ed08b7d7659341a122b9a2f2ede99bb6bd0df7398d7ffe488beab65',
+        description: 'description of sending transaction',
+        status: 'failed',
+      },
+      {
+        type: 'receive',
+        createdAt: '',
+        updatedAt: '',
+        timestamp: (Date.now() - 600000).toString(),
+        value: '10000',
+        hash: '0x70abeeaa2ed08b7d7659341a122b9a2f2ede99bb6bd0df7398d7ffe488beab66',
+        description: 'description of receiving transaction',
+        status: 'failed',
+      },
+      {
+        type: 'send',
+        createdAt: '',
+        updatedAt: '',
+        timestamp: new Date('2019-05-18').getTime().toString(),
+        value: '10000',
+        hash: '0x70abeeaa2ed08b7d7659341a122b9a2f2ede99bb6bd0df7398d7ffe488beab67',
+        description: 'description of sending transaction',
+        status: 'failed',
+      },
+      {
+        type: 'receive',
+        createdAt: '',
+        updatedAt: '',
+        timestamp: new Date('2019-05-18').getTime().toString(),
+        value: '10000',
+        hash: '0x70abeeaa2ed08b7d7659341a122b9a2f2ede99bb6bd0df7398d7ffe488beab68',
+        description: 'description of receiving transaction',
+        status: 'failed',
+      },
+    ],
+  },
+}
+const story = storiesOf('TransactionList', module)
+Object.keys(states).forEach(key => {
+  story.add(states[key].title, () => <TransactionList walletID="1" items={states[key].list} dispatch={() => {}} />)
+})
