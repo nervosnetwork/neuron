e1f67f71d4471f665d6759a0fada0c132e4a436f
James Chen <james@ashchan.com>
2019-07-11 09:19:53 +0900

    feat: Override Fabric theme to make small font size bigger
    DetailsList and other compoments use this size thus would render larger.

--- packages/neuron-ui/src/index.tsx
+++ packages/neuron-ui/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { HashRouter as Router, Route } from 'react-router-dom'
+import { loadTheme } from 'office-ui-fabric-react'
+
 import 'styles/index.scss'
 import 'utils/i18n'
 import * as serviceWorker from 'serviceWorker'
@@ -10,6 +12,12 @@ import Notification from 'containers/Notification'
 import Main from 'containers/Main'
 import Footer from 'containers/Footer'
 import withProviders from 'states/stateProvider'
+
+loadTheme({
+  fonts: {
+    small: { fontSize: '14px' },
+  },
+})
 
 export const containers: CustomRouter.Route[] = [
   {
