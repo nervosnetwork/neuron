3308ba57f4cc50aa1c6d7ae2eb22c8ebe8cdd9c7
James Chen <james@ashchan.com>
2019-07-11 09:55:49 +0900

    feat: Override all theme font sizes

--- packages/neuron-ui/src/index.tsx
+++ packages/neuron-ui/src/index.tsx
@@ -15,7 +15,17 @@ import withProviders from 'states/stateProvider'
 
 loadTheme({
   fonts: {
+    tiny: { fontSize: '11px' },
+    xSmall: { fontSize: '12px' },
     small: { fontSize: '14px' },
+    smallPlus: { fontSize: '15px' },
+    medium: { fontSize: '16px' },
+    mediumPlus: { fontSize: '17px' },
+    large: { fontSize: '18px' },
+    xLarge: { fontSize: '22px' },
+    xxLarge: { fontSize: '28px' },
+    superLarge: { fontSize: '42px' },
+    mega: { fontSize: '72px' },
   },
 })
 
