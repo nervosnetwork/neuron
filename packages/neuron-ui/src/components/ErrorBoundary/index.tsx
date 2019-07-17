d5e93f7fbefa9e65e9894b99a27b0c7d6e8d8938
Keith <keithwhisper@gmail.com>
2019-07-17 12:42:27 +0800

    feat(neuron-ui): add ErrorBoundary to handle errors happens in lifecycle

---
+++ packages/neuron-ui/src/components/ErrorBoundary/index.tsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { appCalls } from 'services/UILayer'
+import { Stack, Spinner } from 'office-ui-fabric-react'
+
+const handleError = (error: Error) => {
+  appCalls.handleViewError(error.toString())
+  setTimeout(() => {
+    window.location.reload()
+  }, 0)
+  return { hasError: true }
+}
+
+class ErrorBoundary extends Component<{ children: React.ReactChild }, { hasError: boolean }> {
+  state = {
+    hasError: false,
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    return handleError(error)
+  }
+
+  public componentDidCatch(error: Error) {
+    this.setState(handleError(error))
+  }
+
+  render() {
+    const { hasError } = this.state
+    const { children } = this.props
+    return hasError ? (
+      <Stack verticalFill verticalAlign="center">
+        <Spinner />
+      </Stack>
+    ) : (
+      children
+    )
+  }
+}
+
+export default ErrorBoundary
