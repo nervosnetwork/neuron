29372db245748e4c27c78b4f4f3f87d439c951c3
Keith <keithwhisper@gmail.com>
2019-08-01 16:07:57 +0800

    feat(neuron-ui): add dynamic prmopt in wallet wizard

--- packages/neuron-ui/src/utils/validators.ts
+++ packages/neuron-ui/src/utils/validators.ts
@@ -4,30 +4,46 @@ export const verifyAddress = (address: string): boolean => {
   // TODO: verify address, prd required
   return address.length === ADDRESS_LENGTH
 }
-export const verifyWalletSubmission = ({
-  password,
-  confirmPassword,
-  name,
-}: {
-  password: string
-  confirmPassword: string
-  name: string
-}) => {
-  return (
-    password &&
-    name &&
-    password === confirmPassword &&
-    password.length >= MIN_PASSWORD_LENGTH &&
-    password.length <= MAX_PASSWORD_LENGTH
-  )
-}
 
 export const verifyAmountRange = (amount: string) => {
   return +amount >= MIN_AMOUNT
 }
 
+export const verifyPasswordComplexity = (password: string) => {
+  if (!password) {
+    return 'password-is-empty'
+  }
+  if (password.length < MIN_PASSWORD_LENGTH) {
+    return 'password-is-too-short'
+  }
+  if (password.length > MAX_PASSWORD_LENGTH) {
+    return 'password-is-too-long'
+  }
+  let complex = 0
+  let reg = /\d/
+  if (reg.test(password)) {
+    complex++
+  }
+  reg = /[a-z]/
+  if (reg.test(password)) {
+    complex++
+  }
+  reg = /[A-Z]/
+  if (reg.test(password)) {
+    complex++
+  }
+  reg = /[^0-9a-zA-Z]/
+  if (reg.test(password)) {
+    complex++
+  }
+  if (complex < 3) {
+    return 'password-is-too-simple'
+  }
+  return true
+}
+
 export default {
   verifyAddress,
-  verifyWalletSubmission,
   verifyAmountRange,
+  verifyPasswordComplexity,
 }
