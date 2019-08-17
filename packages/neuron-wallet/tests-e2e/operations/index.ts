20d833f10fc74bbabc5eb2ad594e58afef7d5e6c
cezres <cezr@sina.com>
2019-08-18 01:20:16 +0800

    chore(e2e): Add operation of get current wallet address

--- packages/neuron-wallet/tests-e2e/operations/index.ts
+++ packages/neuron-wallet/tests-e2e/operations/index.ts
@@ -1,9 +1,11 @@
 import { createWallet } from './createWallet'
 import { importWallet } from './importWallet'
 import { searchWalletIndex } from './searchWalletIndex'
+import { currentWalletAddress } from './currentWalletAddress'
 
 export {
   createWallet,
   importWallet,
   searchWalletIndex,
+  currentWalletAddress,
 }
