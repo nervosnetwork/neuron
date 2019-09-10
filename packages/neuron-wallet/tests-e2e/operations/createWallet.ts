8fc9c1454102c5db4eba76f141f1d1682ab8b61f
Keith <keithwhisper@gmail.com>
2019-09-10 19:00:49 +0800

    test: add e2e tests of input boundary validation on the Send View

--- packages/neuron-wallet/tests-e2e/operations/createWallet.ts
+++ packages/neuron-wallet/tests-e2e/operations/createWallet.ts
@@ -1,21 +1,23 @@
 import Application from '../application'
 import { importWallet } from './importWallet'
+import { debuglog } from 'util'
+const log = debuglog(__filename)
 
 export const createWallet = async (app: Application, name: string | undefined = undefined, password: string = 'Azusa2233') => {
   const { client } = app.spectron
-  
+
   // Copy mnemonic
   const mnemonicTextarea = await app.element('<textarea />')
   const mnemonic = await client.elementIdText(mnemonicTextarea.value.ELEMENT)
   let mnemonicText = mnemonic.value
-  console.log(`mnemonicText = ${mnemonicText} ${new Date().toTimeString()}`);
-  
+  log(`mnemonicText = ${mnemonicText} ${new Date().toTimeString()}`);
+
   // Next
   const mnemonicNextButton = await app.getElementByTagName('button', 'Next')
   expect(mnemonicNextButton).not.toBeNull()
   await client.elementIdClick(mnemonicNextButton!.ELEMENT)
   await app.waitUntilLoaded()
-  console.log(`copy mnemonic finish ${new Date().toTimeString()}`);
+  log(`copy mnemonic finish ${new Date().toTimeString()}`);
 
   return await importWallet(app, mnemonicText, name, password)
 }
--- packages/neuron-wallet/tests-e2e/tests/index.ts
+++ packages/neuron-wallet/tests-e2e/tests/index.ts
@@ -1,9 +1,11 @@
 import Wallet from './wallet'
 import Network from './network'
 import Transaction from './transaction'
+import SendTransaction from './sendTransaction'
 
 export default {
   Wallet,
   Network,
   Transaction,
+  SendTransaction,
 }
