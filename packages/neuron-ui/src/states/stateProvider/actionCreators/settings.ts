8b89b1efe1fdf72b9b72a8cf47c45cbf543f7b92
Keith <keithwhisper@gmail.com>
2019-10-15 10:05:00 +0800

    feat(neuron-ui): add loading on creating networks

--- packages/neuron-ui/src/states/stateProvider/actionCreators/settings.ts
+++ packages/neuron-ui/src/states/stateProvider/actionCreators/settings.ts
@@ -15,14 +15,29 @@ export const toggleAddressBook = () => {
 }
 
 export const createNetwork = (params: Controller.CreateNetworkParams) => (dispatch: StateDispatch, history: any) => {
-  createRemoteNetwork(params).then(res => {
-    if (res.status === 1) {
-      addPopup('create-network-successfully')(dispatch)
-      history.push(Routes.SettingsNetworks)
-    } else {
-      addNotification(failureResToNotification(res))(dispatch)
-    }
+  dispatch({
+    type: AppActions.UpdateLoadings,
+    payload: {
+      network: true,
+    },
   })
+  createRemoteNetwork(params)
+    .then(res => {
+      if (res.status === 1) {
+        addPopup('create-network-successfully')(dispatch)
+        history.push(Routes.SettingsNetworks)
+      } else {
+        addNotification(failureResToNotification(res))(dispatch)
+      }
+    })
+    .finally(() => {
+      dispatch({
+        type: AppActions.UpdateLoadings,
+        payload: {
+          network: false,
+        },
+      })
+    })
 }
 
 export const updateNetwork = (params: Controller.UpdateNetworkParams) => (dispatch: StateDispatch, history: any) => {
