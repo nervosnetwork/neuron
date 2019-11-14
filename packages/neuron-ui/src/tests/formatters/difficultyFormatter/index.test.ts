23fe3b118cd25a397c40bd45377e7ee13ebbfea2
Keith <keithwhisper@gmail.com>
2019-11-14 15:24:20 +0800

    test(neuron-ui): fix the tests of difficulty formatter

--- packages/neuron-ui/src/tests/formatters/difficultyFormatter/index.test.ts
+++ packages/neuron-ui/src/tests/formatters/difficultyFormatter/index.test.ts
@@ -3,7 +3,7 @@ import fixtures from './fixtures.json'
 
 describe('test difficulty formatter', () => {
   test.each(fixtures)(`%s => %s`, ({ difficulty, expected }) => {
-    const str = difficultyFormatter(difficulty)
+    const str = difficultyFormatter(BigInt(difficulty))
     expect(str).toBe(expected)
   })
 })
