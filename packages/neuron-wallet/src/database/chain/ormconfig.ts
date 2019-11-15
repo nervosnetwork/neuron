a078e76b3aefce7c96c8927676ed2a1f20747e89
James Chen <james@ashchan.com>
2019-11-15 17:32:20 +0800

    chore: Bump to v0.25.0

--- packages/neuron-wallet/src/database/chain/ormconfig.ts
+++ packages/neuron-wallet/src/database/chain/ormconfig.ts
@@ -31,8 +31,8 @@ const connectOptions = async (genesisBlockHash: string): Promise<SqliteConnectio
   const connectionOptions = await getConnectionOptions()
   const database = env.isTestMode ? ':memory:' : dbPath(genesisBlockHash)
 
-  const logging: boolean | ('query' | 'schema' | 'error' | 'warn' | 'info' | 'log' | 'migration')[] =
-    (env.isDevMode) ? ['warn', 'error', 'log', 'info', 'schema', 'migration'] : ['warn', 'error']
+  const logging: boolean | ('query' | 'schema' | 'error' | 'warn' | 'info' | 'log' | 'migration')[] = ['warn', 'error']
+    // (env.isDevMode) ? ['warn', 'error', 'log', 'info', 'schema', 'migration'] : ['warn', 'error']
 
   return {
     ...connectionOptions,
