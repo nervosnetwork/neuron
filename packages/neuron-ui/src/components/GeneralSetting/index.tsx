38aa4c4a334d44a4a8fc9312f7d6b5fc4a84d064
James Chen <james@ashchan.com>
2019-11-17 20:27:34 +0800

    feat: Add description for clear cache feature

--- packages/neuron-ui/src/components/GeneralSetting/index.tsx
+++ packages/neuron-ui/src/components/GeneralSetting/index.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Stack, PrimaryButton, Spinner } from 'office-ui-fabric-react'
+import { Stack, PrimaryButton, Spinner, Text } from 'office-ui-fabric-react'
 import { StateWithDispatch } from 'states/stateProvider/reducer'
 import { addPopup } from 'states/stateProvider/actionCreators'
 import { clearCellCache } from 'services/remote'
@@ -20,19 +20,24 @@ const GeneralSetting = ({ dispatch }: React.PropsWithoutRef<StateWithDispatch>) 
   }, [dispatch])
 
   return (
-    <Stack tokens={{ childrenGap: 15 }} horizontal horizontalAlign="start">
-      <PrimaryButton
-        onClick={clearCache}
-        disabled={clearing}
-        ariaDescription="Create new network configuration"
-        styles={{
-          root: {
-            minWidth: 150,
-          },
-        }}
-      >
-        {clearing ? <Spinner /> : t('settings.general.clear-cache')}
-      </PrimaryButton>
+    <Stack tokens={{ childrenGap: 15 }}>
+      <Stack horizontal horizontalAlign="start">
+        <PrimaryButton
+          onClick={clearCache}
+          disabled={clearing}
+          ariaDescription="Create new network configuration"
+          styles={{
+            root: {
+              minWidth: 150,
+            },
+          }}
+        >
+          {clearing ? <Spinner /> : t('settings.general.clear-cache')}
+        </PrimaryButton>
+      </Stack>
+      <Text as="p" variant="medium">
+        {t('settings.general.clear-cache-description')}
+      </Text>
     </Stack>
   )
 }
