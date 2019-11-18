56d4a34e4fe21d4e4f2f171368848fe992958de6
James Chen <james@ashchan.com>
2019-11-18 15:03:56 +0800

    refactor: Delete sqlite by clearing tables instead of erasing files

--- packages/neuron-wallet/src/database/chain/cleaner.ts
+++ packages/neuron-wallet/src/database/chain/cleaner.ts
@@ -1,21 +1,14 @@
-import fs from 'fs'
-import path from 'path'
-import env from 'env'
+import { getConnection } from 'typeorm'
+import InputEntity from './entities/input'
+import OutputEntity from './entities/output'
+import TransactionEntity from './entities/transaction'
+import SyncInfoEntity from './entities/sync-info'
 
 // Clean local sqlite storage
 export default class ChainCleaner {
-  // Delete all sqlite files under cells folder.
-  // It doesn't handle error or throw exception when deleting fails.
-  public static clean() {
-    const folder = path.join(env.fileBasePath, 'cells')
-    fs.readdir(folder, (err, files) => {
-      if (err) {
-        return
-      }
-
-      for (const file of files.filter(f => { return path.extname(f) === '.sqlite' })) {
-        fs.unlink(path.join(folder, file), () => {})
-      }
-    })
+  public static async clean() {
+    for (const entity of [InputEntity, OutputEntity, TransactionEntity, SyncInfoEntity]) {
+      await getConnection().getRepository(entity).clear()
+    }
   }
 }
