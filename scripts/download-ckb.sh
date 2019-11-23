e9653c881e019e44c9a75026311186c78ae48337
James Chen <james@ashchan.com>
2019-11-23 14:37:43 +0900

    feat: Add a shell script for downloading ckb binaries
    Note: CKB linux binary is more than 400M.

---
+++ scripts/download-ckb.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+CKB_VERSION=$1
+ROOT_DIR=$(pwd)
+
+# macOS
+CKB_FILENAME="ckb_${CKB_VERSION}_x86_64-apple-darwin"
+cd $ROOT_DIR/packages/neuron-wallet/bin/mac
+
+curl -O -L "https://github.com/nervosnetwork/ckb/releases/download/${CKB_VERSION}/${CKB_FILENAME}.zip"
+unzip ${CKB_FILENAME}.zip
+cp ${CKB_FILENAME}/ckb ./
+rm -rf $CKB_FILENAME
+rm ${CKB_FILENAME}.zip
+
+# Linux
+CKB_FILENAME="ckb_${CKB_VERSION}_x86_64-unknown-linux-gnu"
+cd $ROOT_DIR/packages/neuron-wallet/bin/linux
+
+curl -O -L "https://github.com/nervosnetwork/ckb/releases/download/${CKB_VERSION}/${CKB_FILENAME}.tar.gz"
+tar xvzf ${CKB_FILENAME}.tar.gz
+cp ${CKB_FILENAME}/ckb ./
+rm -rf $CKB_FILENAME
+rm ${CKB_FILENAME}.tar.gz
+
+# Windows
+CKB_FILENAME="ckb_${CKB_VERSION}_x86_64-pc-windows-msvc"
+cd $ROOT_DIR/packages/neuron-wallet/bin/win
+
+curl -O -L "https://github.com/nervosnetwork/ckb/releases/download/${CKB_VERSION}/${CKB_FILENAME}.zip"
+unzip ${CKB_FILENAME}.zip
+cp ${CKB_FILENAME}/ckb.exe ./
+rm -rf $CKB_FILENAME
+rm ${CKB_FILENAME}.zip
