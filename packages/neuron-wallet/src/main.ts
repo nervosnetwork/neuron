1d2d6541339154a803e5d57b1126189463a1ea7a
James Chen <james@ashchan.com>
2019-11-26 15:56:06 +0900

    feat: Do not start block sync and ckb node background processes for test environment

--- packages/neuron-wallet/src/main.ts
+++ packages/neuron-wallet/src/main.ts
@@ -5,6 +5,7 @@ import NodeController from 'controllers/node'
 import SyncController from 'controllers/sync'
 import AppController from 'controllers/app'
 import { changeLanguage } from 'utils/i18n'
+import env from 'env'
 
 const appController = new AppController()
 
@@ -12,8 +13,10 @@ app.on('ready', async () => {
   changeLanguage(app.getLocale())
 
   WalletService.getInstance().generateAddressesIfNecessary()
-  NodeController.startNode()
-  SyncController.startSyncing()
+  if (!env.isTestMode) {
+    NodeController.startNode()
+    SyncController.startSyncing()
+  }
 
   appController.openWindow()
 })
