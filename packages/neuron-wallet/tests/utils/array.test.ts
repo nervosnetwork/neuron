634061ba14c8324e3e2997fb00da116ee64ac74f
classicalliu <classicalliu@gmail.com>
2019-12-10 15:36:42 +0800

    Add test cases for ArrayUtils

--- packages/neuron-wallet/tests/utils/array.test.ts
+++ packages/neuron-wallet/tests/utils/array.test.ts
@@ -21,4 +21,25 @@ describe('Key tests', () => {
       expect(minValue).toEqual(BigInt(4058))
     })
   })
+
+  it('eachSlice', () => {
+    const arr: number[] = [1, 2, 3, 4, 5, 6]
+    const result = ArrayUtils.eachSlice(arr, 4)
+    expect(result).toEqual([[1, 2, 3, 4], [5, 6]])
+  })
+
+  it('range', () => {
+    const result: string[] = ArrayUtils.range('1', '5')
+    expect(result).toEqual(['1', '2', '3', '4', '5'])
+  })
+
+  it('rangeForBigInt', () => {
+    const result: bigint[] = ArrayUtils.rangeForBigInt(BigInt(1), BigInt(3))
+    expect(result).toEqual([BigInt(1), BigInt(2), BigInt(3)])
+  })
+
+  it('mapSeries', async () => {
+    const result = await ArrayUtils.mapSeries([1, 2, 3], async (num: number) => num + 1)
+    expect(result).toEqual([2, 3, 4])
+  })
 })
