72619924fa4c4e09d3fe4b98ec1457a550ee1f77
James Chen <james@ashchan.com>
2019-12-12 09:48:32 +0900

    Try creating db folder before opening

--- packages/neuron-wallet/src/database/leveldb.ts
+++ packages/neuron-wallet/src/database/leveldb.ts
@@ -1,13 +1,17 @@
+import fs from 'fs'
+import path from 'path'
 import levelup, { LevelUp } from 'levelup'
 import leveldown from 'leveldown'
 import sub from 'subleveldown'
-import path from 'path'
 import env from 'env'
 
 // Create a database. If prefix is provided the result will be a sublevel db
 // with its own keyspace.
 const leveldb = (dbname: string, prefix: string | null = null): LevelUp => {
-  const db = levelup(leveldown(path.join(env.fileBasePath, dbname)))
+  const dbpath = path.join(env.fileBasePath, dbname)
+  fs.mkdirSync(dbpath, { recursive: true })
+
+  const db = levelup(leveldown(dbpath))
   if (prefix) {
     return sub(db, prefix, { valueEncoding: 'json' })
   }
