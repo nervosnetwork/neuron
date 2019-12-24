47679a974bf36c6335c4e517c09e63893de24436
classicalliu <classicalliu@gmail.com>
2019-12-24 14:04:42 +0800

    Fix AddressCreatedSubject called in renderer process

--- packages/neuron-wallet/src/models/subjects/address-created-subject.ts
+++ packages/neuron-wallet/src/models/subjects/address-created-subject.ts
@@ -1,10 +1,16 @@
 import { ReplaySubject } from 'rxjs'
 import { Address } from 'database/address/address-dao'
+import ProcessUtils from 'utils/process'
+import { remote } from 'electron'
 
 export default class AddressCreatedSubject {
   private static subject = new ReplaySubject<Address[]>(100)
 
   public static getSubject() {
-    return this.subject
+    if (ProcessUtils.isRenderer()) {
+      return remote.require('./models/subjects/address-created-subject').default.getSubject()
+    } else {
+      return this.subject
+    }
   }
 }
