844a5ddb18c8a74ee20fbae5e1479960b4437539
Chen Yu <keithwhisper@gmail.com>
2019-12-25 11:22:23 +0800

    feat: Update the UI of transaction detail (#1260)
    * feat: Update the UI of transaction detail
    
    * refactor: use spaces instead of tabs

--- packages/neuron-wallet/src/controllers/app/show-window.ts
+++ packages/neuron-wallet/src/controllers/app/show-window.ts
@@ -3,22 +3,24 @@ import path from 'path'
 import env from 'env'
 
 const showWindow = (url: string, title: string): BrowserWindow => {
-	const win = new BrowserWindow({
-		width: 1200,
-		show: false,
-		webPreferences: {
-			preload: path.join(__dirname, './preload.js'),
-		},
-	})
-	const fmtUrl = url.startsWith('http') || url.startsWith('file:') ? url : env.mainURL + url
-	win.loadURL(fmtUrl)
-	win.on('ready-to-show', () => {
-		win.setTitle(title)
-		win.show()
-		win.focus()
-	})
+  const win = new BrowserWindow({
+    width: 1200,
+    minWidth: 900,
+    minHeight: 600,
+    show: false,
+    webPreferences: {
+      preload: path.join(__dirname, './preload.js'),
+    },
+  })
+  const fmtUrl = url.startsWith('http') || url.startsWith('file:') ? url : env.mainURL + url
+  win.loadURL(fmtUrl)
+  win.on('ready-to-show', () => {
+    win.setTitle(title)
+    win.show()
+    win.focus()
+  })
 
-	return win
+  return win
 }
 
 export { showWindow }
