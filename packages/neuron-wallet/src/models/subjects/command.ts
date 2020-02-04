501380a9f44913c7b27f5094a287ba962d8e7ef7
James Chen <james@ashchan.com>
2020-02-04 09:02:27 +0900

    Rename command subject type nav to navigation-to-url

--- packages/neuron-wallet/src/models/subjects/command.ts
+++ packages/neuron-wallet/src/models/subjects/command.ts
@@ -2,7 +2,7 @@ import { Subject } from 'rxjs'
 
 const CommandSubject = new Subject<{
   winID: number
-  type: 'nav' | 'delete-wallet' | 'backup-wallet' | 'export-xpubkey'
+  type: 'navigate-to-url' | 'delete-wallet' | 'backup-wallet' | 'export-xpubkey'
   payload: string | null
   dispatchToUI: boolean
 }>()
