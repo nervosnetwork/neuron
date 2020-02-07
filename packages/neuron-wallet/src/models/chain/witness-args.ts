4ac980f87d376f7d8b4e5299cdba006bf174bbff
classicalliu <classicalliu@gmail.com>
2020-02-07 17:44:40 +0800

    Add WitnessArgs#deserialize

--- packages/neuron-wallet/src/models/chain/witness-args.ts
+++ packages/neuron-wallet/src/models/chain/witness-args.ts
@@ -23,6 +23,36 @@ export default class WitnessArgs {
     this.lock = WitnessArgs.EMPTY_LOCK
   }
 
+  public static deserialize(wit: string): WitnessArgs {
+    const buffer = Buffer.from(wit.slice(2), 'hex')
+    const size = buffer.slice(0, 4).readUInt32LE(0)
+    const offset0 = buffer.readUInt32LE(4)
+    const offset1 = buffer.readUInt32LE(8)
+    const offset2 = buffer.readUInt32LE(12)
+
+    const lockLength = buffer.readUInt32LE(offset0)
+    const lock = '0x' + buffer.slice(offset0 + 4, offset1).toString('hex')
+    const inputTypeLength = offset1 <= size - 4 ? buffer.readUInt32LE(offset1) : 0
+    const inputType = '0x' + buffer.slice(offset1 + 4, offset2).toString('hex')
+    const outputTypeLength = offset2 <= size - 4 ? buffer.readUInt32LE(offset2) : 0
+    const outputType = '0x' + buffer.slice(offset2 + 4, size).toString('hex')
+
+    if (
+      buffer.length !== size ||
+      lock.length !== lockLength * 2 + 2 ||
+      inputType.length !== inputTypeLength * 2 + 2 ||
+      outputType.length !== outputTypeLength * 2 + 2
+    ) {
+      throw new Error('Serialized witness format error!')
+    }
+
+    return WitnessArgs.fromObject({
+      lock: '0x' + buffer.slice(offset0 + 4, offset1).toString('hex'),
+      inputType: inputType === '0x' ? undefined : inputType,
+      outputType: outputType === '0x' ? undefined : outputType
+    })
+  }
+
   public static generateEmpty(): WitnessArgs {
     return new WitnessArgs()
   }
