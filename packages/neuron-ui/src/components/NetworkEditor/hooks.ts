7f32062f33c783fb9d91bd6efd946d1f4a7176d4
Keith <keithwhisper@gmail.com>
2020-03-11 22:10:01 +0800

    refactor: use form in the network editor component

--- packages/neuron-ui/src/components/NetworkEditor/hooks.ts
+++ packages/neuron-ui/src/components/NetworkEditor/hooks.ts
@@ -1,70 +1,103 @@
 import { useCallback } from 'react'
+import { useHistory } from 'react-router-dom'
 
 import { StateDispatch } from 'states/stateProvider/reducer'
 import { createNetwork, updateNetwork, addNotification } from 'states/stateProvider/actionCreators'
 
 import { MAX_NETWORK_NAME_LENGTH, ErrorCode } from 'utils/const'
 
-export const useHandleSubmit = (
+export const useOnSubmit = (
   id: string = '',
   name: string = '',
   remote: string = '',
   networks: Readonly<State.Network[]> = [],
-  history: any,
-  dispatch: StateDispatch
+  history: ReturnType<typeof useHistory>,
+  dispatch: StateDispatch,
+  disabled: boolean
 ) =>
-  useCallback(async () => {
-    let errorMessage: State.Message<ErrorCode, { fieldName: string; fieldValue?: string; length?: string }> | undefined
-    if (!name) {
-      errorMessage = {
-        type: 'warning',
-        timestamp: +new Date(),
-        code: ErrorCode.FieldRequired,
-        meta: {
-          fieldName: 'name',
-        },
+  useCallback(
+    (e: React.FormEvent): void => {
+      if (disabled) {
+        return
       }
-      return addNotification(errorMessage)(dispatch)
-    }
-    if (name.length > MAX_NETWORK_NAME_LENGTH) {
-      errorMessage = {
-        type: 'warning',
-        timestamp: +new Date(),
-        code: ErrorCode.FieldTooLong,
-        meta: {
-          fieldName: 'name',
-          fieldValue: name,
-          length: `${MAX_NETWORK_NAME_LENGTH}`,
-        },
+      e.preventDefault()
+      let errorMessage:
+        | State.Message<ErrorCode, { fieldName: string; fieldValue?: string; length?: string }>
+        | undefined
+      if (!name) {
+        errorMessage = {
+          type: 'warning',
+          timestamp: +new Date(),
+          code: ErrorCode.FieldRequired,
+          meta: {
+            fieldName: 'name',
+          },
+        }
+        addNotification(errorMessage)(dispatch)
+        return
       }
-      return addNotification(errorMessage)(dispatch)
-    }
-    if (!remote) {
-      errorMessage = {
-        type: 'warning',
-        timestamp: +new Date(),
-        code: ErrorCode.FieldRequired,
-        meta: {
-          fieldName: 'remote',
-        },
+      if (name.length > MAX_NETWORK_NAME_LENGTH) {
+        errorMessage = {
+          type: 'warning',
+          timestamp: +new Date(),
+          code: ErrorCode.FieldTooLong,
+          meta: {
+            fieldName: 'name',
+            fieldValue: name,
+            length: `${MAX_NETWORK_NAME_LENGTH}`,
+          },
+        }
+        addNotification(errorMessage)(dispatch)
+        return
       }
-      return addNotification(errorMessage)(dispatch)
-    }
-    if (!remote.startsWith('http')) {
-      errorMessage = {
-        type: 'warning',
-        timestamp: +new Date(),
-        code: ErrorCode.ProtocolRequired,
-        meta: {
-          fieldName: 'remote',
-          fieldValue: remote,
-        },
+      if (!remote) {
+        errorMessage = {
+          type: 'warning',
+          timestamp: +new Date(),
+          code: ErrorCode.FieldRequired,
+          meta: {
+            fieldName: 'remote',
+          },
+        }
+        addNotification(errorMessage)(dispatch)
+        return
       }
-      return addNotification(errorMessage)(dispatch)
-    }
-    // verification, for now, only name is unique
-    if (id === 'new') {
-      if (networks.some(network => network.name === name)) {
+      if (!remote.startsWith('http')) {
+        errorMessage = {
+          type: 'warning',
+          timestamp: +new Date(),
+          code: ErrorCode.ProtocolRequired,
+          meta: {
+            fieldName: 'remote',
+            fieldValue: remote,
+          },
+        }
+        addNotification(errorMessage)(dispatch)
+        return
+      }
+      // verification, for now, only name is unique
+      if (id === 'new') {
+        if (networks.some(network => network.name === name)) {
+          errorMessage = {
+            type: 'warning',
+            timestamp: +new Date(),
+            code: ErrorCode.FieldUsed,
+            meta: {
+              fieldName: 'name',
+              fieldValue: name,
+            },
+          }
+          addNotification(errorMessage)(dispatch)
+          return
+        }
+        createNetwork({
+          name,
+          remote,
+        })(dispatch, history)
+        return
+      }
+
+      if (networks.some(network => network.name === name && network.id !== id)) {
         errorMessage = {
           type: 'warning',
           timestamp: +new Date(),
@@ -74,35 +107,20 @@ export const useHandleSubmit = (
             fieldValue: name,
           },
         }
-        return addNotification(errorMessage)(dispatch)
+        addNotification(errorMessage)(dispatch)
+        return
       }
-      return createNetwork({
-        name,
-        remote,
-      })(dispatch, history)
-    }
-
-    if (networks.some(network => network.name === name && network.id !== id)) {
-      errorMessage = {
-        type: 'warning',
-        timestamp: +new Date(),
-        code: ErrorCode.FieldUsed,
-        meta: {
-          fieldName: 'name',
-          fieldValue: name,
+      updateNetwork({
+        networkID: id!,
+        options: {
+          name,
+          remote,
         },
-      }
-      return addNotification(errorMessage)(dispatch)
-    }
-    return updateNetwork({
-      networkID: id!,
-      options: {
-        name,
-        remote,
-      },
-    })(dispatch, history)
-  }, [id, name, remote, networks, history, dispatch])
+      })(dispatch, history)
+    },
+    [id, name, remote, networks, history, dispatch]
+  )
 
 export default {
-  useHandleSubmit,
+  useOnSubmit,
 }
