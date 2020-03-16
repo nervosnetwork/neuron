fea976a1ab8053210afcffc9a9ca8ff4c1df9f0f
James Chen <james@ashchan.com>
2020-03-16 21:41:39 +0900

    Do not use sublevel db

--- packages/neuron-wallet/src/database/leveldb/index.ts
+++ packages/neuron-wallet/src/database/leveldb/index.ts
@@ -2,26 +2,19 @@ import fs from 'fs'
 import path from 'path'
 import levelup, { LevelUp } from 'levelup'
 import leveldown from 'leveldown'
-import sub from 'subleveldown'
 import env from 'env'
 
-// Create a database. If prefix is provided the result will be a sublevel db
-//   with its own keyspace. If valueEncoding is provided it's applied to the
-//   sublevel db.
-const leveldb = (prefix?: string, valueEncoding?: string): LevelUp => {
-  const dbname = "datastore" // Keep as a single database
-  const dbpath = path.join(env.fileBasePath, dbname)
-  if (!fs.existsSync(dbpath)) {
-    fs.mkdirSync(dbpath, { recursive: true })
+const leveldb = (dbname: string): LevelUp => {
+  const dir = env.fileBasePath
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true })
   }
 
+  const dbpath = path.join(dir, dbname)
   const db = levelup(leveldown(dbpath))
-  if (prefix) {
-    return sub(db, prefix, { valueEncoding })
-  }
   return db
 }
 
-export const txdb = leveldb('transactions')
+export const maindb = leveldb("datastore")
 
 export default leveldb
