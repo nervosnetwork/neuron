f7a0e3e038c85ca1005c58a0577f14fab7b0ce9f
Keith <keithwhisper@gmail.com>
2020-03-25 12:15:34 +0800

    feat: use tooltip to indicate users the balance is under syncing

---
+++ packages/neuron-ui/src/stories/BalanceSyncIcon.stories.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { ConnectionStatus, SyncStatus } from 'utils/const'
+import BalanceSyncIcon, { BalanceSyncIconProps } from 'components/BalanceSyncingIcon'
+
+const stories = storiesOf('Balance Sync Icon', module)
+
+const propsList: { [index: string]: BalanceSyncIconProps } = {
+  Offline: {
+    connectionStatus: ConnectionStatus.Offline,
+    syncStatus: SyncStatus.SyncNotStart,
+  },
+  'Fail to fetch tip': {
+    connectionStatus: ConnectionStatus.Online,
+    syncStatus: SyncStatus.FailToFetchTipBlock,
+  },
+  Completed: {
+    connectionStatus: ConnectionStatus.Online,
+    syncStatus: SyncStatus.SyncCompleted,
+  },
+  'Not Start': {
+    connectionStatus: ConnectionStatus.Online,
+    syncStatus: SyncStatus.SyncNotStart,
+  },
+  Pending: {
+    connectionStatus: ConnectionStatus.Online,
+    syncStatus: SyncStatus.SyncPending,
+  },
+  Syncing: {
+    connectionStatus: ConnectionStatus.Online,
+    syncStatus: SyncStatus.Syncing,
+  },
+}
+
+Object.entries(propsList).forEach(([name, props]) => {
+  stories.add(name, () => {
+    return <BalanceSyncIcon {...props} />
+  })
+})
