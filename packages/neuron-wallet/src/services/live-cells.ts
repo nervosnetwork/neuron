3d2b73f763a4d6605514a144dd414264a450462a
ququzone <xueping.yang@gmail.com>
2020-04-16 19:32:19 +0800

    add query pending balance logic

--- packages/neuron-wallet/src/services/live-cells.ts
+++ packages/neuron-wallet/src/services/live-cells.ts
@@ -1,8 +1,10 @@
-import { getConnection, In } from 'typeorm'
-import { hexToBytes } from '@nervosnetwork/ckb-sdk-utils'
+import { getConnection, In, LessThan, MoreThanOrEqual, Not } from 'typeorm'
 import LiveCellEntity from 'database/chain/entities/live-cell'
+import { LiveCellPersistor } from 'services/tx/livecell-persistor'
 
 export default class LiveCellsService {
+  private static ZERO_BUFFER = Buffer.alloc(0);
+
   // exclude hasData = true and typeScript != null
   public static async getBalance(lockHashes: Set<string>): Promise<{
     liveBalance: Map<string, string>
@@ -10,35 +12,67 @@ export default class LiveCellsService {
     pendingBalance: Map<string, string>
   }> {
     const hashData: Buffer[] = []
-    lockHashes.forEach((lockHash: string) => hashData.push(Buffer.from(hexToBytes(lockHash))))
+    lockHashes.forEach((lockHash: string) => hashData.push(Buffer.from(lockHash.slice(2), 'hex')))
 
-    const cells: { usedBlockNumber: string, lockHash: Buffer, sumOfCapacity: string }[] = await getConnection()
+    const lastConfirmBlockNumber = BigInt(await LiveCellPersistor.lastBlockNumber()) - LiveCellPersistor.CONFIRMATION_THRESHOLD
+
+    const liveCells: { lockHash: Buffer, sumOfCapacity: string }[] = await getConnection()
       .getRepository(LiveCellEntity)
       .createQueryBuilder('cell')
       .select('cell.lockHash', 'lockHash')
-      .addSelect('cell.usedBlockNumber', 'usedBlockNumber')
       .addSelect('CAST(SUM(CAST(cell.capacity AS UNSIGNED BIG INT)) AS VARCHAR)', 'sumOfCapacity')
       .where({
         lockHash: In([...hashData]),
-        data: Buffer.from('0x', 'hex'),
+        data: LiveCellsService.ZERO_BUFFER,
         typeHash: null,
+        usedBlockNumber: null,
+        createdBlockNumber: LessThan(lastConfirmBlockNumber.toString())
       })
       .groupBy('cell.lockHash')
-      .addGroupBy('cell.usedBlockNumber')
+      .getRawMany()
+    const pendingCells: { lockHash: Buffer, sumOfCapacity: string }[] = await getConnection()
+      .getRepository(LiveCellEntity)
+      .createQueryBuilder('cell')
+      .select('cell.lockHash', 'lockHash')
+      .addSelect('CAST(SUM(CAST(cell.capacity AS UNSIGNED BIG INT)) AS VARCHAR)', 'sumOfCapacity')
+      .where({
+        lockHash: In([...hashData]),
+        data: LiveCellsService.ZERO_BUFFER,
+        typeHash: null,
+        usedBlockNumber: null,
+        createdBlockNumber: MoreThanOrEqual(lastConfirmBlockNumber.toString())
+      })
+      .groupBy('cell.lockHash')
+      .getRawMany()
+    const sentCells: { lockHash: Buffer, sumOfCapacity: string }[] = await getConnection()
+      .getRepository(LiveCellEntity)
+      .createQueryBuilder('cell')
+      .select('cell.lockHash', 'lockHash')
+      .addSelect('CAST(SUM(CAST(cell.capacity AS UNSIGNED BIG INT)) AS VARCHAR)', 'sumOfCapacity')
+      .where({
+        lockHash: In([...hashData]),
+        data: LiveCellsService.ZERO_BUFFER,
+        typeHash: null,
+        usedBlockNumber: Not(null)
+      })
+      .groupBy('cell.lockHash')
       .getRawMany()
 
     const liveBalance = new Map<string, string>()
     const sentBalance = new Map<string, string>()
     const pendingBalance = new Map<string, string>()
 
-    cells.forEach(c => {
+    liveCells.forEach(c => {
       const lockHash: string = `0x${c.lockHash.toString('hex')}`
-      const sumOfCapacity: string = c.sumOfCapacity
-      if (c.usedBlockNumber === null) {
-        liveBalance.set(lockHash, sumOfCapacity)
-      } else {
-        sentBalance.set(lockHash, sumOfCapacity)
-      }
+      liveBalance.set(lockHash, c.sumOfCapacity)
+    })
+    pendingCells.forEach(c => {
+      const lockHash: string = `0x${c.lockHash.toString('hex')}`
+      pendingBalance.set(lockHash, c.sumOfCapacity)
+    })
+    sentCells.forEach(c => {
+      const lockHash: string = `0x${c.lockHash.toString('hex')}`
+      sentBalance.set(lockHash, c.sumOfCapacity)
     })
 
     return {
