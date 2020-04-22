26ab6a6c35a42af4e836b8aedb2b7f6d32ec5b70
Keith <keithwhisper@gmail.com>
2020-04-22 15:00:29 +0800

    feat: add custom exceptions

---
+++ packages/neuron-ui/src/exceptions/index.ts
@@ -0,0 +1,59 @@
+import { ErrorCode } from 'utils/const'
+
+export class FieldInvalidException extends Error {
+  public code = ErrorCode.FieldInvalid
+  public i18n: {
+    fieldName: string
+  }
+
+  constructor(fieldName: string) {
+    super(`messages.codes.${ErrorCode.FieldInvalid}`)
+    this.i18n = {
+      fieldName,
+    }
+  }
+}
+
+export class FieldRequiredException extends Error {
+  public code = ErrorCode.FieldRequired
+  public i18n: {
+    fieldName: string
+  }
+
+  constructor(fieldName: string) {
+    super(`messages.codes.${ErrorCode.FieldRequired}`)
+    this.i18n = { fieldName }
+  }
+}
+
+export class FieldTooLongException extends Error {
+  public code = ErrorCode.FieldTooLong
+  public i18n: {
+    fieldName: string
+    length: number
+  }
+
+  constructor(fieldName: string, length: number) {
+    super(`messages.codes.${ErrorCode.FieldTooLong}`)
+    this.i18n = {
+      fieldName,
+      length,
+    }
+  }
+}
+
+export class FieldUsedException extends Error {
+  public code = ErrorCode.FieldUsed
+  public i18n: {
+    fieldName: string
+  }
+
+  constructor(fieldName: string) {
+    super(`messages.codes.${ErrorCode.FieldUsed}`)
+    this.i18n = {
+      fieldName,
+    }
+  }
+}
+
+export default undefined
