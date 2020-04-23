b7a38f25bf3addaf9de1af0882496140175dbb34
Keith <keithwhisper@gmail.com>
2020-04-23 23:14:30 +0800

    feat: update amount on sending all and the generated tx is valid

--- packages/neuron-ui/src/components/SUDTSend/index.tsx
+++ packages/neuron-ui/src/components/SUDTSend/index.tsx
@@ -208,6 +208,12 @@ const SUDTSend = () => {
     accountInfo,
   ])
 
+  useEffect(() => {
+    if (sendState.sendAll && experimental?.tx?.value && accountInfo?.decimal) {
+      dispatch({ type: Fields.Amount, payload: sudtValueToAmount(experimental.tx.value, accountInfo.decimal) })
+    }
+  }, [sendState.sendAll, experimental, accountInfo])
+
   const onInput = useCallback(
     e => {
       const {
@@ -299,7 +305,12 @@ const SUDTSend = () => {
               )
             })}
             <div className={styles.sendAll}>
-              <Button type="primary" label="Max" onClick={onToggleSendingAll} disabled={!sendState.address} />
+              <Button
+                type="primary"
+                label="Max"
+                onClick={onToggleSendingAll}
+                disabled={!sendState.address || !!errors.address}
+              />
             </div>
             <div className={styles.fee}>
               <TransactionFeePanel
