223ed3bc1bc3e95cc68cafc0c465467e9d93283a
Chen Yu <keithwhisper@gmail.com>
2020-04-28 16:49:31 +0800

    feat: add custom exceptions (#1573)

---
+++ packages/neuron-ui/src/exceptions/index.ts
@@ -0,0 +1,66 @@
+import { ErrorCode } from 'utils/const'
+
+export class FieldInvalidException extends Error {
+  public code = ErrorCode.FieldInvalid
+  public i18n: {
+    fieldName: string
+  }
+
+  constructor(fieldName: string) {
+    super(`messages.codes.${ErrorCode.FieldInvalid}`)
+    this.i18n = {
+      fieldName,
+    }
+  }
+}
+
+export class FieldRequiredException extends Error {
+  public code = ErrorCode.FieldRequired
+  public i18n: {
+    fieldName: string
+  }
+
+  constructor(fieldName: string) {
+    super(`messages.codes.${ErrorCode.FieldRequired}`)
+    this.i18n = { fieldName }
+  }
+}
+
+export class FieldTooLongException extends Error {
+  public code = ErrorCode.FieldTooLong
+  public i18n: {
+    fieldName: string
+    length: number
+  }
+
+  constructor(fieldName: string, length: number) {
+    super(`messages.codes.${ErrorCode.FieldTooLong}`)
+    this.i18n = {
+      fieldName,
+      length,
+    }
+  }
+}
+
+export class FieldUsedException extends Error {
+  public code = ErrorCode.FieldUsed
+  public i18n: {
+    fieldName: string
+  }
+
+  constructor(fieldName: string) {
+    super(`messages.codes.${ErrorCode.FieldUsed}`)
+    this.i18n = {
+      fieldName,
+    }
+  }
+}
+
+export class AmountNotEnoughException extends Error {
+  public code = ErrorCode.AmountNotEnough
+  constructor() {
+    super(`messages.codes.${ErrorCode.AmountNotEnough}`)
+  }
+}
+
+export default undefined
