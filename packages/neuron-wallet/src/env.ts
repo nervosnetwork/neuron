90eeda73088fefcd5060df6813d8ff82c8e36fd3
Keith <keithwhisper@gmail.com>
2020-06-12 11:27:05 +0800

    feat: enable local env config

--- packages/neuron-wallet/src/env.ts
+++ packages/neuron-wallet/src/env.ts
@@ -1,9 +1,7 @@
-import { app as electronApp, remote } from 'electron'
 import os from 'os'
-import * as path from 'path'
+import path from 'path'
+import { app as electronApp, remote } from 'electron'
 import dotenv from 'dotenv'
-
-dotenv.config()
 
 const app = electronApp ?? (remote?.app) ?? {
   getPath(aPath: string): string {
@@ -18,6 +16,11 @@ const app = electronApp ?? (remote?.app) ?? {
 
 const isTestMode = process.env.NODE_ENV === 'test'
 const isDevMode = !app.isPackaged && !isTestMode
+
+if (isDevMode) {
+  dotenv.config({ path: path.resolve(__dirname, '..', '.env.development.local') })
+}
+dotenv.config()
 
 const fileBase = (() => {
   if (isTestMode) {
