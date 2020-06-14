97f93077638752e126bff5e24f6df3824b5925ef
kata <kata.choi@gmail.com>
2020-06-14 15:34:43 +0800

    Adds IndexerConnect#connect

--- packages/neuron-wallet/src/block-sync-renderer/sync/indexer-connector.ts
+++ packages/neuron-wallet/src/block-sync-renderer/sync/indexer-connector.ts
@@ -1,49 +1,90 @@
-import {ReplaySubject} from 'rxjs'
-import CommonUtils from 'utils/common'
+import { Subject, ReplaySubject } from 'rxjs'
 import { Indexer, Tip } from '@ckb-lumos/indexer'
+import CommonUtils from 'utils/common'
+import RpcService from 'services/rpc-service'
+import AddressParser from "models/address-parser"
+import TransactionWithStatus from 'models/chain/transaction-with-status'
 
 export default class IndexerConnector {
   private indexer: Indexer
+  private rpcService: RpcService
   private stop: boolean = true
+  private addressesToWatch: string[] = []
   public blockTipSubject: ReplaySubject<Tip> = new ReplaySubject<Tip>(3)
+  public transactionsSubject: Subject<Array<TransactionWithStatus>> = new Subject<Array<TransactionWithStatus>>()
 
-  constructor(nodeUrl: string, indexerFolderPath: string) {
+  constructor(addresses: string[], nodeUrl: string, indexerFolderPath: string) {
     this.indexer = new Indexer(nodeUrl, indexerFolderPath)
+    this.addressesToWatch = addresses
+    this.rpcService = new RpcService(nodeUrl)
   }
 
   public async connect() {
     this.indexer.startForever()
     this.stop = false
+
     while (!this.stop) {
       try {
         const lastIndexerTip = this.indexer.tip()
         this.blockTipSubject.next(lastIndexerTip)
 
-        //loop through address map
-        //poll transaction hashes by lock hashes
-        //compare transaction hashes with the local cache
-        //push new transactions through rxjs subject if size different
-
-
+        const sortedGroupedTransactions = await this.fetchNewTransactions()
+        for (const txs of sortedGroupedTransactions) {
+          this.transactionsSubject.next(txs)
+        }
       } catch (error) {
         console.error(error)
       } finally {
-        await CommonUtils.sleep(1000)
+        await CommonUtils.sleep(5000)
       }
     }
   }
 
-  public async addTransactionSubjectByAddress(address: string) {
-    //parse address to lock script
-    //create a rxjs subject
-    //add (address, lockscript, subject) to a map
-    //return subject
+  private async fetchNewTransactions() {
+    const transactions = await Promise.all(
+      this.addressesToWatch.map(async address => {
+        const lockScript = AddressParser.parse(address)
+        const txHashes = this.indexer.getTransactionsByLockScript({
+          code_hash: lockScript.codeHash,
+          hash_type: lockScript.hashType,
+          args: lockScript.args
+        })
+
+        const txs = await Promise.all(
+          txHashes.map(async hash => {
+            const txWithStatus = await this.rpcService.getTransaction(hash)
+            const blockHeader = await this.rpcService.getHeader(txWithStatus!.txStatus.blockHash!)
+            txWithStatus!.transaction.blockNumber = blockHeader?.number
+            txWithStatus!.transaction.blockHash = txWithStatus!.txStatus.blockHash!
+            txWithStatus!.transaction.timestamp = blockHeader?.timestamp
+            return txWithStatus
+          })
+        )
+        return txs
+      })
+    )
+
+    const groupedTransactions = transactions
+      .flat()
+      .filter((tx: TransactionWithStatus) => tx)
+      .reduce((groupedMap: Map<string, Array<TransactionWithStatus>>, tx: TransactionWithStatus) => {
+        const {transaction} = tx
+        if (transaction.blockNumber === undefined) {
+          return groupedMap
+        }
+        const blockNumber = transaction.blockNumber.toString()
+
+        const grouped = groupedMap.get(blockNumber) || []
+        grouped.push(tx)
+
+        groupedMap.set(blockNumber, grouped)
+        return groupedMap
+      }, new Map())
+
+    const sortedGroupedTransactions = [...groupedTransactions!.keys()]
+      .sort((a, b) => parseInt(a) - parseInt(b))
+      .map(blockNumber => groupedTransactions!.get(blockNumber))
+
+    return sortedGroupedTransactions
   }
 }
-// const depositTxs = await getConnection()
-//           .getRepository(TransactionEntity)
-//           .createQueryBuilder('tx')
-//           .where({
-//             hash: In(depositTxHashes)
-//           })
-//           .getMany()
--- packages/neuron-wallet/tests/services/indexer-connector.test.ts
+++ packages/neuron-wallet/tests/services/indexer-connector.test.ts
@@ -1,46 +1,68 @@
 import path from 'path'
+import AddressGenerator from "../../src/models/address-generator"
+import { AddressPrefix } from '../../src/models/keys/address'
+import SystemScriptInfo from '../../src/models/system-script-info'
+import IndexerConnector from '../../src/block-sync-renderer/sync/indexer-connector'
 
 const stubbedStartForeverFn = jest.fn()
 const stubbedTipFn = jest.fn()
+const stubbedGetTransactionsByLockScriptFn = jest.fn()
+const stubbedGetTransactionFn = jest.fn()
+const stubbedGetHeaderFn = jest.fn()
 
 const stubbedIndexerConstructor = jest.fn().mockImplementation(
   () => ({
     startForever: stubbedStartForeverFn,
-    tip: stubbedTipFn
+    tip: stubbedTipFn,
+    getTransactionsByLockScript: stubbedGetTransactionsByLockScriptFn
+  })
+)
+
+const stubbedRPCServiceConstructor = jest.fn().mockImplementation(
+  () => ({
+    getTransaction: stubbedGetTransactionFn,
+    getHeader: stubbedGetHeaderFn
   })
 )
 
 const resetMocks = () => {
-  stubbedStartForeverFn.mockReset()
   stubbedTipFn.mockReset()
+  stubbedStartForeverFn.mockReset()
+  stubbedGetTransactionFn.mockReset()
+  stubbedGetHeaderFn.mockReset()
+  stubbedGetTransactionsByLockScriptFn.mockReset()
 }
 
-stubbedIndexerConstructor.prototype.reset = function() {
-  stubbedStartForeverFn.mockReset()
-  stubbedTipFn.mockReset()
+const connectIndexer = async (indexerConnector: IndexerConnector) => {
+  indexerConnector.connect()
+  await Promise.resolve()
 }
-
 
 describe('unit tests for IndexerConnector', () => {
   const nodeUrl = 'http://nodeurl:8114'
   const indexerFolderPath = '/indexer/data/path'
-  let IndexerConnector
+  let stubbedIndexerConnector: any
   beforeEach(() => {
     jest.doMock('@ckb-lumos/indexer', () => {
       return {
         Indexer : stubbedIndexerConstructor
       }
     });
-    IndexerConnector = require('../../src/block-sync-renderer/sync/indexer-connector').default
+    jest.doMock('services/rpc-service', () => {
+      return stubbedRPCServiceConstructor
+    });
+    stubbedIndexerConnector = require('../../src/block-sync-renderer/sync/indexer-connector').default
     resetMocks()
     jest.useFakeTimers()
-
+  });
+  afterEach(() => {
+    jest.clearAllTimers()
   });
 
   describe('#constructor', () => {
     describe('when success', () => {
       beforeEach(() => {
-        new IndexerConnector(nodeUrl, indexerFolderPath)
+        new stubbedIndexerConnector([], nodeUrl, indexerFolderPath)
       });
       it('init with a node url and indexer folder path', () => {
         expect(stubbedIndexerConstructor).toHaveBeenCalledWith(nodeUrl, path.join(indexerFolderPath))
@@ -58,46 +80,92 @@ describe('unit tests for IndexerConnector', () => {
   describe('#connect', () => {
     const fakeTip1 = {block_number: '1', block_hash: 'hash1'}
     const fakeTip2 = {block_number: '2', block_hash: 'hash2'}
-    let indexerConnector
+    const fakeBlock1 = {number: '1', hash: '1', timestamp: '1'}
+    const fakeBlock2 = {number: '2', hash: '2', timestamp: '2'}
+    const fakeTx1 = {transaction: {hash: 'hash1', blockNumber: fakeBlock1.number}, txStatus: {status: 'committed', blockHash: fakeBlock1.hash}}
+    const fakeTx2 = {transaction: {hash: 'hash2', blockNumber: fakeBlock2.number}, txStatus: {status: 'committed', blockHash: fakeBlock2.hash}}
+    const fakeTx3 = {transaction: {hash: 'hash3', blockNumber: fakeBlock2.number}, txStatus: {status: 'committed', blockHash: fakeBlock2.hash}}
+
+    let indexerConnector: IndexerConnector
     beforeEach(() => {
       stubbedTipFn.mockReturnValueOnce(fakeTip1)
       stubbedTipFn.mockReturnValueOnce(fakeTip2)
-      indexerConnector = new IndexerConnector('', '')
-      indexerConnector.connect()
+
+      const shortAddressInfo = {
+        lock: SystemScriptInfo.generateSecpScript('0x36c329ed630d6ce750712a477543672adab57f4c'),
+      }
+      const address = AddressGenerator.toShort(shortAddressInfo.lock, AddressPrefix.Testnet)
+      const addressesToWatch = [address, address]
+      indexerConnector = new stubbedIndexerConnector(addressesToWatch, '', '')
     });
-    it('starts indexer', () => {
+    it('starts indexer', async () => {
+      stubbedGetTransactionFn.mockReturnValue(fakeTx1)
+      stubbedGetTransactionsByLockScriptFn.mockReturnValue([fakeTx1.transaction.hash])
+      await connectIndexer(indexerConnector)
       expect(stubbedStartForeverFn).toHaveBeenCalled()
     });
     describe('polls for new data', () => {
-      describe('#getTransactionSubject(address)', () => {
-
+      describe('#transactionsSubject', () => {
+        let transactionsSubject: any
+        beforeEach(() => {
+          transactionsSubject = indexerConnector.transactionsSubject
+        });
         describe('check if new transactions are available for an address', () => {
           //Use ckb-indexer's last cursor to facilitate
-          describe('when the total count of transactions from indexer is equal to the committed ones from DB', () => {
+          describe('when there are no new transactions', () => {
+            let txObserver: any
+            beforeEach(async () => {
+              stubbedGetTransactionsByLockScriptFn.mockReturnValue([])
+              txObserver = jest.fn()
+              transactionsSubject.subscribe((transactions: any) => txObserver(transactions))
+              await connectIndexer(indexerConnector)
+            });
             it('should not emit new transactions for the address', () => {
-
+              expect(txObserver).toHaveBeenCalledTimes(0)
             })
           });
-          describe('when the total count of transactions from indexer is greater than the committed ones from DB', () => {
-            it('emits new transactions for the address', () => {
+          describe('when there are new transactions', () => {
+            let txObserver: any
+            beforeEach(async () => {
+              stubbedGetTransactionsByLockScriptFn.mockReturnValueOnce([fakeTx1.transaction.hash, fakeTx2.transaction.hash])
+              stubbedGetTransactionsByLockScriptFn.mockReturnValueOnce([fakeTx3.transaction.hash])
+              stubbedGetTransactionFn.mockReturnValueOnce(fakeTx1)
+              stubbedGetTransactionFn.mockReturnValueOnce(fakeTx2)
+              stubbedGetTransactionFn.mockReturnValueOnce(fakeTx3)
+              stubbedGetHeaderFn.mockReturnValueOnce(fakeBlock1)
+              stubbedGetHeaderFn.mockReturnValueOnce(fakeBlock2)
+              stubbedGetHeaderFn.mockReturnValueOnce(fakeBlock2)
 
+              txObserver = jest.fn()
+              transactionsSubject.subscribe((transactions: any) => txObserver(transactions))
+              await connectIndexer(indexerConnector)
+            });
+            it('emits new transactions in batch by block number', async () => {
+              expect(txObserver).toHaveBeenCalledWith([fakeTx1])
+              expect(txObserver).toHaveBeenCalledWith([fakeTx2, fakeTx3])
             });
           });
         });
       });
       describe('#blockTipSubject', () => {
-        let blockTipSubject
-        let nextBlockTipSubjectSpy
-        beforeEach(() => {
-          blockTipSubject = indexerConnector.blockTipSubject
-          nextBlockTipSubjectSpy = jest.spyOn(indexerConnector.blockTipSubject, 'next')
+        let tipObserver: any
+        beforeEach(async () => {
+          tipObserver = jest.fn()
+          indexerConnector.blockTipSubject.subscribe(tip => {
+            tipObserver(tip)
+          })
+          stubbedGetTransactionFn.mockReturnValue(fakeTx3)
+          stubbedGetTransactionsByLockScriptFn.mockReturnValue([fakeTx3.transaction.hash])
+          await connectIndexer(indexerConnector)
         });
         describe('when the block tip is higher than previous one', () => {
-          it('observed new tips', (done) => {
-            blockTipSubject.subscribe(tip => {
-              expect(tip).toEqual(fakeTip1)
-              done()
-            })
+          it('observed new tips', async () => {
+            for (let second = 1; second <= 1; second++) {
+              jest.advanceTimersByTime(5000)
+              await Promise.resolve()
+            }
+            expect(tipObserver).toHaveBeenCalledWith(fakeTip1)
+            expect(tipObserver).toHaveBeenCalledWith(fakeTip2)
           })
         });
       });
