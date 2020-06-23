75ac8714a19d29bcbc2daca1b7e837d76b16c239
kata <kata.choi@gmail.com>
2020-06-23 17:12:06 +0800

    Add setup script for wallet jest test to mock XMLHttpRequest

---
+++ packages/neuron-wallet/tests/setup.ts
@@ -0,0 +1,24 @@
+export const originalXMLHttpRequest = window.XMLHttpRequest
+
+export const mockedXMLHttpRequest = {
+  open: jest.fn(),
+  send: jest.fn(),
+  readyState: null,
+  responseText: null,
+}
+
+Object.defineProperty(mockedXMLHttpRequest, 'readyState', {
+  get: jest.fn(() => 4),
+  set: jest.fn()
+});
+Object.defineProperty(mockedXMLHttpRequest, 'responseText', {
+  get: jest.fn(() => JSON.stringify([])),
+  set: jest.fn()
+});
+
+Object.defineProperty(window, "XMLHttpRequest", jest.fn(() => ({
+  open: mockedXMLHttpRequest.open,
+  send: mockedXMLHttpRequest.send,
+  readyState: mockedXMLHttpRequest.readyState,
+  responseText: mockedXMLHttpRequest.responseText
+})));
