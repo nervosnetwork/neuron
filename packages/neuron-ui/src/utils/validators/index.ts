f414561287d2f1cf84021d296c8d1893bf805818
Keith <keithwhisper@gmail.com>
2020-06-26 01:45:35 +0800

    refactor: use validatePasswordComplexity instead of verifyPasswordComplexity

--- packages/neuron-ui/src/utils/validators/index.ts
+++ packages/neuron-ui/src/utils/validators/index.ts
@@ -1,12 +1,5 @@
 // REFACTOR: throw exceptions directly
-import {
-  MIN_PASSWORD_LENGTH,
-  MAX_PASSWORD_LENGTH,
-  MIN_AMOUNT,
-  SINCE_FIELD_SIZE,
-  MAX_DECIMAL_DIGITS,
-  SHANNON_CKB_RATIO,
-} from 'utils/const'
+import { MIN_AMOUNT, SINCE_FIELD_SIZE, MAX_DECIMAL_DIGITS, SHANNON_CKB_RATIO } from 'utils/const'
 import { ErrorCode } from 'utils/enums'
 import { CKBToShannonFormatter } from 'utils/formatters'
 import { ckbCore } from 'services/chain'
@@ -161,40 +154,6 @@ export const verifyTotalAmount = (totalAmount: string, fee: string, balance: str
     return false
   }
   return BigInt(totalAmount) + BigInt(fee) <= BigInt(balance)
-}
-
-// done
-export const verifyPasswordComplexity = (password: string) => {
-  if (!password) {
-    return 'password-is-empty'
-  }
-  if (password.length < MIN_PASSWORD_LENGTH) {
-    return 'password-is-too-short'
-  }
-  if (password.length > MAX_PASSWORD_LENGTH) {
-    return 'password-is-too-long'
-  }
-  let complex = 0
-  let reg = /\d/
-  if (reg.test(password)) {
-    complex++
-  }
-  reg = /[a-z]/
-  if (reg.test(password)) {
-    complex++
-  }
-  reg = /[A-Z]/
-  if (reg.test(password)) {
-    complex++
-  }
-  reg = /[^0-9a-zA-Z]/
-  if (reg.test(password)) {
-    complex++
-  }
-  if (complex < 3) {
-    return 'password-is-too-simple'
-  }
-  return true
 }
 
 // done
