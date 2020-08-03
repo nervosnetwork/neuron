1c1a0732f162c628230a7122c054cb7563ea9323
yuche <i@yuche.me>
2020-08-03 16:53:22 +0800

    refactor(renderer): use ts-loader compile ts file

--- packages/neuron-ui/config-overrides.js
+++ packages/neuron-ui/config-overrides.js
@@ -1,4 +1,18 @@
 module.exports = function override(config, env) {
-  //do stuff with the webpack config...
-  return config;
+  const oneOfRules = config.module.rules.find(r => Array.isArray(r.oneOf))
+
+  oneOfRules.oneOf.unshift({
+    test: /\.tsx?$/,
+    loader: 'ts-loader',
+    options: {
+      // since react-app-rewired have ForkTsCheckerWebpackPlugin injected for type checking,
+      // transpileOnly will be fine
+      transpileOnly: true,
+    },
+  })
+  return {
+    ...config,
+    mode: env,
+    devtool: env === 'production' ? false : 'eval-cheap-module-source-map',
+  }
 }
