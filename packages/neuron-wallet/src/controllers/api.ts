809e4ee7c6bcb07a2a7d7e780fa925e35c099fdb
kata <kata.choi@gmail.com>
2020-09-04 14:39:31 +0800

    Await network initialized before open the window

--- packages/neuron-wallet/src/controllers/api.ts
+++ packages/neuron-wallet/src/controllers/api.ts
@@ -41,7 +41,7 @@ export default class ApiController {
   public async mount() {
     this.registerHandlers()
 
-    this.networksController.start()
+    await this.networksController.start()
   }
 
   public runCommand(command: string, params: string) {
