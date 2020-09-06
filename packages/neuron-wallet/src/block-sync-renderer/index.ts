c9eb965103e5d260828af70e7ebf1d9d02a0b4db
kata <kata.choi@gmail.com>
2020-09-06 18:01:37 +0800

    Encapsulates both kill and start sync task operations and process them one by one in a queue

--- packages/neuron-wallet/src/block-sync-renderer/index.ts
+++ packages/neuron-wallet/src/block-sync-renderer/index.ts
@@ -3,6 +3,7 @@ import path from 'path'
 import { fork } from 'child_process'
 import { Network, EMPTY_GENESIS_HASH } from 'models/network'
 import DataUpdateSubject from 'models/subjects/data-update'
+import env from 'env'
 import AddressCreatedSubject from 'models/subjects/address-created-subject'
 import WalletDeletedSubject from 'models/subjects/wallet-deleted-subject'
 import SyncedBlockNumberSubject from 'models/subjects/node'
@@ -11,27 +12,39 @@ import NetworksService from 'services/networks'
 import AddressService from 'services/addresses'
 import WalletService from 'services/wallets'
 import logger from 'utils/logger'
-import CommonUtils from 'utils/common'
 import { LumosCellQuery, LumosCell } from './sync/indexer-connector'
 import IndexerFolderManager from './sync/indexer-folder-manager'
 import { spawn, terminate, subscribe as subscribeToWorkerProcess } from 'utils/worker'
 import SyncApiController from 'controllers/sync-api'
 import type { SyncTask } from './task'
-import env from 'env'
 import TxDbChangedSubject from 'models/subjects/tx-db-changed-subject'
 import AddressDbChangedSubject from 'models/subjects/address-db-changed-subject'
+import { queue } from 'async'
 
 let syncTask: SyncTask | null
 let network: Network | null
 
-const restartSyncTask = async () => {
+const resetSyncTaskQueue = queue(async ({startTask, clearIndexerFolder}) => {
   await killBlockSyncTask()
-  await createBlockSyncTask()
+  if (startTask) {
+    await createBlockSyncTask(clearIndexerFolder)
+  }
+}, 1)
+
+resetSyncTaskQueue.error(err => {
+  logger.error(err)
+})
+
+export const resetSyncTask = async (startTask: boolean = true, clearIndexerFolder: boolean = false) => {
+  if (resetSyncTaskQueue.length() === 0) {
+    resetSyncTaskQueue.push({startTask, clearIndexerFolder})
+    await resetSyncTaskQueue.drain()
+  }
 }
 
 if (BrowserWindow) {
-  AddressCreatedSubject.getSubject().subscribe(restartSyncTask)
-  WalletDeletedSubject.getSubject().subscribe(restartSyncTask)
+  AddressCreatedSubject.getSubject().subscribe(() => resetSyncTask())
+  WalletDeletedSubject.getSubject().subscribe(() => resetSyncTask())
 }
 
 export const switchToNetwork = async (newNetwork: Network, reconnected = false, shouldSync = true) => {
@@ -39,7 +52,7 @@ export const switchToNetwork = async (newNetwork: Network, reconnected = false, 
   network = newNetwork
 
   if (previousNetwork && !reconnected) {
-    if (previousNetwork.id === newNetwork.id || previousNetwork.genesisHash === newNetwork.genesisHash) {
+    if (previousNetwork.id === newNetwork.id) {
       // There's no actual change. No need to reconnect.
       return
     }
@@ -51,55 +64,46 @@ export const switchToNetwork = async (newNetwork: Network, reconnected = false, 
     logger.info('Network:\tswitched to:', network)
   }
 
-  await killBlockSyncTask()
-  //TODO evalutate if this is necessary. This might be unnecessary legacy code.
-  if (shouldSync) {
-    await createBlockSyncTask()
-  } else {
-    SyncedBlockNumberSubject.getSubject().next('-1')
-  }
+  await resetSyncTask(shouldSync)
 }
 
-export const createBlockSyncTask = async (clearIndexerFolder = false) => {
-  await CommonUtils.sleep(2000) // Do not start too fast
-
+export const createBlockSyncTask = async (clearIndexerFolder: boolean) => {
   if (clearIndexerFolder) {
     await new SyncedBlockNumber().setNextBlock(BigInt(0))
     IndexerFolderManager.resetIndexerData()
   }
 
-  if (syncTask) {
-    return
-  }
-
   logger.info('Sync:\tstarting background process')
 
   // prevents the sync task from being started repeatedly if fork does not finish executing.
-  syncTask = Object.create(null)
-  syncTask = await spawn<SyncTask>(
-    fork(path.join(__dirname, 'task-wrapper.js'), [], {
-      env: {
-        fileBasePath: env.fileBasePath
-      }
-    })
-  )
+  const childProcess = fork(path.join(__dirname, 'task-wrapper.js'), [], {
+    env: {
+      fileBasePath: env.fileBasePath
+    },
+    stdio: ['ipc', process.stdout, 'pipe']
+  })
+  childProcess.stderr!.setEncoding('utf8').on('data', data => {
+    logger.error('Sync:ChildProcess:', data)
+  })
+  syncTask = await spawn<SyncTask>(childProcess)
 
   subscribeToWorkerProcess(syncTask, msg => {
-    switch (msg?.channel) {
+    switch (msg.channel) {
       case 'synced-block-number-updated':
-        SyncApiController.emiter.emit('synced-block-number-updated', msg?.result)
+        SyncApiController.emiter.emit('synced-block-number-updated', msg.result)
         break
       case 'tx-db-changed':
-        TxDbChangedSubject.getSubject().next(msg?.result)
+        TxDbChangedSubject.getSubject().next(msg.result)
         break
       case 'address-db-changed':
-        AddressDbChangedSubject.getSubject().next(msg?.result)
+        AddressDbChangedSubject.getSubject().next(msg.result)
         break
       case 'wallet-deleted':
       case 'address-created':
-        restartSyncTask()
+        resetSyncTask()
         break
       default:
+        logger.debug(`Unhandled message event from child process: ${msg.channel}`)
         break
     }
   })
@@ -121,7 +125,7 @@ export const createBlockSyncTask = async (clearIndexerFolder = false) => {
     await WalletService.getInstance().generateAddressesIfNecessary()
 
     // re init txCount in addresses if switch network
-    syncTask?.start(
+    syncTask!.start(
       network.remote,
       network.genesisHash,
       await AddressService.getAddressesByAllWallets()
@@ -133,7 +137,7 @@ export const killBlockSyncTask = async () => {
   if (syncTask) {
     logger.info('Sync:\tkill background process')
     await syncTask.unmount()
-    terminate(syncTask)
+    await terminate(syncTask)
     syncTask = null
   }
 }
