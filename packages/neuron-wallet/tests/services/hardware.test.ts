b9c532b980d05cdd1b0bfff8978bb4425da001ba
yuche <i@yuche.me>
2020-10-09 18:59:23 +0800

    test(main): controllers hould have its own test file

--- packages/neuron-wallet/tests/services/hardware.test.ts
+++ packages/neuron-wallet/tests/services/hardware.test.ts
@@ -1,9 +1,6 @@
 import { Manufacturer } from '../../src/services/hardware/common'
 import HardwareService from '../../src/services/hardware'
-import HardwareController from '../../src/controllers/hardware'
-import { ResponseCode } from '../../src/utils/const'
-import { ledgerNanoS, LedgerNanoX, LedgerCkbApp } from '../mock/hardware'
-import { connectDeviceFailed } from '../../src/exceptions'
+import { ledgerNanoS, LedgerNanoX } from '../mock/hardware'
 
 describe('HardwareWalletService', () => {
   describe('service', () => {
@@ -60,51 +57,4 @@ describe('HardwareWalletService', () => {
       })
     })
   })
-
-  describe('controller', () => {
-    const hardwareControler = new HardwareController()
-
-    beforeEach(async () => {
-      const device = await HardwareService.getInstance().initHardware(ledgerNanoS)
-      await device.connect()
-    })
-
-    describe('connect device', () => {
-      it('connect success', async () => {
-        const result = await hardwareControler.connectDevice(ledgerNanoS)
-        expect(result.status).toBe(ResponseCode.Success)
-      })
-
-      it('connect fail should throw connectDeviceFailed exception', async () => {
-        try {
-          await hardwareControler.connectDevice({
-            ...ledgerNanoS,
-            descriptor: '@throw me a error'
-          })
-        } catch (error) {
-          expect(error).toEqual(new connectDeviceFailed())
-        }
-      })
-    })
-
-    it('#getCkbAppVersion', async () => {
-      const { result } = await hardwareControler.getCkbAppVersion()
-      expect(result).toBe(LedgerCkbApp.version)
-     })
-
-     it('#getPublicKey', async () => {
-       const { result } = await hardwareControler.getPublicKey()
-       expect(result!.publicKey).toBe(LedgerCkbApp.publicKey)
-       expect(result!.chainCode).toBe(LedgerCkbApp.chainCode)
-     })
-
-     it('#detectDevice', async () => {
-      const { result } = await hardwareControler.detectDevice({
-        manufacturer: Manufacturer.Ledger,
-        product: 'Nano X'
-      })
-      expect(result).toEqual([LedgerNanoX])
-     })
-  })
 })
-
