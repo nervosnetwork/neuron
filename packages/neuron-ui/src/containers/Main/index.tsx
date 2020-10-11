d7f91d7da16fe55ff6118ebc974bbc7ededdab95
yuche <i@yuche.me>
2020-10-11 21:20:43 +0800

    fix: DAO and sudt not showing when offline sign popup

--- packages/neuron-ui/src/containers/Main/index.tsx
+++ packages/neuron-ui/src/containers/Main/index.tsx
@@ -93,7 +93,7 @@ export const mainContents: CustomRouter.Route[] = [
   {
     name: `NervosDAO`,
     path: RoutePath.NervosDAO,
-    exact: true,
+    exact: false,
     component: NervosDAO,
   },
   {
@@ -105,7 +105,7 @@ export const mainContents: CustomRouter.Route[] = [
   {
     name: `SUDTAccountList`,
     path: RoutePath.SUDTAccountList,
-    exact: true,
+    exact: false,
     component: SUDTAccountList,
   },
   {
