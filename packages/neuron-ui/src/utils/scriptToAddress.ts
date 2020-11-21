256438525d48b7fb6fe29f02fd752dc797fb3eb4
Keith <keithwhisper@gmail.com>
2020-11-21 14:09:08 +0800

    fix(ui): fix script to address
    1. add checking on args' length
    2. add anyone-can-pay into recognizable lock scripts

--- packages/neuron-ui/src/utils/scriptToAddress.ts
+++ packages/neuron-ui/src/utils/scriptToAddress.ts
@@ -5,7 +5,10 @@ export const scriptToAddress = (lock: CKBComponents.Script, isMainnet: boolean) 
   const addressPrefix = isMainnet ? ckbCore.utils.AddressPrefix.Mainnet : ckbCore.utils.AddressPrefix.Testnet
 
   const foundLock = [MultiSigLockInfo, DefaultLockInfo, AnyoneCanPayLockInfoOnAggron, AnyoneCanPayLockInfoOnLina].find(
-    info => lock.codeHash === info.CodeHash && lock.hashType === info.HashType
+    info =>
+      lock.codeHash === info.CodeHash &&
+      lock.hashType === info.HashType &&
+      info.ArgsLen.split(',').includes(`${(lock.args.length - 2) / 2}`)
   )
 
   if (foundLock) {
