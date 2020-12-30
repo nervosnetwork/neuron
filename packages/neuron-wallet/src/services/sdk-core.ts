7ac4d75a1896f300f146c2171c79d364037c7d4d
kata <kata.choi@gmail.com>
2020-12-30 10:53:06 +0800

    fix: avoid creating new http agents for every ckb sdk creation

--- packages/neuron-wallet/src/services/sdk-core.ts
+++ packages/neuron-wallet/src/services/sdk-core.ts
@@ -2,13 +2,14 @@ import CKB from '@nervosnetwork/ckb-sdk-core'
 import https from 'https'
 import http from 'http'
 
+const httpsAgent = new https.Agent({ keepAlive: true })
+const httpAgent = new http.Agent({ keepAlive: true })
+
 export const generateCKB = (url: string): CKB => {
   const ckb = new CKB(url)
   if (url.startsWith('https')) {
-    const httpsAgent = new https.Agent({ keepAlive: true })
     ckb.rpc.setNode({ url, httpsAgent })
   } else {
-    const httpAgent = new http.Agent({ keepAlive: true })
     ckb.rpc.setNode({ url, httpAgent })
   }
   return ckb
