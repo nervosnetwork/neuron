4db83c9b465799c7836d40dd9e6433f629bbec41
yuche <i@yuche.me>
2021-12-05 22:45:35 +0800

    feat: clear sync data

--- packages/neuron-wallet/src/controllers/node.ts
+++ packages/neuron-wallet/src/controllers/node.ts
@@ -1,10 +1,21 @@
-import { startCkbNode, stopCkbNode } from 'services/ckb-runner'
+import { startCkbNode, stopCkbNode, ckbDataPath } from 'services/ckb-runner'
 import { ResponseCode } from 'utils/const'
+import { deleteFolderRecursive } from 'block-sync-renderer/sync/indexer-folder-manager'
 
 export default class NodeController {
   public async startNode() {
     await startCkbNode()
 
+    return {
+      status: ResponseCode.Success,
+      result: true
+    }
+  }
+
+  public async clearCache() {
+    await this.stopNode()
+    deleteFolderRecursive(ckbDataPath())
+    await this.startNode()
     return {
       status: ResponseCode.Success,
       result: true
