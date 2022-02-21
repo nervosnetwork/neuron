b4e830c2e17d529df5896197a7a56bca7c3349a0
yanguoyu <841185308@qq.com>
2022-02-21 21:21:30 +0800

    use svg replace grommet-icons
    remove grommet-icons and  styled-components lib

--- packages/neuron-ui/src/theme.tsx
+++ packages/neuron-ui/src/theme.tsx
@@ -1,36 +1,30 @@
 import React from 'react'
-import { loadTheme, getTheme } from 'office-ui-fabric-react'
-import { ReactComponent as Explorer } from 'widgets/Icons/Explorer.svg'
-import { ReactComponent as Search } from 'widgets/Icons/Search.svg'
-import { ReactComponent as FirstPage } from 'widgets/Icons/FirstPage.svg'
-import { ReactComponent as PreviousPage } from 'widgets/Icons/PreviousPage.svg'
-import { ReactComponent as LastPage } from 'widgets/Icons/LastPage.svg'
-import { ReactComponent as NextPage } from 'widgets/Icons/NextPage.svg'
-import { ReactComponent as Matched } from 'widgets/Icons/Matched.svg'
-import { ReactComponent as Unmatched } from 'widgets/Icons/Unmatched.svg'
-import { ReactComponent as Close } from 'widgets/Icons/Close.svg'
-import { ReactComponent as More } from 'widgets/Icons/More.svg'
-import { ReactComponent as ArrowDown } from 'widgets/Icons/ArrowToNext.svg'
-
+import { loadTheme } from 'office-ui-fabric-react'
 import {
-  Alert as AlertIcon,
-  Checkmark as SuccessIcon,
-  CircleInformation as InfoIcon,
-  Close as DismissIcon,
-  Close as FailIcon,
-  Copy as CopyIcon,
-  FormClose as ClearIcon,
-  FormAdd as CreateIcon,
-  FormPreviousLink as LeaveIcon,
-  FormUp as ExpandIcon,
-  FormUpload as ImportIcon,
-  License as KeystoreIcon,
-  Nodes as ConnectedIcon,
-  Scan as ScanIcon,
-  SettingsOption as SettingsIcon,
-  Update as UpdateIcon,
-  Update as PendingIcon,
-} from 'grommet-icons'
+  Explorer,
+  Search,
+  FirstPage,
+  PreviousPage,
+  LastPage,
+  NextPage,
+  Matched,
+  InfoCircleOutlined,
+  Close,
+  More,
+  ArrowDown,
+  Alert,
+  Check,
+  CopyOutlined,
+  RightOutlined,
+  Plus,
+  ArrowLeftOutlined,
+  Connected,
+  PendingIcon,
+  Keystore,
+  Settings,
+  ScanOutlined,
+  UploadOutlined,
+} from 'widgets/Icons/icon'
 
 import { registerIcons } from 'utils/icons'
 
@@ -51,40 +45,39 @@ loadTheme({
   },
 })
 
-const theme = getTheme()
-const { semanticColors } = theme
-
 registerIcons({
   icons: {
-    info: <InfoIcon size="16px" />,
-    errorbadge: <AlertIcon size="16px" />,
-    completed: <SuccessIcon size="16px" />,
-    cancel: <DismissIcon size="16px" />,
-    MiniCopy: <CopyIcon size="small" color={semanticColors.primaryButtonBackground} />,
+    info: <InfoCircleOutlined />,
+    errorbadge: <Alert />,
+    completed: <Check />,
+    cancel: <Close />,
+    MiniCopy: <CopyOutlined type="activity" />,
     Search: <Search />,
-    FirstPage: <FirstPage />,
-    LastPage: <LastPage />,
-    PrevPage: <PreviousPage />,
-    NextPage: <NextPage />,
+    FirstPage: <FirstPage type="activity" />,
+    LastPage: <LastPage type="activity" />,
+    PrevPage: <PreviousPage type="activity" />,
+    NextPage: <NextPage type="activity" />,
     ArrowDown: <ArrowDown />,
-    ChevronRightMed: <ExpandIcon size="16px" style={{ transform: 'rotate(90deg) translate(2px, 0px)' }} />,
-    Scan: <ScanIcon />,
-    Import: <ImportIcon color="white" />,
-    Create: <CreateIcon />,
-    Clear: <ClearIcon size="16px" />,
+    ChevronRightMed: <RightOutlined />,
+    Scan: <ScanOutlined />,
+    Import: <UploadOutlined />,
+    Create: <Plus />,
+    Clear: <Close style={{ height: '14px', width: '14px' }} />,
     Dismiss: <Close />,
-    Leave: <LeaveIcon />,
-    Connected: <ConnectedIcon size="small" color="green" />,
-    Disconnected: <AlertIcon size="small" color="red" />,
-    Updating: <UpdateIcon size="16px" style={{ animation: 'rotate360 3s linear infinite' }} />,
+    Leave: <ArrowLeftOutlined />,
+    Connected: <Connected />,
+    Disconnected: <Alert type="error" />,
+    Updating: <PendingIcon style={{ animation: 'rotate360 3s linear infinite' }} />,
     More: <More />,
     Matched: <Matched />,
-    Unmatched: <Unmatched />,
-    TransactionSuccess: <SuccessIcon size="14px" color="green" />,
-    TransactionFailure: <FailIcon size="14px" color="#d50000" />,
-    TransactionPending: <PendingIcon size="14px" style={{ animation: 'rotate360 3s linear infinite' }} />,
-    Keystore: <KeystoreIcon color="white" style={{ transform: 'scale(0.6)' }} />,
-    Settings: <SettingsIcon size="20px" />,
+    Unmatched: <InfoCircleOutlined type="error" />,
+    TransactionSuccess: <Check type="success" />,
+    TransactionFailure: <Close type="error" />,
+    TransactionPending: (
+      <PendingIcon style={{ animation: 'rotate360 3s linear infinite', width: '14px', height: '14px' }} />
+    ),
+    Keystore: <Keystore />,
+    Settings: <Settings />,
     Explorer: <Explorer />,
   },
 })
