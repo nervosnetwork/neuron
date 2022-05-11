90863b4f99d8510efe9ad3d81cf8bfeb8d358ef9
qiweiii <yangqiwei97@gmail.com>
2022-05-11 13:58:02 +0800

    chore(lint): react-script eslint fix

--- packages/neuron-ui/.eslintrc.js
+++ packages/neuron-ui/.eslintrc.js
@@ -1,86 +1,150 @@
 module.exports = {
-  "extends": ["airbnb", "plugin:prettier/recommended"],
-  "parser": "@typescript-eslint/parser",
-  "plugins": ["@typescript-eslint"],
-  "settings": {
-    "import/resolver": {
-      "node": {
-        "paths": ["src"],
-        "extensions": [".js", ".ts", ".jsx", ".tsx"]
-      }
+  extends: ['airbnb', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.ts', '.jsx', '.tsx'],
+      },
     },
-    "react": {
-      "version": "detect"
-    }
+    react: {
+      version: 'detect',
+    },
   },
-  "rules": {
-    "prettier/prettier": [2, {
-      "printWidth": 120
-    }],
-    "semi": [2, "never"],
-    "curly": [2, "all"],
-    "comma-dangle": [2, {
-      "arrays": "always-multiline",
-      "objects": "always-multiline",
-      "imports": "always-multiline",
-      "exports": "always-multiline",
-      "functions": "ignore"
-    }],
-    "import/no-extraneous-dependencies": [2, {
-      "devDependencies": true
-    }],
-    "no-unused-vars": "off",
-    "implicit-arrow-linebreak": "off",
-    "@typescript-eslint/no-unused-vars": ["error", {
-      "vars": "local",
-      "args": "after-used",
-      "ignoreRestSiblings": false
-    }],
-    "arrow-parens": [2, "as-needed"],
-    "max-len": [2, {
-      "code": 120,
-      "ignoreComments": true,
-      "ignoreTrailingComments": true,
-      "ignoreUrls": true,
-      "ignoreStrings": true,
-      "ignoreTemplateLiterals": true,
-      "ignoreRegExpLiterals": true,
-    }],
-    "object-curly-newline": ["error", {
-      "ObjectExpression": {
-        "consistent": true
+  rules: {
+    'prettier/prettier': [
+      2,
+      {
+        printWidth: 120,
       },
-      "ObjectPattern": {
-        "consistent": true
+    ],
+    semi: [2, 'never'],
+    curly: [2, 'all'],
+    'comma-dangle': [
+      2,
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'ignore',
       },
-      "ImportDeclaration": {
-        "consistent": true,
+    ],
+    'import/no-extraneous-dependencies': [
+      2,
+      {
+        devDependencies: true,
       },
-      "ExportDeclaration": {
-        "multiline": true,
-        "minProperties": 3
-      }
-    }],
-    "no-plusplus": [0],
-    "lines-between-class-members": ["error", "always", { exceptAfterSingleLine: true }],
-    "max-classes-per-file": [0],
-    "react/jsx-filename-extension": [1, {
-      "extensions": [".ts", ".tsx"]
-    }],
-    "react/jsx-props-no-spreading": [0],
-    "typescript-eslint/no-angle-bracket-type-assertion": [0],
-    "no-alert": [0],
-    "no-console": [2, {
-      "allow": ["info", "warn", "error", "group", "groupEnd"]
-    }],
-    "no-bitwise": [0]
+    ],
+    'no-unused-vars': 'off',
+    'implicit-arrow-linebreak': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        vars: 'local',
+        args: 'after-used',
+        ignoreRestSiblings: false,
+      },
+    ],
+    'arrow-parens': [2, 'as-needed'],
+    'max-len': [
+      2,
+      {
+        code: 120,
+        ignoreComments: true,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+    'object-curly-newline': [
+      'error',
+      {
+        ObjectExpression: {
+          consistent: true,
+        },
+        ObjectPattern: {
+          consistent: true,
+        },
+        ImportDeclaration: {
+          consistent: true,
+        },
+        ExportDeclaration: {
+          multiline: true,
+          minProperties: 3,
+        },
+      },
+    ],
+    'no-plusplus': [0],
+    'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
+    'max-classes-per-file': [0],
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.ts', '.tsx'],
+      },
+    ],
+    'react/jsx-props-no-spreading': [0],
+    'typescript-eslint/no-angle-bracket-type-assertion': [0],
+    'no-alert': [0],
+    'no-console': [
+      2,
+      {
+        allow: ['info', 'warn', 'error', 'group', 'groupEnd'],
+      },
+    ],
+    'no-bitwise': [0],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+    '@typescript-eslint/no-var-requires': 'off',
+    'react/prop-types': 'off',
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'react/require-default-props': 'off',
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        extendDefaults: true,
+        types: {
+          '{}': false,
+          Function: false,
+        },
+      },
+    ],
+    'default-param-last': 'off',
+    'no-unsafe-optional-chaining': 'warn',
   },
-  "env": {
-    "jest": true,
-    "node": true,
-    "browser": true
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'no-undef': 'off',
+      },
+    },
+  ],
+  env: {
+    jest: true,
+    node: true,
+    browser: true,
   },
-  "globals": {
-    "BigInt": "readonly"
+  globals: {
+    BigInt: 'readonly',
   },
 }
