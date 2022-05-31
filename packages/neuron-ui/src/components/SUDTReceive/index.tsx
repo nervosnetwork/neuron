c2e42df9733f0c335049487f260b65a532d5d072
Keith <keithwhisper@gmail.com>
2022-05-31 21:22:01 +0800

    feat: add address format toggle in receive tabs
    Add a button next to the address in receive tabs to toggle
    address foramts between full version and deprecated version.
    
    The switch is designed to be neglectable because the deprecated
    format is discouraged.
    
    Ref: https://github.com/Magickbase/neuron-public-issues/issues/22

--- packages/neuron-ui/src/components/SUDTReceive/index.tsx
+++ packages/neuron-ui/src/components/SUDTReceive/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
+import { addressToScript, bech32Address, AddressPrefix } from '@nervosnetwork/ckb-sdk-utils'
 import SUDTAvatar from 'widgets/SUDTAvatar'
+import { ReactComponent as AddressToggleIcon } from 'widgets/Icons/AddressTransform.svg'
 import { ReactComponent as Attention } from 'widgets/Icons/Attention.svg'
 import Breadcrum from 'widgets/Breadcrum'
 import QRCode from 'widgets/QRCode'
@@ -13,17 +15,32 @@ import { RoutePath, CONSTANTS } from 'utils'
 import styles from './sUDTReceive.module.scss'
 
 const { DEFAULT_SUDT_FIELDS } = CONSTANTS
+const toShortAddr = (addr: string) => {
+  try {
+    const script = addressToScript(addr)
+    const isMainnet = addr.startsWith('ckb')
+    return bech32Address(script.args, {
+      prefix: isMainnet ? AddressPrefix.Mainnet : AddressPrefix.Testnet,
+      codeHashOrCodeHashIndex: '0x02',
+    })
+  } catch {
+    return ''
+  }
+}
 
 const SUDTReceive = () => {
   const dispatch = useDispatch()
   const [t] = useTranslation()
   const { search } = useLocation()
+  const [isInShortFormat, setIsInShortFormat] = useState(false)
   const { address, accountName, tokenName, symbol } = Object.fromEntries([...new URLSearchParams(search)])
   const breakcrum = [{ label: t('navbar.s-udt'), link: RoutePath.SUDTAccountList }]
 
   if (!address) {
     return <div>Not Found</div>
   }
+
+  const displayedAddr = isInShortFormat ? toShortAddr(address) : address
 
   return (
     <div
@@ -41,11 +58,19 @@ const SUDTReceive = () => {
         <div className={styles.accountName}>{accountName || DEFAULT_SUDT_FIELDS.accountName}</div>
         <div className={styles.tokenName}>{tokenName || DEFAULT_SUDT_FIELDS.tokenName}</div>
       </div>
-      <QRCode value={address} size={220} includeMargin dispatch={dispatch} />
+      <QRCode value={displayedAddr} size={220} includeMargin dispatch={dispatch} />
       <div className={styles.address}>
-        <CopyZone content={address} name={t('receive.copy-address')} style={{ lineHeight: '1.625rem' }}>
-          {`${address.slice(0, 30)}...${address.slice(-30)}`}
+        <CopyZone content={displayedAddr} name={t('receive.copy-address')} style={{ lineHeight: '1.625rem' }}>
+          {displayedAddr}
         </CopyZone>
+        <button
+          type="button"
+          className={styles.addressToggle}
+          onClick={() => setIsInShortFormat(is => !is)}
+          title={t(isInShortFormat ? `receive.turn-into-full-version-fomrat` : `receive.turn-into-deprecated-format`)}
+        >
+          <AddressToggleIcon />
+        </button>
       </div>
       <p className={styles.notation}>
         <Attention />
