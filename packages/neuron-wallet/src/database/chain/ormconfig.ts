1925bb0c15bebfe83c0ac3877b6a37589589a615
yanguoyu <841185308@qq.com>
2022-07-04 18:26:21 +0800

    fix: Execute remove duplicate migrate again

--- packages/neuron-wallet/src/database/chain/ormconfig.ts
+++ packages/neuron-wallet/src/database/chain/ormconfig.ts
@@ -45,7 +45,7 @@ import { AddAddressDescription1602543179168 } from './migrations/1602543179168-A
 import { AddMultisigConfig1646817547204 } from './migrations/1646817547204-AddMultisigConfig'
 import { AddMultisigOutput1649729996969 } from './migrations/1649729996969-AddMultisigOutput'
 import { UpdateAddressDescription1650984779265 } from './migrations/1650984779265-UpdateAddressDescription'
-import { RemoveDuplicateBlake160s1651133703535 } from './migrations/1651133703535-RemoveDuplicateBlake160s'
+import { RemoveDuplicateBlake160s1656930265386 } from './migrations/1656930265386-RemoveDuplicateBlake160s'
 import { UpdateOutputChequeLockHash1652945662504 } from './migrations/1652945662504-UpdateOutputChequeLockHash'
 import { RemoveAddressesMultisigConfig1651820157100 } from './migrations/1651820157100-RemoveAddressesMultisigConfig'
 
@@ -109,7 +109,7 @@ const connectOptions = async (genesisBlockHash: string): Promise<SqliteConnectio
       AddMultisigConfig1646817547204,
       AddMultisigOutput1649729996969,
       UpdateAddressDescription1650984779265,
-      RemoveDuplicateBlake160s1651133703535,
+      RemoveDuplicateBlake160s1656930265386,
       UpdateOutputChequeLockHash1652945662504,
       RemoveAddressesMultisigConfig1651820157100
     ],
