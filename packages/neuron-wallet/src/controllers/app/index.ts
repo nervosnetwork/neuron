a848998fbe0875a2e659caebbf7431368caa7cc5
yanguoyu <841185308@qq.com>
2023-01-06 16:42:32 +0800

    fix: Remove old indexer data and fix typo.

--- packages/neuron-wallet/src/controllers/app/index.ts
+++ packages/neuron-wallet/src/controllers/app/index.ts
@@ -15,6 +15,7 @@ import SyncApiController from 'controllers/sync-api'
 import { SETTINGS_WINDOW_TITLE } from 'utils/const'
 import { stopCkbNode } from 'services/ckb-runner'
 import startMonitor from 'services/monitor'
+import IndexerService from 'services/indexer'
 
 const app = electronApp
 
@@ -52,6 +53,8 @@ export default class AppController {
     await this.openWindow()
 
     startMonitor()
+
+    IndexerService.cleanOldIndexerData()
   }
 
   /**
