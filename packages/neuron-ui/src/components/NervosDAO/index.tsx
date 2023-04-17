4c6ef294dbca328d5660f77d4a31d6c9fb50135a
严国宇 <841185308@qq.com>
2023-04-17 20:42:43 +0800

    fix: Fix when enter over 8 decimal crash. (#2629)

--- packages/neuron-ui/src/components/NervosDAO/index.tsx
+++ packages/neuron-ui/src/components/NervosDAO/index.tsx
@@ -247,8 +247,14 @@ const NervosDAO = () => {
   }, [])
 
   useEffect(() => {
-    if (BigInt(CKBToShannonFormatter(depositValue)) > maxDepositAmount) {
-      setDepositValue(shannonToCKBFormatter(`${maxDepositAmount}`, false, ''))
+    try {
+      if (BigInt(CKBToShannonFormatter(depositValue)) > maxDepositAmount) {
+        setDepositValue(shannonToCKBFormatter(`${maxDepositAmount}`, false, ''))
+      }
+    } catch (error) {
+      // ignore error
+      // When the depositValue is invalid, it displays the error in the textField, but it will throw an exception when valid wheater it's big than the max deposit value
+      // and when the depositValue is invalid, it's no need to set max depositValue.
     }
   }, [maxDepositAmount, depositValue, setDepositValue])
 
