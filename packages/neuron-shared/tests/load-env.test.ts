f1b03a777db750c241d8785fda849de97bf11e74
FrankFang <frankfang1990@gmail.com>
2023-06-07 12:07:48 +0800

    rename TEST_ENV to TEST_VAR

--- packages/neuron-shared/tests/load-env.test.ts
+++ packages/neuron-shared/tests/load-env.test.ts
@@ -5,13 +5,13 @@ import { loadEnv } from "../src/load-env"
 jest.mock('fs')
 
 const contentMap: Record<string, string> = {
-  '.env': 'TEST_ENV="from .env"',
-  '.env.test': 'TEST_ENV="from .env.test"',
-  '.env.test.local': 'TEST_ENV="from .env.test.local"',
-  '.env.development': 'TEST_ENV="from .env.development"',
-  '.env.development.local': 'TEST_ENV="from .env.development.local"',
-  '.env.production': 'TEST_ENV="from .env.production"',
-  '.env.production.local': 'TEST_ENV="from .env.production.local"',
+  '.env': 'TEST_VAR="from .env"',
+  '.env.test': 'TEST_VAR="from .env.test"',
+  '.env.test.local': 'TEST_VAR="from .env.test.local"',
+  '.env.development': 'TEST_VAR="from .env.development"',
+  '.env.development.local': 'TEST_VAR="from .env.development.local"',
+  '.env.production': 'TEST_VAR="from .env.production"',
+  '.env.production.local': 'TEST_VAR="from .env.production.local"',
 }
 
 describe("Load Env", () => {
@@ -31,68 +31,68 @@ describe("Load Env", () => {
   describe("in test environment", () => {
     beforeEach(() => {
       process.env.NODE_ENV = 'test'
-      delete process.env.TEST_ENV
+      delete process.env.TEST_VAR
     })
     it(".env", () => {
       fs.existsSync = jest.fn().mockImplementation((filepath) => {
         return ['.env'].includes(filepath)
       })
       loadEnv()
-      expect(process.env.TEST_ENV).toEqual('from .env')
+      expect(process.env.TEST_VAR).toEqual('from .env')
     })
     it(".env.test > .env", () => {
       fs.existsSync = jest.fn().mockImplementation((filepath) => {
         return ['.env.test', '.env'].includes(filepath)
       })
       loadEnv()
-      expect(process.env.TEST_ENV).toEqual('from .env.test')
+      expect(process.env.TEST_VAR).toEqual('from .env.test')
     })
     it(".env.test.local > .env.test > .env", () => {
       fs.existsSync = jest.fn().mockImplementation((filepath) => {
         return ['.env.test.local', '.env.test', '.env'].includes(filepath)
       })
       loadEnv()
-      expect(process.env.TEST_ENV).toEqual('from .env.test.local')
+      expect(process.env.TEST_VAR).toEqual('from .env.test.local')
     })
     it("does not throw error when no env file exists", () => {
       fs.existsSync = jest.fn().mockImplementation((filepath) => {
         return !(['.env.test.local', '.env.test', '.env'].includes(filepath))
       })
-      expect(process.env.TEST_ENV).toBeUndefined()
+      expect(process.env.TEST_VAR).toBeUndefined()
       expect(() => loadEnv()).not.toThrow()
     })
   })
   describe("in development environment", () => {
     beforeEach(() => {
       process.env.NODE_ENV = 'development'
-      delete process.env.TEST_ENV
+      delete process.env.TEST_VAR
     })
     it(".env", () => {
       fs.existsSync = jest.fn().mockImplementation((filepath) => {
         return ['.env'].includes(filepath)
       })
       loadEnv()
-      expect(process.env.TEST_ENV).toEqual('from .env')
+      expect(process.env.TEST_VAR).toEqual('from .env')
     })
     it(".env.development > .env", () => {
       fs.existsSync = jest.fn().mockImplementation((filepath) => {
         return ['.env.development', '.env'].includes(filepath)
       })
       loadEnv()
-      expect(process.env.TEST_ENV).toEqual('from .env.development')
+      expect(process.env.TEST_VAR).toEqual('from .env.development')
     })
     it(".env.development.local > .env.development > .env", () => {
       fs.existsSync = jest.fn().mockImplementation((filepath) => {
         return ['.env.development.local', '.env.development', '.env'].includes(filepath)
       })
       loadEnv()
-      expect(process.env.TEST_ENV).toEqual('from .env.development.local')
+      expect(process.env.TEST_VAR).toEqual('from .env.development.local')
     })
     it("does not throw error when no env file exists", () => {
       fs.existsSync = jest.fn().mockImplementation((filepath) => {
         return !(['.env.development.local', '.env.development', '.env'].includes(filepath))
       })
-      expect(process.env.TEST_ENV).toBeUndefined()
+      expect(process.env.TEST_VAR).toBeUndefined()
       expect(() => loadEnv()).not.toThrow()
     })
   })
@@ -100,34 +100,34 @@ describe("Load Env", () => {
   describe("in production environment", () => {
     beforeEach(() => {
       process.env.NODE_ENV = 'production'
-      delete process.env.TEST_ENV
+      delete process.env.TEST_VAR
     })
     it(".env", () => {
       fs.existsSync = jest.fn().mockImplementation((filepath) => {
         return ['.env'].includes(filepath)
       })
       loadEnv()
-      expect(process.env.TEST_ENV).toEqual('from .env')
+      expect(process.env.TEST_VAR).toEqual('from .env')
     })
     it(".env.production > .env", () => {
       fs.existsSync = jest.fn().mockImplementation((filepath) => {
         return ['.env.production', '.env'].includes(filepath)
       })
       loadEnv()
-      expect(process.env.TEST_ENV).toEqual('from .env.production')
+      expect(process.env.TEST_VAR).toEqual('from .env.production')
     })
     it(".env.production.local > .env.production > .env", () => {
       fs.existsSync = jest.fn().mockImplementation((filepath) => {
         return ['.env.production.local', '.env.production', '.env'].includes(filepath)
       })
       loadEnv()
-      expect(process.env.TEST_ENV).toEqual('from .env.production.local')
+      expect(process.env.TEST_VAR).toEqual('from .env.production.local')
     })
     it("does not throw error when no env file exists", () => {
       fs.existsSync = jest.fn().mockImplementation((filepath) => {
         return !(['.env.production.local', '.env.production', '.env'].includes(filepath))
       })
-      expect(process.env.TEST_ENV).toBeUndefined()
+      expect(process.env.TEST_VAR).toBeUndefined()
       expect(() => loadEnv()).not.toThrow()
     })
   })
