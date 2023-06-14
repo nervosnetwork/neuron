54988d943825534a5af2fea18141245823d95819
Jun Ma <ma82180597@163.com>
2023-06-14 17:20:17 +0800

    fix: issues for history page

--- packages/neuron-ui/src/components/LockInfoDialog/index.tsx
+++ packages/neuron-ui/src/components/LockInfoDialog/index.tsx
@@ -1,12 +1,9 @@
 import { useTranslation } from 'react-i18next'
-import React, { useState, useRef } from 'react'
+import React, { useRef } from 'react'
 import { bech32Address, AddressPrefix } from '@nervosnetwork/ckb-sdk-utils'
 import CopyZone from 'widgets/CopyZone'
 import Dialog from 'widgets/Dialog'
-import { ReactComponent as Copy } from 'widgets/Icons/TinyCopy.svg'
-import { ReactComponent as Check } from 'widgets/Icons/Check.svg'
 import { useDialog } from 'utils'
-import { onEnter } from 'utils/inputDevice'
 import getLockSupportShortAddress from '../../utils/getLockSupportShortAddress'
 
 import styles from './lockInfoDialog.module.scss'
@@ -40,7 +37,7 @@ const ShortAddr = ({ lockScript, isMainnet }: { lockScript: CKBComponents.Script
         {t('transaction.deprecated-address-format')}
       </div>
       <div className={styles.shortAddr}>
-        <CopyZone content={shortAddr} name={t('history.copy-address')}>
+        <CopyZone content={shortAddr}>
           <span>{shortAddr}</span>
         </CopyZone>
       </div>
@@ -50,11 +47,8 @@ const ShortAddr = ({ lockScript, isMainnet }: { lockScript: CKBComponents.Script
 
 const LockInfoDialog = ({ lockInfo, isMainnet, onDismiss }: LockInfoDialogProps) => {
   const [t] = useTranslation()
-  const [copied, setCopied] = useState(false)
   const dialogRef = useRef<HTMLDialogElement | null>(null)
   useDialog({ show: !!lockInfo, dialogRef, onClose: onDismiss })
-
-  const timer = useRef<ReturnType<typeof setTimeout>>()
 
   if (!lockInfo) {
     return null
@@ -66,16 +60,6 @@ const LockInfoDialog = ({ lockInfo, isMainnet, onDismiss }: LockInfoDialogProps)
     "args": "${lockInfo.args}"
   }`
 
-  const handleCopy = () => {
-    setCopied(true)
-    window.navigator.clipboard.writeText(rawLock)
-
-    clearTimeout(timer.current!)
-    timer.current = setTimeout(() => {
-      setCopied(false)
-    }, 1000)
-  }
-
   return (
     <Dialog
       show={Boolean(lockInfo)}
@@ -84,20 +68,17 @@ const LockInfoDialog = ({ lockInfo, isMainnet, onDismiss }: LockInfoDialogProps)
       onCancel={onDismiss}
       showFooter={false}
     >
-      <>
-        <div className={styles.container}>
-          <div title={t('transaction.lock-script')} className={`${styles.title} ${styles.lockScriptTitle}`}>
-            {t('transaction.lock-script')}
-          </div>
-          <div className={styles.lock}>
-            <div className={styles.copyBtn} onClick={handleCopy} onKeyPress={onEnter(handleCopy)} role="none">
-              {copied ? <Check /> : <Copy />}
-            </div>
-            <pre>{rawLock}</pre>
-          </div>
-          <ShortAddr isMainnet={isMainnet} lockScript={lockInfo} />
+      <div className={styles.container}>
+        <div title={t('transaction.lock-script')} className={`${styles.title} ${styles.lockScriptTitle}`}>
+          {t('transaction.lock-script')}
         </div>
-      </>
+        <div className={styles.lock}>
+          <CopyZone content={rawLock}>
+            <pre>{rawLock}</pre>
+          </CopyZone>
+        </div>
+        <ShortAddr isMainnet={isMainnet} lockScript={lockInfo} />
+      </div>
     </Dialog>
   )
 }
