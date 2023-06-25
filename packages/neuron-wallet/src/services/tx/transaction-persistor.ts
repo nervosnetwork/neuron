c89bd54fc94e4f9f86a1524a231fabf3bd982ef6
严国宇 <841185308@qq.com>
2023-06-25 19:47:49 +0800

    refactor: Optimize input and output storage (#2672)
    1. Storage others' cells tx and locks into tx-lock for search others' addresses.
    2. Get transaction detail by rpc.

--- packages/neuron-wallet/src/services/tx/transaction-persistor.ts
+++ packages/neuron-wallet/src/services/tx/transaction-persistor.ts
@@ -9,6 +9,8 @@ import OutPoint from '../../models/chain/out-point'
 import Output, { OutputStatus } from '../../models/chain/output'
 import Transaction, { TransactionStatus } from '../../models/chain/transaction'
 import Input from '../../models/chain/input'
+import TxLockEntity from '../../database/chain/entities/tx-lock'
+import { CHEQUE_ARGS_LENGTH, DEFAULT_ARGS_LENGTH } from '../../utils/const'
 
 export enum TxSaveType {
   Sent = 'sent',
@@ -42,7 +44,10 @@ export class TransactionPersistor {
   // After the tx is fetched:
   // 1. If the tx is not persisted before fetching, output = live, input = dead
   // 2. If the tx is already persisted before fetching, output = live, input = dead
-  private static saveWithFetch = async (transaction: Transaction): Promise<TransactionEntity> => {
+  private static saveWithFetch = async (
+    transaction: Transaction,
+    lockArgsSetNeedsDetail?: Set<string>
+  ): Promise<TransactionEntity> => {
     const connection = getConnection()
     const txEntity: TransactionEntity | undefined = await connection
       .getRepository(TransactionEntity)
@@ -190,14 +195,15 @@ export class TransactionPersistor {
       return txEntity
     }
 
-    return TransactionPersistor.create(transaction, OutputStatus.Live, OutputStatus.Dead)
+    return TransactionPersistor.create(transaction, OutputStatus.Live, OutputStatus.Dead, lockArgsSetNeedsDetail)
   }
 
   // only create, check exist before this
   public static create = async (
     transaction: Transaction,
     outputStatus: OutputStatus,
-    inputStatus: OutputStatus
+    inputStatus: OutputStatus,
+    lockArgsSetNeedsDetail?: Set<string>
   ): Promise<TransactionEntity> => {
     const connection = getConnection()
     const tx = new TransactionEntity()
@@ -297,22 +303,30 @@ export class TransactionPersistor {
       }
       return output
     })
+    let willSaveDetailInputs: InputEntity[] = inputs
+    let willSaveDetailOutputs: OutputEntity[] = outputs
+    let txLocks: TxLockEntity[] = []
+    if (lockArgsSetNeedsDetail?.size) {
+      willSaveDetailInputs = inputs.filter(v => this.shouldSaveDetail(v, lockArgsSetNeedsDetail))
+      willSaveDetailOutputs = outputs.filter(v => this.shouldSaveDetail(v, lockArgsSetNeedsDetail))
+      txLocks = [
+        ...new Set([
+          ...inputs.filter(v => v.lockHash && !this.shouldSaveDetail(v, lockArgsSetNeedsDetail)).map(v => v.lockHash!),
+          ...outputs.filter(v => !this.shouldSaveDetail(v, lockArgsSetNeedsDetail)).map(v => v.lockHash),
+        ]),
+      ].map(v => TxLockEntity.fromObject({ txHash: tx.hash, lockHash: v }))
+    }
 
-    const sliceSize = 100
+    const chunk = 100
     const queryRunner = connection.createQueryRunner()
     await TransactionPersistor.waitUntilTransactionFinished(queryRunner)
     await queryRunner.startTransaction()
     try {
       await queryRunner.manager.save(tx)
-      for (const slice of ArrayUtils.eachSlice(inputs, sliceSize)) {
-        await queryRunner.manager.save(slice)
-      }
-      for (const slice of ArrayUtils.eachSlice(previousOutputs, sliceSize)) {
-        await queryRunner.manager.save(slice)
-      }
-      for (const slice of ArrayUtils.eachSlice(outputs, sliceSize)) {
-        await queryRunner.manager.save(slice)
-      }
+      await queryRunner.manager.save(willSaveDetailInputs, { chunk })
+      await queryRunner.manager.save(previousOutputs, { chunk })
+      await queryRunner.manager.save(willSaveDetailOutputs, { chunk })
+      await queryRunner.manager.save(txLocks, { chunk })
       await queryRunner.commitTransaction()
     } catch (err) {
       logger.error('Database:\tcreate transaction error:', err)
@@ -342,7 +356,8 @@ export class TransactionPersistor {
   // when fetch a transaction, use TxSaveType.Fetch
   public static convertTransactionAndSave = async (
     transaction: Transaction,
-    saveType: TxSaveType
+    saveType: TxSaveType,
+    lockArgsSetNeedsDetail?: Set<string>
   ): Promise<TransactionEntity> => {
     const tx: Transaction = transaction
 
@@ -350,17 +365,21 @@ export class TransactionPersistor {
     if (saveType === TxSaveType.Sent) {
       txEntity = await TransactionPersistor.saveWithSent(tx)
     } else if (saveType === TxSaveType.Fetch) {
-      txEntity = await TransactionPersistor.saveWithFetch(tx)
+      txEntity = await TransactionPersistor.saveWithFetch(tx, lockArgsSetNeedsDetail)
     } else {
       throw new Error('Error TxSaveType!')
     }
     return txEntity
   }
 
-  public static saveFetchTx = async (transaction: Transaction): Promise<TransactionEntity> => {
+  public static saveFetchTx = async (
+    transaction: Transaction,
+    lockArgsSetNeedsDetail?: Set<string>
+  ): Promise<TransactionEntity> => {
     const txEntity: TransactionEntity = await TransactionPersistor.convertTransactionAndSave(
       transaction,
-      TxSaveType.Fetch
+      TxSaveType.Fetch,
+      lockArgsSetNeedsDetail
     )
     return txEntity
   }
@@ -372,6 +391,17 @@ export class TransactionPersistor {
     })
     const txEntity: TransactionEntity = await TransactionPersistor.convertTransactionAndSave(tx, TxSaveType.Sent)
     return txEntity
+  }
+
+  private static shouldSaveDetail(cell: InputEntity | OutputEntity, lockArgsSetNeedsDetail: Set<string>) {
+    return (
+      cell.lockArgs &&
+      (lockArgsSetNeedsDetail.has(cell.lockArgs) ||
+        (cell.lockArgs.length === CHEQUE_ARGS_LENGTH &&
+          [cell.lockArgs.slice(0, DEFAULT_ARGS_LENGTH), `0x${cell.lockArgs.slice(DEFAULT_ARGS_LENGTH)}`].some(v =>
+            lockArgsSetNeedsDetail.has(v)
+          )))
+    )
   }
 }
 
