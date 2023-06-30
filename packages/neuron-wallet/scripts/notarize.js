4d2f9b9df297a8ac54b6a810742aa7315b91d0b3
yanguoyu <841185308@qq.com>
2023-06-30 10:40:57 +0800

    chore: prettier notarize.js

--- packages/neuron-wallet/scripts/notarize.js
+++ packages/neuron-wallet/scripts/notarize.js
@@ -1,7 +1,7 @@
 const { notarize } = require('@electron/notarize')
 
 exports.default = async function notarizing(context) {
-  const { electronPlatformName, appOutDir } = context;
+  const { electronPlatformName, appOutDir } = context
   if (electronPlatformName !== 'darwin') {
     return
   }
@@ -11,18 +11,18 @@ exports.default = async function notarizing(context) {
     return Promise.resolve('skip')
   }
 
-  const appleId = process.env.APPLE_ID;
-  const appleIdPassword = process.env.APPLE_ID_PASSWORD;
-  const teamId = process.env.TEAM_ID;
+  const appleId = process.env.APPLE_ID
+  const appleIdPassword = process.env.APPLE_ID_PASSWORD
+  const teamId = process.env.TEAM_ID
 
   if (!appleId || !appleIdPassword || !teamId) {
-    console.warn(`${appleId ? (appleIdPassword ? "Team id" : "Apple id password") : "Apple id"} is not found`)
+    console.warn(`${appleId ? (appleIdPassword ? 'Team id' : 'Apple id password') : 'Apple id'} is not found`)
     process.exit(1)
   }
 
   const appName = context.packager.appInfo.productFilename
 
-  console.info("Notarization started")
+  console.info('Notarization started')
 
   try {
     await notarize({
@@ -32,7 +32,7 @@ exports.default = async function notarizing(context) {
       appleIdPassword,
       teamId,
     })
-    console.info("Notarization finished")
+    console.info('Notarization finished')
   } catch (err) {
     console.error(err)
     process.exit(1)
