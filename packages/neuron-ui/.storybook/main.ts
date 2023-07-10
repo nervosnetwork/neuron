92952766804771f76434b5b965def1b66313233c
yanguoyu <841185308@qq.com>
2023-07-10 11:42:39 +0800

    fix: Add .story directory to eslint.

--- packages/neuron-ui/.storybook/main.ts
+++ packages/neuron-ui/.storybook/main.ts
@@ -1,18 +1,24 @@
 export default {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/preset-create-react-app", "storybook-addon-react-router-v6"],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/preset-create-react-app',
+    'storybook-addon-react-router-v6',
+  ],
   framework: {
-    name: "@storybook/react-webpack5",
-    options: {}
+    name: '@storybook/react-webpack5',
+    options: {},
   },
   webpackFinal: config => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      electron: require.resolve('./electron')
-    };
-    return config;
+      electron: require.resolve('./electron'),
+    }
+    return config
   },
   docs: {
-    autodocs: true
+    autodocs: true,
   },
 }
