3b90931b63605536150ca4a85c2c29cc5b4a54c9
yanguoyu <841185308@qq.com>
2023-10-16 14:14:44 +0800

    fix: Format date when output log. Catch exception when export public key info.

--- packages/neuron-wallet/src/utils/logger.ts
+++ packages/neuron-wallet/src/utils/logger.ts
@@ -4,7 +4,12 @@ import env from '../env'
 if (!env.isDevMode) {
   logger.transports.file.level = 'info'
 }
-
+logger.transports.file.format = ({ date, level, data }) => {
+  return `[${date.toISOString()}] [${level}] ${data}`
+}
+logger.transports.console.format = ({ date, level, data }) => {
+  return `[${date.toISOString()}] [${level}] ${data}`
+}
 // logger.catchErrors({ showDialog: false })
 
 export default logger
