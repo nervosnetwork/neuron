0a47f354e3fc4ff6a5c5937f313e46a237a6edd3
Chen Yu <keithwhisper@gmail.com>
2023-10-27 03:58:28 +0900

    Update packages/neuron-wallet/scripts/customSign.js

--- packages/neuron-wallet/scripts/customSign.js
+++ packages/neuron-wallet/scripts/customSign.js
@@ -2,7 +2,7 @@ const { execSync } = require('node:child_process')
 
 exports.default = async configuration => {
   if (!configuration.path) {
-    throw new Error(`Configuration is required`)
+    throw new Error(`Path of application is not found`)
   }
 
   execSync(`smctl sign --keypair-alias="${process.env.SM_KEYPAIR_NAME}" --input "${String(configuration.path)}"`, {
