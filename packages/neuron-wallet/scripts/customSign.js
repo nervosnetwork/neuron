0abb49752497d973900595434e910692c5413a1e
Keith <keithwhisper@gmail.com>
2023-10-27 12:13:46 +0900

    ci: skip connecting to key locker if api token is not found

--- packages/neuron-wallet/scripts/customSign.js
+++ packages/neuron-wallet/scripts/customSign.js
@@ -1,6 +1,11 @@
 const { execSync } = require('node:child_process')
 
 exports.default = async configuration => {
+  if (!process.env.SM_API_KEY) {
+    console.info(`Skip signing because SM_API_KEY and  not configured`)
+    return
+  }
+
   if (!configuration.path) {
     throw new Error(`Path of application is not found`)
   }
