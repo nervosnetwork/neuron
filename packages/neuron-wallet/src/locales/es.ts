8f920b3f0f4b2c15bd3938018bda7460fd4db41c
Natixe <49234582+Natixe@users.noreply.github.com>
2024-01-14 00:52:14 +0100

    Update packages/neuron-wallet/src/locales/es.ts
    Co-authored-by: alejandroRbit <94946624+alejandroRbit@users.noreply.github.com>

--- packages/neuron-wallet/src/locales/es.ts
+++ packages/neuron-wallet/src/locales/es.ts
@@ -137,7 +137,7 @@ export default {
       'no-exist-ckb-node-data':
         '{{path}} no tiene configuración y almacenamiento de nodo CKB, presiona confirmar para sincronizar desde cero',
       'light-client-sudt-acp-error':
-        'El modo cliente liviano no admite el envío de activos a la cuenta de activos de otra persona',
+        'El modo cliente ligero no admite el envío de activos a la cuenta de activos de otra persona',
       'could-not-connect-service': 'No se pudo conectar al servicio, por favor, inténtalo nuevamente más tarde.',
     },
     messageBox: {
