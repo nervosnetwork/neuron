f8e204f3eee19db460bcd9c3d1fc3f5dce0c0a89
devchenyan <sgt39007@163.com>
2024-05-06 21:30:47 +0800

    feat: Add a window/dialog after successful transaction actions (#3131)
    * feat: Add a window/dialog after successful transaction actions
    
    * feat: update
    
    * fix: comments
    
    * fix: comments
    
    * fix: comments
    
    * fix: showLoading
    
    * fix
    
    * fix
    
    ---------
    
    Co-authored-by: Chen Yu <keithwhisper@gmail.com>

---
+++ packages/neuron-ui/src/components/DepositRulesDialog/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import Dialog from 'widgets/Dialog'
+import styles from './depositRulesDialog.module.scss'
+
+const DepositRulesDialog = ({ show, onClose }: { show: boolean; onClose: () => void }) => {
+  const [t] = useTranslation()
+
+  return (
+    <Dialog
+      show={show}
+      title={t('nervos-dao.deposit-rules')}
+      onCancel={onClose}
+      onConfirm={onClose}
+      showCancel={false}
+      confirmText={t('deposit-rules.get')}
+      className={styles.container}
+    >
+      <div className={styles.content}>
+        {[
+          // TODO: calculating by the capacity of the lock
+          ['deposit-rules.minimum-deposit', '102 CKB'],
+          ['deposit-rules.single-compensation-cycle', 'deposit-rules.single-compensation-cycle-description'],
+          ['deposit-rules.withdraw', 'deposit-rules.withdraw-description'],
+          ['deposit-rules.unlock', 'deposit-rules.unlock-description'],
+        ].map(([title, description]) => (
+          <div className={styles.item}>
+            <p>{t(title)}</p>
+            <p className={styles.description}>{t(description)}</p>
+          </div>
+        ))}
+      </div>
+    </Dialog>
+  )
+}
+
+DepositRulesDialog.displayName = 'DepositRulesDialog'
+
+export default DepositRulesDialog
