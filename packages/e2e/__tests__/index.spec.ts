b587f959a1ef432bbc920fd02a042673286d1ffe
xu11 <5661458+pengpeng11@user.noreply.gitee.com>
2024-06-05 17:10:39 +0800

    添加用例

--- packages/e2e/__tests__/index.spec.ts
+++ packages/e2e/__tests__/index.spec.ts
@@ -1,10 +1,10 @@
-import { _electron as electron, ElectronApplication, Page } from "playwright";
-import { test, expect } from "@playwright/test";
+import {_electron as electron, ElectronApplication, Page} from "playwright";
+import {test, expect} from "@playwright/test";
 
 let electronApp: ElectronApplication;
 
 test.beforeAll(async () => {
-  electronApp = await electron.launch({ args: ["../../packages/neuron-wallet/dist/main.js"] });
+  electronApp = await electron.launch({args: ["../../packages/neuron-wallet/dist/main.js"]});
   electronApp.on("window", async (page) => {
     const filename = page.url()?.split("/").pop();
     console.log(`Window opened: ${filename}`);
@@ -18,14 +18,21 @@ test.beforeAll(async () => {
   });
 });
 
+/*
 test.afterAll(async () => {
   await electronApp.close();
 });
+*/
+
+test.beforeEach(async () => {
+  test.setTimeout(60000);
+});
+
 
 let page: Page;
 
 test("Launch Neuron", async () => {
-  const isPackaged = await electronApp.evaluate(async ({ app }) => {
+  const isPackaged = await electronApp.evaluate(async ({app}) => {
     // This runs in Electron's main process, parameter here is always
     // the result of the require('electron') in the main app script.
     return app.isPackaged;
@@ -34,7 +41,7 @@ test("Launch Neuron", async () => {
   expect(isPackaged).toBe(false);
 
   page = await electronApp.firstWindow();
-  await page.screenshot({ path: "./test-results/intro.png" });
+  await page.screenshot({path: "./test-results/intro.png"});
 
   const title = await page.title();
   expect(title).toBe("Neuron");
@@ -73,36 +80,67 @@ test("Launch Neuron", async () => {
   await page.getByPlaceholder("重复密码").fill("Aa111111");
   await page.getByLabel("完成创建").click();
   await page.screenshot({ path: "./test-results/createWallet.png" });
+});*/
+
+test.describe('OverViewPage tests', () => {
+  /*test("send transaction", async () => {
+    await page.waitForTimeout(40000);
+    await page.waitForSelector('.syncStatus_synced__JM5ln');
+    await page.getByTitle('总览').click();
+    await page.getByRole('button', {name: '转账'}).click();
+    await page.locator("id=address").fill("ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsq2glcd40rclyg8zmv6a9uzun0stz5rzp9q4jzxqs");
+    await page.locator("id=amount").fill("103");
+    await page.getByRole('button', {name: '发送'}).click();
+    await page.locator("id=password").fill('Aa111111');
+    await page.getByRole('button', {name: '确认'}).click();
+    await expect(page.getByText('已提交')).toBeVisible();
+    await page.waitForTimeout(5000);
+  });
+
+
+  test("amend transaction ", async () => {
+    await page.locator('//!*[@id="root"]/div/div/div[2]/div[1]/table/tbody/tr[1]/td[7]').click();
+    await page.getByRole('button', {name: '修改'}).click();
+    await page.getByTitle('发送').click();
+    await page.locator("id=password").fill('Aa111111');
+    await page.getByRole('button', {name: '确认'}).click();
+    await expect(page.getByText('已提交').first()).toBeVisible();
+  });*/
+  test("two cells consumes", async () => {
+    await page.waitForTimeout(10000);
+    await page.getByTitle('总览').click();
+    await page.locator('//*[@id="root"]/div/div/div[2]/div[1]/div/div[1]/div[2]/button[3]').click();
+    await expect(page.getByTitle('Cell管理')).toBeVisible();
+    //复选框勾选可选的第一个/第二个
+    await page.locator('id=0xb37255202fee78163e3933d1e27698d96d00cc279381da376fcefa4e563e4fbb_0').check();
+    await page.locator('id=0x1b19b7c4be4e3e38774600adab14cd015b160eb4a612d704aef2ee655a862219_1').check();
+
+    await page.getByRole('button', {name: '消耗'}).click();
+    await page.getByRole('button', {name: '确认'}).click();
+    await page.locator("id=address").fill('ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqt6q3xq73zw456d5me5d3gxpjyqu6wtvvsrvay5z');
+    await page.getByRole('button', {name: '发送'}).click();
+    await page.locator("id=password").fill('Aa111111');
+    await page.getByRole('button', {name: '确认'}).click();
+  });
+
 });
 
 
-test("send transaction", async () => {
-  await page.waitForSelector('.syncStatus_synced__JM5ln');
-  await page.getByTitle('总览').click();
-  await page.getByRole('button', {name: '转账'}).click();
-  await page.locator("id=address").fill("ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsq2glcd40rclyg8zmv6a9uzun0stz5rzp9q4jzxqs");
-  await page.locator("id=amount").fill("103");
-  await page.getByRole('button', {name: '发送'}).click();
-  await page.locator("id=password").fill('Aa111111');
-  await page.getByRole('button',{name: '确认'}).click();
-  await page.waitForTimeout(300000);
-});*/
-
-
-test("nervos dao deposit", async () => {
+// 等待同步进度到100%
+/*test("nervos dao deposit", async () => {
+  await page.waitForTimeout(10000);
   await page.getByTitle('Nervos Dao').click();
   await page.getByRole('button', {name: '存入'}).click();
   await page.locator("id=depositValue").fill("104");
   await page.getByRole('button', {name: '继续'}).click();
   await page.locator("id=password").fill('Aa111111');
   await page.getByRole('button',{name: '确认'}).click();
-  await page.waitForTimeout(300000);
-  await expect(page.getByRole('button', {name: '查看交易详情'})).toBeVisible();
-
+  await page.waitForTimeout(10000);
+  await expect(page.getByText('正在存入...', { exact: true })).toBeVisible();
 });
 
 
-/*test("check transaction history", async () => {
+test("check transaction history", async () => {
   await page.getByTitle('交易历史').click();
   await page.getByPlaceholder('使用交易哈希、地址或日期(yyyy-mm-dd)进行搜索').fill('2024-05-23');
   let EnterKey = "Enter";
@@ -111,30 +149,37 @@ test("nervos dao deposit", async () => {
   await page.getByRole('button', {name: '导出交易历史'}).click();
 
 });*/
+/*test.describe('实验性功能', () => {
+  test("create account in asset accounts", async () => {
+    await page.getByTitle('实验性功能').click();
+    await page.getByTitle('资产账户').click();
+    await page.getByRole('button', {name: '创建资产账户'}).click();
+    const tp: number = Date.parse(new Date().toString());
+    await page.locator("id=accountName").fill("te" + tp);
+    await page.getByRole('button', {name: '下一步'}).click();
+    await page.locator("id=tokenId").fill("0xb1718e7c0175d8a6428a6ddca708b765803e3131d07c8e0046a94be310f1722b");
+    await page.locator("id=tokenName").fill("tk01");
+    await page.locator("id=symbol").fill("sy01");
+    await page.locator("id=decimal").fill("4");
+    await page.getByRole('button', {name: '下一步'}).click();
+    await page.locator("id=password").fill("Aa111111");
+    await page.getByRole('button', {name: '确认'}).click();
+    await page.waitForTimeout(40000);
+    await expect(page.getByText('te' + tp, {exact: true})).toBeVisible();
+  });
 
 
-/*test("two cells consume", async () => {
+  /!*test("claim in customized page ", async () => {
+
+
+  });*!/
 
 
 });*/
 
 
-/*test("create account in asset accounts", async () => {
 
 
-});*/
-
-
-/*test("amend transaction ", async () => {
-
-
-});*/
-
-
-/*test("claim in customized page ", async () => {
-
-
-});*/
 
 
 
