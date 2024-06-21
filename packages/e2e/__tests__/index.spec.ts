33d3a40e9e21466dc4180214934a7bba782237ec
xu11 <5661458+pengpeng11@user.noreply.gitee.com>
2024-06-21 16:58:27 +0800

    add  single sign &&hard wallet create&&hard wallet receive&&hard wallet send testcases

--- packages/e2e/__tests__/index.spec.ts
+++ packages/e2e/__tests__/index.spec.ts
@@ -36,8 +36,6 @@ let page: Page;
 
 test("Launch Neuron", async () => {
   const appName = await electronApp.evaluate(async ({app}) => {
-    // This runs in Electron's main process, parameter here is always
-    // the result of the require('electron') in the main app script.
     return app.name;
   });
   console.log('应用名称:', appName);
