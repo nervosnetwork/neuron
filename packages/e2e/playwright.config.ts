4a24cc976085a3a014d1d2276fdd85be4e5626e5
xu11 <5661458+pengpeng11@user.noreply.gitee.com>
2024-06-28 15:52:55 +0800

    add Export transaction&&sign and export transaction&& offline sign&&multisig sign && broadcast transaction tests cases

--- packages/e2e/playwright.config.ts
+++ packages/e2e/playwright.config.ts
@@ -1,8 +1,10 @@
 import { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
-  testDir: "./__tests__",
+  testDir: "./__tests__/tests/",
   maxFailures: 2,
+  timeout: 60000,
+  retries: 1
 };
 
 export default config;
