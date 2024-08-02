f0b53875bb5ee57a74caac79d4d7eb7cc5b95ff2
devchenyan <sgt39007@163.com>
2024-08-02 09:24:03 +0800

    fix: Exception on checking addresses (#3216)
    * fix: Exception on checking addresses
    
    * fix: comment
    
    ---------
    
    Co-authored-by: Chen Yu <keithwhisper@gmail.com>

--- packages/neuron-wallet/src/locales/zh-tw.ts
+++ packages/neuron-wallet/src/locales/zh-tw.ts
@@ -129,6 +129,7 @@ export default {
       'no-exist-ckb-node-data': '{{path}} 目錄下沒有找到 CKB Node 配置和數據, 點擊確認重新同步',
       'light-client-sudt-acp-error': '輕節點模式不支持發送資產給其他用戶的資產賬戶',
       'could-not-connect-service': '無法連接到服務，請稍後再試。',
+      'address-required': '地址不可為空',
     },
     messageBox: {
       button: {
