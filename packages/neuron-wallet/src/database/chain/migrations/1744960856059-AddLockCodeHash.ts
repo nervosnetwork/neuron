b40cb488e01950ff9297dc06dce0b165139984ab
chenyan <sgt39007@163.com>
2025-05-14 01:50:37 +0800

    fix

--- packages/neuron-wallet/src/database/chain/migrations/1744960856059-AddLockCodeHash.ts
+++ packages/neuron-wallet/src/database/chain/migrations/1744960856059-AddLockCodeHash.ts
@@ -5,7 +5,7 @@ export class AddLockCodeHash1744960856059 implements MigrationInterface {
     name = 'AddLockCodeHash1744960856059'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.query(`ALTER TABLE "multisig_config" ADD COLUMN "lockCodeHash" character(32);`)
+      await queryRunner.query(`ALTER TABLE "multisig_config" ADD COLUMN "lockCodeHash" varchar`)
       await queryRunner.query(`UPDATE "multisig_config" SET lockCodeHash = "${SystemScriptInfo.LEGACY_MULTISIG_CODE_HASH}" where lockCodeHash IS NULL`)
     }
 
