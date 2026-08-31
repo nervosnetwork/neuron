6af6d17307b487f46154537240d0a0572d507453
elonfliter <elonfliter@outlook.com>
2026-08-31 18:05:30 +0800

    test: correct SUDT parser test description
    Signed-off-by: elonfliter <elonfliter@outlook.com>

--- packages/neuron-wallet/tests/utils/parse_sudt_token_info/index.test.ts
+++ packages/neuron-wallet/tests/utils/parse_sudt_token_info/index.test.ts
@@ -1,7 +1,7 @@
 import parseSUDTTokenInfo from '../../../src/utils/parse_sudt_token_info'
 
 describe('parseSUDTTokenInfo', () => {
-  it('parse sudt token info should success', function () {
+  it('parse sudt token info should succeed', function () {
     const hexData = '0x080a456972632d320a455432'
     const { decimal, name, symbol } = parseSUDTTokenInfo(hexData)
     expect(decimal).toEqual('8')
